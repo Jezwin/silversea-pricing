250541d85aaa94aa3a58d7c0dd7787041507cbe7
Anais SIBA <asiba@sqli.com>
2017-01-25 14:38:47 +0100

    Header : clean css, dev plugin burger nav. wip : search bar & dropdown lang

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/burgernav.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/burgernav.js
@@ -1,9 +1,50 @@
 +function ($) {
     'use strict';
 
-    $(function() {
-        $(".c-nav-burger button").click(function () {
-            $(this).parent().toggleClass("is-active");
+    $.fn.BurgerNav = function() {
+        
+        var _self = this;
+        this.isActive = false;
+        
+        /*
+        ** Toggled Classes to the Navigation
+        */
+        this.toggledClass = function () {
+            _self.upActivity();
+            
+            _self.parent().toggleClass("is-active");
+            $('.c-main-navigation').toggleClass("c-main-navigation--open");
+            $('.c-main-navigation__backdrop').toggleClass("c-main-navigation__backdrop--is-active");
+        }
+        
+        /*
+        ** Update the status of navigation
+        */
+        this.upActivity = function () {
+            _self.isActive = !_self.isActive;
+        }
+
+        /*
+        ** Click Event to show / hide navigation
+        */
+        this.click(function () {
+            _self.toggledClass();
+            
+            if (_self.isActive) {
+                $(window).on("resize", function() {
+                    if ($(this).width() >= 992 && _self.isActive) {
+                        _self.toggledClass();
+                        $(this).off("resize");
+                    }
+                });
+            }
         });
+    };
+    
+    $(function() {
+        /*
+        ** Set Plugin BurgerNav to the button
+        */
+        $(".c-nav-burger button").BurgerNav();
     });
 }(jQuery);
--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/header.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/header.js
@@ -2,8 +2,34 @@
     'use strict';
 
     $(function() {
-//        $(".c-nav-burger button").click(function () {
-//            $(this).parent().toggleClass("is-active");
-//        });
+
+        var show = true;
+        $(document).scroll(function () {
+            console.log('$(document).scrollTop() : ', $(document).scrollTop());
+            
+            if ($(document).scrollTop() > 120 && show) {
+                $('.c-main-navigation__bottom').toggleClass('c-main-navigation__bottom--hide');  
+                show = false;
+            }
+            else if ($(document).scrollTop() < 120 && !show) {
+                $('.c-main-navigation__bottom').toggleClass('c-main-navigation__bottom--hide');  
+                show = true;
+            }
+            
+        });
+        /*
+            <div class="new section aem-Grid-newComponent" style="
+                display: table;
+                height: 900px;
+                width: 100%;
+            ">
+              <div style="
+                display: table-cell;
+                vertical-align: middle;
+            ">
+              	CONTENT
+              </div>
+            </div>
+        */
     });
 }(jQuery);
