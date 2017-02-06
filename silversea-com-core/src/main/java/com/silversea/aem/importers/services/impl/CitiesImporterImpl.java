ccfb0ceffa8f82503bfb29d7577e4120d49c62c5
Aurelien Olivier <auolivier@sqli.com>
2017-02-06 14:36:28 +0100

    Importer update

--- silversea-com-core/src/main/java/com/silversea/aem/importers/services/impl/CitiesImporterImpl.java
+++ silversea-com-core/src/main/java/com/silversea/aem/importers/services/impl/CitiesImporterImpl.java
@@ -1,6 +1,10 @@
 package com.silversea.aem.importers.services.impl;
 
+import com.day.cq.commons.jcr.JcrUtil;
+import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
+import com.day.cq.wcm.api.WCMException;
+import com.silversea.aem.importers.ImportersConstants;
 import com.silversea.aem.importers.authentication.DigestAuthenticationInfos;
 import com.silversea.aem.importers.services.CitiesImporter;
 import io.swagger.client.ApiException;
@@ -12,9 +16,15 @@ import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import java.io.IOException;
 import java.util.List;
 
@@ -28,7 +38,7 @@ public class CitiesImporterImpl implements CitiesImporter {
     static final private Logger LOGGER = LoggerFactory.getLogger(CitiesImporterImpl.class);
 
     @Reference
-    private PageManager pageManager;
+    private ResourceResolverFactory resourceResolverFactory;
 
     @Override
     public void importCities() throws IOException {
@@ -56,23 +66,51 @@ public class CitiesImporterImpl implements CitiesImporter {
                 List<City> result = citiesApi.citiesGet(null, null, null,
                         null, null, null, null);
 
-                for (City city : result) {
-                    LOGGER.debug("City: {}", city.getCityName());
+                ResourceResolver resourceResolver = resourceResolverFactory.getAdministrativeResourceResolver(null);
+                PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+                Session session = resourceResolver.adaptTo(Session.class);
 
-                    /*Page citiesRootPage = pageManager.getPage(ImportersConstants.CITIES_BASE_PATH);
-                    pageManager.create(citiesRootPage.getPath(),
-                            JcrUtil.createValidChildName(citiesRootPage.adaptTo(Node.class), city.getCityName()),
-                            "",
+                Page citiesRootPage = pageManager.getPage(ImportersConstants.BASEPATH_PORTS);
+
+                int i = 0;
+
+                for (City city : result) {
+                    LOGGER.error("City: {}", city.getCityName());
+
+                    final String portFirstLetter = String.valueOf(city.getCityName().charAt(0));
+                    final String portFirstLetterName = JcrUtil.createValidName(portFirstLetter);
+
+                    Page portFirstLetterPage;
+
+                    if (citiesRootPage.hasChild(portFirstLetterName)) {
+                        portFirstLetterPage = pageManager.getPage(ImportersConstants.BASEPATH_PORTS
+                                + "/" + portFirstLetterName);
+                    } else {
+                        portFirstLetterPage = pageManager.create(citiesRootPage.getPath(),
+                                portFirstLetterName,
+                                "/apps/silversea/silversea-com/templates/page",
+                                portFirstLetter,
+                                false);
+                    }
+
+                    pageManager.create(portFirstLetterPage.getPath(),
+                            JcrUtil.createValidChildName(portFirstLetterPage.adaptTo(Node.class), city.getCityName()),
+                            "/apps/silversea/silversea-com/templates/page",
                             city.getCityName(),
-                            false);*/
+                            false);
+
+                    i++;
+
+                    if (i%100 == 0) {
+                        session.save();
+                    }
                 }
-            } catch (ApiException e) {
+
+                session.save();
+
+            } catch (ApiException | WCMException | LoginException | RepositoryException e) {
                 LOGGER.error("Exception importing cities", e);
-            }/* catch (RepositoryException e) {
-                e.printStackTrace();
-            } catch (WCMException e) {
-                e.printStackTrace();
-            }*/
+            }
         }
     }
 
