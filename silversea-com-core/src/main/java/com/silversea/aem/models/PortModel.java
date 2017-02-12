dc22dbfbda56116826fe9c4205b60b9c75f01efd
Aurelien Olivier <auolivier@sqli.com>
2017-02-13 00:19:52 +0100

    Adding link between models exemple

--- silversea-com-core/src/main/java/com/silversea/aem/models/PortModel.java
+++ silversea-com-core/src/main/java/com/silversea/aem/models/PortModel.java
@@ -2,10 +2,18 @@ package com.silversea.aem.models;
 
 import com.day.cq.commons.jcr.JcrConstants;
 import com.day.cq.wcm.api.Page;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by aurelienolivier on 12/02/2017.
@@ -13,11 +21,39 @@ import javax.inject.Named;
 @Model(adaptables = Page.class)
 public class PortModel {
 
+    static final private Logger LOGGER = LoggerFactory.getLogger(PortModel.class);
+
+    @Inject @Self
+    private Page page;
+
     @Inject @Named(JcrConstants.JCR_CONTENT + "/" + JcrConstants.JCR_TITLE)
     private String title;
 
     @Inject @Named(JcrConstants.JCR_CONTENT + "/apiDescription")
     private String apiDescription;
+
+    private List<ExcursionModel> excursions;
+
+    @PostConstruct
+    private void init() {
+        excursions = new ArrayList<>();
+
+        final Iterator<Page> childs = page.listChildren();
+
+        LOGGER.debug("{}", page.getPath());
+
+        while (childs.hasNext()) {
+            Page child = childs.next();
+
+            if (child.getName().equals("excursions")) {
+                Iterator<Page> excursionsPages = child.listChildren();
+
+                while (excursionsPages.hasNext()) {
+                    excursions.add(excursionsPages.next().adaptTo(ExcursionModel.class));
+                }
+            }
+        }
+    }
 
     public String getTitle() {
         return title;
@@ -25,5 +61,9 @@ public class PortModel {
 
     public String getApiDescription() {
         return apiDescription;
+    }
+
+    public List<ExcursionModel> getExcursions() {
+        return excursions;
     }
 }
