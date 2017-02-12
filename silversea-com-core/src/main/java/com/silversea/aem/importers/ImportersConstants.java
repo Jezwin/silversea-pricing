2faa423f6eb38b624507f98017fbe910cc17f10d
Aurelien Olivier <auolivier@sqli.com>
2017-02-12 23:22:01 +0100

    Removing unused constants

--- silversea-com-core/src/main/java/com/silversea/aem/importers/ImportersConstants.java
+++ silversea-com-core/src/main/java/com/silversea/aem/importers/ImportersConstants.java
@@ -1,7 +1,5 @@
 package com.silversea.aem.importers;
 
-import java.util.HashMap;
-
 /**
  * @author aurelienolivier
  */
@@ -9,9 +7,4 @@ public interface ImportersConstants {
 
     String BASEPATH_PORTS = "/content/silversea-com/en/other-resources/find-a-port";
 
-    HashMap PROPERTIES_MAPPING_PORTS = new HashMap() {{
-        put("city_cod", "cityCode");
-        put("city_name", "jcr:title");
-        put("description", "jcr:description");
-    }};
 }
