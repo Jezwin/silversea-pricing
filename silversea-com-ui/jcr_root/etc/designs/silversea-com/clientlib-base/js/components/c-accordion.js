206b440e3961a31aba9b8e486350db5fa89a50a8
Anais SIBA <asiba@sqli.com>
2017-02-14 12:23:46 +0100

    SSC-982 : Excursions list

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-accordion.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-accordion.js
@@ -8,7 +8,10 @@
         var _data = {
             elem: '.c-accordion__elem',
             link: '.c-accordion__header',
-            content: '.c-accordion__body'
+            content: '.c-accordion__body',
+            action: '.c-accordion__action',
+            more: 'See More&nbsp;<i class="fa fa-angle-down"></i>',
+            less: 'Close&nbsp;<i class="fa fa-angle-up"></i>'
         };
         var _elems = _self.find(_data.elem);
         var _links = _self.find(_data.link);
@@ -17,13 +20,16 @@
         /*
         ** Link Click Event to show content
         */
-        _elems.children(".c-accordion__header").click(function (e) {
+        _elems.children('.c-accordion__header').click(function (e) {
             e.preventDefault();
 
-            var elem = $(this).parent().children(".c-accordion__body");
-            var isCollapsed = (elem.attr("data-collapsed") == 'true');
-            elem.attr("data-collapsed", !isCollapsed);
-            $(this).attr("data-state", !isCollapsed);
+            var elem = $(this).parent().children('.c-accordion__body');
+            var isCollapsed = (elem.attr('data-collapsed') == 'true');
+            var value = (!isCollapsed) ? _data.less : _data.more;
+
+            elem.attr('data-collapsed', !isCollapsed);
+            $(this).attr('data-state', !isCollapsed);
+            $(this).children(_data.action).html(value);
 
         });
     };
