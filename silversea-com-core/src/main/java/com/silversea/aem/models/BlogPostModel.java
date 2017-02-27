709989a28469f5227c8e2b214bde6e66b51bfb5a
mbennabi <mbennabi@sqli.com>
2017-02-27 18:33:54 +0100

    SCC-871 blog post template ,

---
+++ silversea-com-core/src/main/java/com/silversea/aem/models/BlogPostModel.java
@@ -0,0 +1,47 @@
+package com.silversea.aem.models;
+
+import java.util.Date;
+import java.util.Iterator;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.cq.commons.jcr.JcrConstants;
+import com.day.cq.wcm.api.Page;
+
+/**
+ * Created by mbennabi on 20/02/2017.
+ */
+@Model(adaptables = Page.class)
+public class BlogPostModel {
+
+    static final private Logger LOGGER = LoggerFactory.getLogger(BlogPostModel.class);
+    
+    @Inject @Self
+    private Page page;
+
+    @Inject @Named(JcrConstants.JCR_CONTENT + "/" + JcrConstants.JCR_TITLE)
+    private String title;
+
+    
+    @PostConstruct
+    private void init() {
+
+    
+
+    }
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+}
