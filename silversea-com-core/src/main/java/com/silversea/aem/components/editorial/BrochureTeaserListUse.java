43ff42e6f1b27b300f149d135d6b93a972f9bf65
Jedli Mejdi <Jedli Mejdi>
2017-02-28 15:13:12 +0100

    SSC-933 Brochure list component add list lang

--- silversea-com-core/src/main/java/com/silversea/aem/components/editorial/BrochureTeaserListUse.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/editorial/BrochureTeaserListUse.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import javax.jcr.Node;
+import javax.jcr.NodeIterator;
 
 import org.apache.sling.api.resource.Resource;
 
@@ -12,6 +13,8 @@ import com.adobe.cq.sightly.WCMUsePojo;
 import com.silversea.aem.components.services.GeolocationTagCacheService;
 
 public class BrochureTeaserListUse extends WCMUsePojo {
+
+    private static final String CONTENT_DAM_SIVERSEA_COM = "/content/dam/siversea-com";
 
     private static final String COUNTRY_ID_PARAM = "countryIdParam";
 
@@ -21,8 +24,22 @@ public class BrochureTeaserListUse extends WCMUsePojo {
 
     private GeolocationTagCacheService geolocService;
 
+    private List<String> langList;
+
     @Override
     public void activate() throws Exception {
+
+        Resource resourceNode = getResourceResolver().getResource(CONTENT_DAM_SIVERSEA_COM);
+        Node silverseaDamNode = resourceNode.adaptTo(Node.class);
+        NodeIterator langNodeIterator = silverseaDamNode.getNodes();
+
+        langList = new ArrayList<String>();
+
+        while (langNodeIterator.hasNext()) {
+            Node currentLangNode = (Node) langNodeIterator.next();
+            if (currentLangNode.getName() != null && !"jcr:content".equals(currentLangNode.getName()))
+                langList.add(currentLangNode.getName());
+        }
 
         geolocService = getSlingScriptHelper().getService(GeolocationTagCacheService.class);
 
@@ -40,5 +57,9 @@ public class BrochureTeaserListUse extends WCMUsePojo {
 
     public List<String> getBrochureList() {
         return brochureList;
+    }
+
+    public List<String> getLangList() {
+        return langList;
     }
 }
