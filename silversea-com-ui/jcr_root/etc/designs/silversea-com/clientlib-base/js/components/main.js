42fe03f652b639c8f77fb996ab6d6a47993a08a3
psoeung <psoeung@sqli.com>
2017-03-01 11:02:57 +0100

    SSC-872 Clean modal content on close event

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/main.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/main.js
@@ -1,3 +1,11 @@
-jQuery(document).ready(function(){
-	jQuery(".chosen").chosen();
+$(function() {
+    // Activate chosen plugin
+    jQuery(document).ready(function(){
+        jQuery(".chosen").chosen();
+    });
+
+    // Clean modal content on close event
+    $(document).on('hide.bs.modal', function (e) {
+        $(e.target).removeData('bs.modal');
+    });
 });
