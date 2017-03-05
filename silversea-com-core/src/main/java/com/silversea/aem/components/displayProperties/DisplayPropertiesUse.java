dc5f835421ee6f11d3287305a74fbeba38e5c6fe
Aurelien Olivier <auolivier@sqli.com>
2017-03-05 23:58:55 +0100

    SSC-991 Init port display properties component

--- silversea-com-core/src/main/java/com/silversea/aem/components/displayProperties/DisplayPropertiesUse.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/displayProperties/DisplayPropertiesUse.java
@@ -1,21 +1,32 @@
-package com.silversea.aem.components.editorial;
+package com.silversea.aem.components.displayProperties;
 
 import com.adobe.cq.sightly.WCMUsePojo;
 import org.apache.sling.api.resource.ValueMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by aurelienolivier on 05/03/2017.
  */
 public class DisplayPropertiesUse extends WCMUsePojo {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DisplayPropertiesUse.class);
+
     private String displayProperty = null;
+
+    protected String defaultTarget = null;
+
+    protected String defaultFallback = null;
 
     @Override
     public void activate() throws Exception {
         ValueMap pageProperties = getCurrentPage().getProperties();
 
-        String target = getProperties().get("target", String.class);
-        String fallback = getProperties().get("fallback", String.class);
+        LOGGER.debug("defaultTarget: {}", defaultTarget);
+        LOGGER.debug("defaultFallback: {}", defaultFallback);
+
+        String target = getProperties().get("target", defaultTarget);
+        String fallback = getProperties().get("fallback", defaultFallback);
 
         if (target != null) {
             displayProperty = pageProperties.get(target, String.class);
