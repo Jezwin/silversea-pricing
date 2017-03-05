2975233701f04547471d2326a0014639f5348400
Aurelien Olivier <auolivier@sqli.com>
2017-03-05 19:00:12 +0100

    SSC-912 fixing floating bar issues on edit mode

---
+++ silversea-com-core/src/main/java/com/silversea/aem/helper/RunModesHelper.java
@@ -0,0 +1,36 @@
+package com.silversea.aem.helper;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+import org.apache.sling.settings.SlingSettingsService;
+
+public class RunModesHelper extends WCMUsePojo {
+
+    private SlingSettingsService slingSettingsService;
+
+    @Override
+    public void activate() throws Exception {
+        slingSettingsService = getSlingScriptHelper().getService(SlingSettingsService.class);
+    }
+
+    /**
+     * Check runmode if author.
+     *
+     * @return
+     */
+    public boolean isAuthor() {
+        return slingSettingsService.getRunModes().contains("author");
+    }
+
+    /**
+     * Check runmode if publish
+     *
+     * @return
+     */
+    public boolean isPublish() {
+        return slingSettingsService.getRunModes().contains("publish");
+    }
+
+    public String getAuthorRunModeClass() {
+        return isAuthor() ? "runmode-author" : "";
+    }
+}
