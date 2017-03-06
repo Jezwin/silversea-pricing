6fd82a2970ade9475d7ea74f2adb6418c6915797
psoeung <psoeung@sqli.com>
2017-03-06 14:32:59 +0100

    SSC-953 get modal size from lightbox page

---
+++ silversea-com-core/src/main/java/com/silversea/aem/helper/PageHelper.java
@@ -0,0 +1,23 @@
+package com.silversea.aem.helper;
+
+import org.apache.sling.api.resource.Resource;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+import com.day.cq.wcm.api.Page;
+
+public class PageHelper extends WCMUsePojo {
+    private Page page;
+
+    @Override
+    public void activate() throws Exception {
+        String path = get("path", String.class);
+        Resource resource = getResourceResolver().getResource(path);
+        if (resource != null) {
+            page = resource.adaptTo(Page.class);
+        }
+    }
+
+    public Page getPage() {
+        return page;
+    }
+}
