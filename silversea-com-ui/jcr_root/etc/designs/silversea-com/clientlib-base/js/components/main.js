4a7570a987f8ac5caf08b5ee95bcd0e9e092a29c
psoeung <psoeung@sqli.com>
2017-03-07 18:38:07 +0100

    SSC-1148 init Asset Automatic Lighbox

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/main.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/main.js
@@ -1,15 +1,33 @@
 $(function() {
+    /***************************************************************************
+     * Chosen
+     **************************************************************************/
     // Activate chosen plugin
     jQuery(document).ready(function() {
         jQuery(".chosen").chosen();
     });
 
+    /***************************************************************************
+     * Modal
+     **************************************************************************/
     // Clean modal content on close event
     $(document).on('hide.bs.modal', function(e) {
         $(e.target).removeData('bs.modal');
         $('.modal-content').empty();
     });
 
+    // Build modal fragment for image
+    $('.imagemodal').on('click', function(e) {
+        e.preventDefault();
+        var img = $('<img style="width:100%" />');
+        img.attr('src', $(this).attr('href'));
+        $($(this).data('target')).modal('show');
+        $('.modal-content:visible').append(img);
+    });
+
+    /***************************************************************************
+     * Brochure teaser
+     **************************************************************************/
     // Redirect to page with brochure in the current language
     $('#selectBrochureListLangId').on('change', function() {
         window.location.href = this.value;
