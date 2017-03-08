b4309f2aef0d75b5a6fb168f4caec16c0b88b904
psoeung <psoeung@sqli.com>
2017-03-08 18:34:37 +0100

    SSC-1148 create java class for getter

--- silversea-com-core/src/main/java/com/silversea/aem/components/included/VariationAssetDisplay.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/included/VariationAssetDisplay.java
@@ -1,19 +1,77 @@
 package com.silversea.aem.components.included;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.adobe.cq.sightly.WCMUsePojo;
 
 public class VariationAssetDisplay extends WCMUsePojo {
-    private Resource resourceImage;
+    private Boolean hasPlanContent;
+    private Boolean hasLocationContent;
+    private Boolean hasVirtualTourContent;
+
+    private String planLightboxPath;
+    private String[] locationlightboxPaths;
+    private String virualtourLightboxPath;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VariationAssetDisplay.class);
 
     @Override
     public void activate() throws Exception {
-        Resource resourceImage = getResourceResolver().getResource(getProperties().get("plan", String.class));
+        hasPlanContent = hasContent(getResource().getPath() + "/plan/image");
+        hasLocationContent = hasContent(getResource().getPath() + "/locationImages/image");
+        hasVirtualTourContent = hasContent(getResource().getPath() + "/virtualTour/image");
     }
 
-    public Resource getResourceImage() {
-        return resourceImage;
+    private Boolean hasContent(String path) {
+        Resource res = getResourceResolver().resolve(path);
+        if (res != null) {
+            return StringUtils.isNotEmpty(res.getValueMap().get("fileReference", String.class));
+        }
+        return false;
     }
 
+    /**
+     * @return the hasPlanContent
+     */
+    public Boolean hasPlanContent() {
+        return hasPlanContent;
+    }
+
+    /**
+     * @return the hasLocationContent
+     */
+    public Boolean hasLocationContent() {
+        return hasLocationContent;
+    }
+
+    /**
+     * @return the hasVirtualTourContent
+     */
+    public Boolean hasVirtualTourContent() {
+        return hasVirtualTourContent;
+    }
+
+    /**
+     * @return the planLightboxPath
+     */
+    public String getPlanLightboxPath() {
+        return planLightboxPath;
+    }
+
+    /**
+     * @return the locationThumbnailPaths
+     */
+    public String[] getLocationThumbnailPaths() {
+        return locationlightboxPaths;
+    }
+
+    /**
+     * @return the virualtourLightboxPath
+     */
+    public String getVirualtourLightboxPath() {
+        return virualtourLightboxPath;
+    }
 }
