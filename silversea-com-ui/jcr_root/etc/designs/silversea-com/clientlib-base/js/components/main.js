c0841f5bb0d2006960333302bdac9474537dd2e0
psoeung <psoeung@sqli.com>
2017-03-10 10:46:23 +0100

    SSC-1148 custom style for variation asset display lightbox

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/main.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/main.js
@@ -14,15 +14,22 @@ $(function() {
     $(document).on('hide.bs.modal', function(e) {
         $(e.target).removeData('bs.modal');
         $('.modal-content').empty();
+        // Force to default class
+        $('.modal-content').attr('class', 'modal-content');
     });
 
     // Build modal fragment for image
     $('.automatic-modal, .gallery-modal, .virtual-tour-modal').on('click', function(e) {
         e.preventDefault();
-        var img = $('<img style="display:block;margin: 0 auto;" />');
-        img.attr('src', $(this).attr('href'));
+        $modalContent = $('<div class="modal-content modal-content-transparent">' +
+                              '<div class="modal-header"><button class="close c-btn--close" type="button" data-dismiss="modal" aria-label="Close"></button></div>' +
+                              '<div class="modal-body automatic-modal-body"></div>' +
+                          '</div>')
+        var $img = $('<img class="o-img" />');
+        $img.attr('src', $(this).attr('href'));
         $($(this).data('target')).modal('show');
-        $('.modal-content:visible').append(img);
+        $('.modal-content:visible').replaceWith($modalContent);
+        $('.modal-content:visible .modal-body').append($img);
     });
 
     /***************************************************************************
