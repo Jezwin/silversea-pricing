e19b4255d5f01641e865ece7ed2607f5756d2e85
psoeung <psoeung@sqli.com>
2017-03-15 15:32:56 +0100

    SSC-1148 Get thumbnail from asset (Image and ImageSet)

--- silversea-com-core/src/main/java/com/silversea/aem/components/included/VariationAssetDisplay.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/included/VariationAssetDisplay.java
@@ -1,12 +1,20 @@
 package com.silversea.aem.components.included;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.resource.collection.ResourceCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.adobe.cq.sightly.WCMUsePojo;
 import com.day.cq.dam.api.Asset;
+import com.day.cq.dam.api.s7dam.constants.S7damConstants;
 
 public class VariationAssetDisplay extends WCMUsePojo {
+    private static final Logger LOGGER = LoggerFactory.getLogger(VariationAssetDisplay.class);
 
     @Override
     public void activate() throws Exception {
@@ -15,16 +23,39 @@ public class VariationAssetDisplay extends WCMUsePojo {
     /**
      * @return image path
      */
-    private String renditionLightboxPath(String componentPath) {
+    private String renditionPath(String componentPath) {
+        // Check if the node is created
         Resource componentRes = getResourceResolver().resolve(getResource().getPath() + componentPath);
         if (componentRes != null) {
             String imagePath = componentRes.getValueMap().get("fileReference", String.class);
             if (StringUtils.isNotEmpty(imagePath)) {
-                Asset asset;
-                Resource resAsset = getResourceResolver().getResource(imagePath);
-                if (resAsset != null) {
-                    asset = resAsset.adaptTo(Asset.class);
-                    return asset.getImagePreviewRendition().getPath();
+                Resource assetResource = getResourceResolver().getResource(imagePath);
+                // Check if the asset exist
+                if (assetResource != null) {
+                    Asset asset = assetResource.adaptTo(Asset.class);
+                    Node assetNode = assetResource.adaptTo(Node.class);
+
+                    String assetType = null;
+                    String thumbnailPath = "";
+                    try {
+                        if (assetNode.hasProperty(S7damConstants.S7_ASSET_METADATA_NODE + "/" + S7damConstants.PN_S7_TYPE)) {
+                            assetType = assetNode.getProperty(S7damConstants.S7_ASSET_METADATA_NODE + "/" + S7damConstants.PN_S7_TYPE).getString();
+                        }
+                    } catch (RepositoryException e) {
+                        LOGGER.error("Exception, asset node not find", e);
+                    }
+
+                    if (assetType.equals(S7damConstants.S7_IMAGE_SET)) {
+                        // Dynamic Media Image Set
+                        Resource members = getResourceResolver().resolve(asset.getPath() + "/jcr:content/related/s7Set");
+                        ResourceCollection membersCollection = members.adaptTo(ResourceCollection.class);
+                        // Use the first member for thumbnail
+                        thumbnailPath = membersCollection.getResources().next().getPath();
+                    } else if (assetType.equals(S7damConstants.S7_IMAGE)) {
+                        // Dynamic Media Image
+                        thumbnailPath = asset.getImagePreviewRendition().getPath();
+                    }
+                    return thumbnailPath;
                 }
             }
         }
@@ -34,21 +65,21 @@ public class VariationAssetDisplay extends WCMUsePojo {
     /**
      * @return the plan image path
      */
-    public String getPlanLightboxPath() {
-        return renditionLightboxPath("/plan/image");
+    public String getPlanImageRenditionPath() {
+        return renditionPath("/plan/image");
     }
 
     /**
      * @return the location image path
      */
-    public String getLocationLightboxPath() {
-        return renditionLightboxPath("/locationImages/image");
+    public String getLocationImageRenditionPath() {
+        return renditionPath("/locationImages/image");
     }
 
     /**
      * @return the virtual tour image path
      */
-    public String getVirualtourLightboxPath() {
-        return renditionLightboxPath("/virtualTour/image");
+    public String getVirtualtourImageRenditionPath() {
+        return renditionPath("/virtualTour/image");
     }
 }
