c71a9185bf78ef382b735c6f9ff7f36f1b56afba
psoeung <psoeung@sqli.com>
2017-03-17 10:00:55 +0100

    SSC-1148 - Customize gallery style

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/main.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/main.js
@@ -13,26 +13,52 @@ $(function() {
     // Clean modal content on close event
     $(document).on('hide.bs.modal', function(e) {
         $(e.target).removeData('bs.modal');
-        $('.modal-content').empty();
+        var $modalContent = $('body > .modal-content');
+        $modalContent.empty();
+
         // Force to default class
-        $('.modal-content').attr('class', 'modal-content');
+        $modalContent.attr('class', 'modal-content');
+
+        // Destroy gallery inside modal
+        $modalContent.find('.c-slider--for, .c-slider--nav').slick('unslick');
+
     });
 
-    // Build modal fragment for image
-    $('.automatic-modal, .gallery-modal, .virtual-tour-modal').on(
-            'click',
-            function(e) {
-                e.preventDefault();
-                $modalContent = $('<div class="modal-content modal-content-transparent">'
-                        + '<div class="modal-header"><button class="close c-btn--close" type="button" data-dismiss="modal" aria-label="Close"></button></div>'
-                        + '<div class="modal-body automatic-modal-body"></div>' + '</div>')
-                var $img = $('<img class="o-img" />');
-                $img.attr('src', $(this).attr('href'));
-                $($(this).data('target')).modal('show');
-                $('.modal-content:visible').replaceWith($modalContent);
-                $('.modal-content:visible .modal-body').append($img);
-            });
+    // Build modal for image
+    $('.automatic-modal, .virtual-tour-modal').on('click', function(e) {
+        e.preventDefault();
+        $modalContent = $('<div class="modal-content modal-content--transparent">'
+                + '<div class="modal-header"><button class="close c-btn--close" type="button" data-dismiss="modal" aria-label="Close"></button></div>'
+                + '<div class="modal-body automatic-modal-body"></div>' + '</div>')
+        var $img = $('<img class="o-img" />');
+        $img.attr('src', $(this).attr('href'));
+        $($(this).data('target')).modal('show');
+        $('.modal-content:visible').replaceWith($modalContent);
+        $('.modal-content:visible .modal-body').append($img);
+    });
 
+    // Build modal fragment for Gallery
+    $('.automatic-gallery-modal').on('click', function(e) {
+        e.preventDefault();
+        var $link = $(this);
+
+        $($link.data('target')).modal('show');
+        var $modal = $('.modal-content:visible');
+        $modal.replaceWith($link.siblings(':hidden').html());
+
+        // Build gallery
+        $('.c-slider--for').slick({
+            slidesToShow : 1,
+            slidesToScroll : 1,
+            asNavFor : '.c-slider--nav'
+        });
+        $('.c-slider--nav').slick({
+            slidesToShow : 6,
+            slidesToScroll : 1,
+            asNavFor : '.c-slider--for',
+            focusOnSelect : true
+        });
+    });
     /***************************************************************************
      * Brochure teaser
      **************************************************************************/
@@ -40,20 +66,4 @@ $(function() {
     $('#selectBrochureListLangId').on('change', function() {
         window.location.href = this.value;
     })
-
-    /***************************************************************************
-     * Gallery
-     **************************************************************************/
-    $('.slider-for').slick({
-        slidesToShow : 1,
-        slidesToScroll : 1,
-        asNavFor : '.slider-nav'
-    });
-    $('.slider-nav').slick({
-        slidesToShow : 6,
-        slidesToScroll : 1,
-        asNavFor : '.slider-for',
-        dots : true,
-        focusOnSelect : true
-    });
 });
