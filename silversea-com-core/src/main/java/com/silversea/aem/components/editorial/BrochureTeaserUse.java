2de31c7c6f80bc817cd8662cebd0011e7b6c6c96
Aurelien Olivier <auolivier@sqli.com>
2017-03-18 21:46:36 +0100

    Brochure teaser refactoring

--- silversea-com-core/src/main/java/com/silversea/aem/components/editorial/BrochureTeaserUse.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/editorial/BrochureTeaserUse.java
@@ -1,101 +1,68 @@
 package com.silversea.aem.components.editorial;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
 import com.adobe.cq.sightly.WCMUsePojo;
 import com.day.cq.dam.api.Asset;
 import com.day.cq.dam.api.DamConstants;
+import com.day.cq.dam.api.Rendition;
+import com.day.cq.dam.api.RenditionPicker;
+import com.day.cq.dam.commons.util.PrefixRenditionPicker;
+import com.silversea.aem.constants.WcmConstants;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
+
+import java.util.List;
 
 public class BrochureTeaserUse extends WCMUsePojo {
 
-    private static final String BROCHURE_PROPERTY = "brochure";
-
     private String brochurePath;
-
-    private String assetDescription;
-
-    private String assetTitle;
+    private Asset asset;
 
     private List<String> assetThumbnailsList;
 
-    private Asset asset;
-
     private boolean showOnlineRequestForm;
-
     private boolean showPrintedRequestForm;
-
     private String onlineRequestFormURL;
-
     private String printedRequestForm;
 
     @Override
     public void activate() throws Exception {
+        ValueMap properties = getProperties();
 
-        if (get("brochurePathParam", String.class) != null) {
-            getRequest().setAttribute(BROCHURE_PROPERTY, get("brochurePathParam", String.class));
-        }
-        if (getRequest().getAttribute(BROCHURE_PROPERTY) != null) {
-            brochurePath = getRequest().getAttribute(BROCHURE_PROPERTY).toString();
+        brochurePath = properties.get(WcmConstants.PN_FILE_REFERENCE, String.class);
 
-            // TODO set the currentstyle param.
-            showOnlineRequestForm = false;
-            showPrintedRequestForm = false;
-            onlineRequestFormURL = "";
-            printedRequestForm = "";
-        } else {
-            brochurePath = getProperties().get(BROCHURE_PROPERTY, String.class);
-        }
+        // TODO set the currentstyle param.
+        showOnlineRequestForm = false;
+        showPrintedRequestForm = false;
+        onlineRequestFormURL = "";
+        printedRequestForm = "";
+
         if (brochurePath != null) {
-            Resource resourceDam = getResourceResolver().getResource(brochurePath);
-            asset = resourceDam.adaptTo(Asset.class);
+            Resource assetResource = getResourceResolver().getResource(brochurePath);
+            asset = assetResource.adaptTo(Asset.class);
+        }
+    }
 
-            /* TODO replace by dynamic media */
-            assetThumbnailsList = getThumbnailsPathList(getRequest());
+    public String getCover() {
+        if (asset != null) {
+            RenditionPicker renditionPicker = new PrefixRenditionPicker("cover");
+            Rendition rendition = asset.getRendition(renditionPicker);
 
-            assetTitle = asset.getMetadataValue(DamConstants.DC_TITLE);
-            assetDescription = asset.getMetadataValue(DamConstants.DC_DESCRIPTION);
-        } else {
-            assetTitle = "";
-            assetDescription = "";
+            return rendition.getPath();
         }
 
-    }
-
-    /* TODO replace by dynamic media */
-    public List<String> getThumbnailsPathList(SlingHttpServletRequest request) {
-
-        List<String> list = new ArrayList<String>();
-        for (int i = 0; i < asset.getRenditions().size(); i++) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(request.getRequestURL().toString().split("/content/")[0]);
-            sb.append(asset.getRenditions().get(i).getPath());
-            list.add(sb.toString());
-        }
-        return list;
-    }
-
-    public List<String> getAssetThumbnailsList() {
-        return assetThumbnailsList;
-    }
-
-    public String getAssetImage() {
-        // "http://localhost:4502" + asset.getRenditions().get(0).getPath();
-        return assetThumbnailsList.get(1).toString();
+        return null;
     }
 
     public String getBrochurePath() {
         return brochurePath;
     }
 
-    public String getAssetDescription() {
-        return assetDescription;
+    public String getAssetTitle() {
+        return asset != null ? asset.getMetadataValue(DamConstants.DC_TITLE) : null;
     }
 
-    public String getAssetTitle() {
-        return assetTitle;
+    public String getAssetDescription() {
+        return asset != null ? asset.getMetadataValue(DamConstants.DC_DESCRIPTION) : null;
     }
 
     public boolean isShowOnlineRequestForm() {
--- silversea-com-core/src/main/java/com/silversea/aem/constants/WcmConstants.java
+++ silversea-com-core/src/main/java/com/silversea/aem/constants/WcmConstants.java
@@ -7,7 +7,7 @@ package com.silversea.aem.constants;
  * @author mjedli
  *
  */
-public final class WcmConstants {
+public interface WcmConstants {
     /*
      * Before creating constant, check first this following page
      * https://docs.adobe.com/docs/en/aem/6-2/develop/ref/javadoc/constant-
@@ -15,10 +15,10 @@ public final class WcmConstants {
      */
 
     // Properties
-    public static final String PN_NOT_IN_SITEMAP = "notInSitemap";
-    public static final String PN_REFERENCE_PAGE_MAIN_NAVIGATION_BOTTOM = "referencePageMainNavigationBottom";
+    String PN_NOT_IN_SITEMAP = "notInSitemap";
+    String PN_FILE_REFERENCE = "fileReference";
+    String PN_REFERENCE_PAGE_MAIN_NAVIGATION_BOTTOM = "referencePageMainNavigationBottom";
 
     // Resource Type
-    public static final String RT_SUB_REDIRECT_PAGE = "subRedirectPage";
-
+    String RT_SUB_REDIRECT_PAGE = "subRedirectPage";
 }
