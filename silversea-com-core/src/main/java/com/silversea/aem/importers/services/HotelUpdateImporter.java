6d8495159e4beed7dca2929fb2efe8fe00927385
mbennabi <mbennabi@sqli.com>
2017-03-20 11:04:54 +0100

    SSC-1112 Hotel APi refresh

---
+++ silversea-com-core/src/main/java/com/silversea/aem/importers/services/HotelUpdateImporter.java
@@ -0,0 +1,13 @@
+package com.silversea.aem.importers.services;
+
+import java.io.IOException;
+
+import com.day.cq.replication.ReplicationException;
+
+/**
+ * Created by mbennabi on 16/03/2017.
+ */
+public interface HotelUpdateImporter {
+
+    void importUpdateHotel() throws IOException, ReplicationException;
+}
