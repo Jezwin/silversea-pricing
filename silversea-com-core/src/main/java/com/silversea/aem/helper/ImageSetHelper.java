76e7f6cf07b4f811cb84edf402d257d7505c024b
psoeung <psoeung@sqli.com>
2017-03-22 20:00:00 +0100

    SSC-1148 refactor code for variation asset display

---
+++ silversea-com-core/src/main/java/com/silversea/aem/helper/ImageSetHelper.java
@@ -0,0 +1,42 @@
+package com.silversea.aem.helper;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.resource.collection.ResourceCollection;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+
+public class ImageSetHelper extends WCMUsePojo {
+    private List<String> renditionList;
+
+    @Override
+    public void activate() throws Exception {
+        String path = get("path", String.class);
+        renditionList = renditionPathList(path);
+    }
+
+    private List<String> renditionPathList(String setPath) {
+        // Dynamic Media Image Set
+        Resource members = getResourceResolver().resolve(setPath + "/jcr:content/related/s7Set");
+        ResourceCollection membersCollection = members.adaptTo(ResourceCollection.class);
+
+        Iterator<Resource> it = membersCollection.getResources();
+        
+        List<String> renditionList = new ArrayList<String>();
+        while (it.hasNext()) {
+            Resource res = it.next();
+            renditionList.add(res.getPath());
+        }
+        return renditionList;
+    }
+
+    /**
+     * @return the renditionList
+     */
+    public List<String> getRenditionList() {
+        return renditionList;
+    }
+}
