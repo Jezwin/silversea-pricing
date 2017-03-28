e990194e5cc7f06b57008706ca41f4321b7d4bdd
psoeung <psoeung@sqli.com>
2017-03-28 16:13:26 +0200

    SSC-1148 Fix javascript issue on page resize

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-slider.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-slider.js
@@ -2,33 +2,39 @@ $(function(){
     /***************************************************************************
      * Slider
      **************************************************************************/
-    var prevArrowCustom ="<button type='button' data-role='none' class='slick-prev' aria-label='Previous' tabindex='0' role='button'><i class='fa fa-angle-left'></i></button>";
-    var nextArrowCustom ="<button type='button' data-role='none' class='slick-next' aria-label='Next' tabindex='0' role='button'><i class='fa fa-angle-right'></i></button>";
+    var prevArrowCustom ="<button type='button' data-role='none' class='slick-prev' aria-label='Previous' tabindex='0' role='button'><i class='fa fa-angle-left'></i></button>",
+    nextArrowCustom ="<button type='button' data-role='none' class='slick-next' aria-label='Next' tabindex='0' role='button'><i class='fa fa-angle-right'></i></button>",
+    settingSlider = {
+            prevArrow : prevArrowCustom,
+            nextArrow : nextArrowCustom,
+            responsive : [ {
+                breakpoint : 768,
+                settings : {
+                    slidesToShow : 1,
+                    slidesToScroll : 1
+                }
+            } ]
+        };
 
     // On page load
-    if($.viewportDetect() === "xs") {
-        $(".c-slider:not(.c-slider--first-slide-only)").slick({
-            prevArrow: prevArrowCustom,
-            nextArrow: nextArrowCustom
-        });
+    if ($.viewportDetect() === "xs") {
+        $(".c-slider:not(.c-slider--first-slide-only, .c-slider--for, .c-slider--nav)").slick(settingSlider);
     } else {
-        $(".c-slider:not(.c-slider--for, .c-slider--nav)").slick({
-            prevArrow: prevArrowCustom,
-            nextArrow: nextArrowCustom
-        });
+        $(".c-slider:not(.c-slider--for, .c-slider--nav)").slick(settingSlider);
     }
 
     // On page resize
     $("body").on("trigger.viewport.changed", function() {
-        if($.viewportDetect() === "xs") {
+        var $sliderDisabledOnMobile = $(".c-slider.c-slider--first-slide-only");
+
+        if ($.viewportDetect() === "xs") {
             // Disabled slider if the viewport is XS after page resizes
-            $(".c-slider.c-slider--first-slide-only").slick("unslick");
+            $sliderDisabledOnMobile.slick("unslick");
         } else {
             // Enable slider if the viewport is not XS after page resize
-            $(".c-slider.c-slider--first-slide-only").slick({
-                prevArrow: prevArrowCustom,
-                nextArrow: nextArrowCustom
-            });
+            if (!$sliderDisabledOnMobile.hasClass("slick-initialized")) {
+                $sliderDisabledOnMobile.slick(settingSlider);
+            }
         }
     });
 });
