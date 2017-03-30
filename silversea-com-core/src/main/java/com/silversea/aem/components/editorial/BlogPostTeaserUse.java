4e4bed530a44e029a9de0ecb268fd3776923bf1d
Tu Phan <pttu@sqli.com>
2017-03-30 18:32:24 +0200

    SSC-1333 & SSC-1334 : Component "Blog Post list" - Backend
    - Fix FormatFile

--- silversea-com-core/src/main/java/com/silversea/aem/components/editorial/BlogPostTeaserUse.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/editorial/BlogPostTeaserUse.java
@@ -14,45 +14,45 @@ import com.silversea.aem.services.BlogPostService;
  */
 public class BlogPostTeaserUse extends WCMUsePojo {
 
-	private static final String PROPERTY_BLOG_POST_REFERENCE_PARAM = "blogPostReference";
+    private static final String PROPERTY_BLOG_POST_REFERENCE_PARAM = "blogPostReference";
 
-	private static final String DEFAULT_VALUE_TEMPLATE_PATH = "/apps/silversea/silversea-com/templates/blogpost";
+    private static final String DEFAULT_VALUE_TEMPLATE_PATH = "/apps/silversea/silversea-com/templates/blogpost";
 
-	private BlogPostTeaserModel blogTeaser;
+    private BlogPostTeaserModel blogTeaser;
 
-	private BlogPostService blogPostService;
+    private BlogPostService blogPostService;
 
-	@Override
-	public void activate() throws Exception {
-		ValueMap properties = getProperties();
-		try {
-			blogPostService = getSlingScriptHelper().getService(BlogPostService.class);
-			// Map the blogPostReference in Dialog
-			String blogPostReferenceParam = properties.get(PROPERTY_BLOG_POST_REFERENCE_PARAM, String.class);
-			Resource res = getResourceResolver().getResource(blogPostReferenceParam);
-			if (null != res) {
-				if (null != blogTeaser) {
-					Page targetPage = res.adaptTo(Page.class);
-					if (null != targetPage) {
-						blogTeaser = targetPage.adaptTo(BlogPostTeaserModel.class);
-					}
-				} else {
-					blogTeaser = blogPostService
-							.getBlogPostTeaserModelList(blogPostReferenceParam, WcmConstants.DEFAULT_KEY_CQ_TEMPLATE,
-									DEFAULT_VALUE_TEMPLATE_PATH, WcmConstants.DEFAULT_VALUE_ORDER_BY_SORT_DESC)
-							.get(0);
-				}
-			}
-		} finally
+    @Override
+    public void activate() throws Exception {
+        ValueMap properties = getProperties();
+        try {
+            blogPostService = getSlingScriptHelper().getService(BlogPostService.class);
+            // Map the blogPostReference in Dialog
+            String blogPostReferenceParam = properties.get(PROPERTY_BLOG_POST_REFERENCE_PARAM, String.class);
+            Resource res = getResourceResolver().getResource(blogPostReferenceParam);
+            if (null != res) {
+                if (null != blogTeaser) {
+                    Page targetPage = res.adaptTo(Page.class);
+                    if (null != targetPage) {
+                        blogTeaser = targetPage.adaptTo(BlogPostTeaserModel.class);
+                    }
+                } else {
+                    blogTeaser = blogPostService
+                            .getBlogPostTeaserModelList(blogPostReferenceParam, WcmConstants.DEFAULT_KEY_CQ_TEMPLATE,
+                                    DEFAULT_VALUE_TEMPLATE_PATH, WcmConstants.DEFAULT_VALUE_ORDER_BY_SORT_DESC)
+                            .get(0);
+                }
+            }
+        } finally
 
-		{
-			properties = null;
-		}
+        {
+            properties = null;
+        }
 
-	}
+    }
 
-	public BlogPostTeaserModel getBlogTeaser() {
-		return blogTeaser;
-	}
+    public BlogPostTeaserModel getBlogTeaser() {
+        return blogTeaser;
+    }
 
 }
