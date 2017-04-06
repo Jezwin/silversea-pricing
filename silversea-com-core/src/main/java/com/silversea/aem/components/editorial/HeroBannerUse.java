38b92f282d1f7daab57025c158655aaec8273098
psoeung <psoeung@sqli.com>
2017-04-06 20:26:18 +0200

    SSC-912 show button

--- silversea-com-core/src/main/java/com/silversea/aem/components/editorial/HeroBannerUse.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/editorial/HeroBannerUse.java
@@ -1,32 +1,53 @@
 package com.silversea.aem.components.editorial;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
 
 import com.adobe.cq.sightly.WCMUsePojo;
 
 public class HeroBannerUse extends WCMUsePojo {
-    private static final Logger LOGGER = LoggerFactory.getLogger(HeroBannerUse.class);
-
-    Boolean hasButton1;
-    Boolean hasButton2;
+    private Button btn1;
+    private Button btn2;
 
     @Override
     public void activate() throws Exception {
-        LOGGER.error("======> {}", getCurrentPage().getPath());
+        btn1 = getButton("button1");
+        btn2 = getButton("button2");
     }
 
     /**
-     * @return the hasButton1
+     * @return Button
      */
-    public Boolean hasButton1() {
-        return hasButton1;
+    private Button getButton(String node){
+        Button button;
+
+        Resource res = getResourceResolver().getResource(getResource().getPath() + "/" + node);
+        if(res != null) {
+            ValueMap ppties = res.getValueMap();
+            button = new Button(
+                    ppties.get("titleDesktop", String.class),
+                    ppties.get("titleTablet", String.class),
+                    ppties.get("reference", String.class),
+                    ppties.get("color", String.class),
+                    ppties.get("analyticType", String.class),
+                    ppties.get("size", String.class));
+            return button;
+        }
+
+        return null;
     }
 
     /**
-     * @return the hasButton2
+     * @return the btn1
      */
-    public Boolean hasButton2() {
-        return hasButton2;
+    public Button getBtn1() {
+        return btn1;
+    }
+
+    /**
+     * @return the btn2
+     */
+    public Button getBtn2() {
+        return btn2;
     }
 }
