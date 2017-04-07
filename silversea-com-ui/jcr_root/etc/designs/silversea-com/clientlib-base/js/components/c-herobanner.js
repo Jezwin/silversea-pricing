ed4936be46dc25923b0292d2a06327d6cd281c7e
psoeung <psoeung@sqli.com>
2017-04-07 18:20:44 +0200

    SSC-903 set image in background with javascript

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-herobanner.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-herobanner.js
@@ -1,10 +1,10 @@
-$(function(){
+$(function() {
     $(window).on('resize load', function() {
         var $col = $('.c-hero-banner__content');
         var $row = $('.c-hero-banner__row');
         var offsetLeft = $row.offset().left;
 
-        if($.viewportDetect() === 'sm' || $.viewportDetect() === 'md') {
+        if ($.viewportDetect() === 'sm' || $.viewportDetect() === 'md') {
             if (offsetLeft < 60) {
                 $col.css('left', 60 - offsetLeft);
             } else {
@@ -13,5 +13,12 @@ $(function(){
         } else {
             $col.css('left', 'auto');
         }
+    });
+
+    var $imageBannerList = $('.c-hero-banner .o-img');
+    $imageBannerList.each(function() {
+        var $imageBanner = $(this);
+        $imageBanner.closest('.c-hero-banner__image').css('background-image', 'url(' + $imageBanner.prop("currentSrc") + ')');
+        $imageBanner.css('visibility', 'hidden');
     });
 });
