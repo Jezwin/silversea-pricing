b3126251d1a0f44cbee63a3cb7c13d2785a81acb
Anais SIBA <asiba@sqli.com>
2017-04-10 10:58:25 +0200

    ssc-1394: Cookie disclamer anomalie visuel

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-cookie.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-cookie.js
@@ -5,18 +5,17 @@
         /***************************************************************************
          * Show/hide message according to the "cookieMessageDisclaimer" cookie
          **************************************************************************/
-        var hideCookie = function() {
-            $("#c-cookie").attr('data-show', false);
+        var showCookie = function(val) {
+            $("#c-cookie").attr('data-show', val);
         }
         var cookieStatus = document.cookie.match(/cookieMessageDisclaimer=(true||false)/);
-        if (cookieStatus != null && cookieStatus[1] == 'true') {
-            hideCookie();
-        } else {
+        if (cookieStatus == null || (cookieStatus != null && cookieStatus[1] != 'true')) {
+            showCookie(true);
             $('#c-cookie__close').on('click', function() {
                 var today = new Date();
                 today.setDate(today.getDate() + 365);
                 document.cookie = "cookieMessageDisclaimer=true; expires=" + today.toUTCString();
-                hideCookie();
+                showCookie(false);
             });
         }
     });
