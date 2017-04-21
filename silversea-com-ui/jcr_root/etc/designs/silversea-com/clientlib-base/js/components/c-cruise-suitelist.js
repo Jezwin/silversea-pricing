1b9af06fefd60cbf16152875bee0d6ed1a122116
psoeung <psoeung@sqli.com>
2017-04-21 18:54:46 +0200

    SSC-1466 page cruise, suite list : scroll feature

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-cruise-suitelist.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-cruise-suitelist.js
@@ -6,6 +6,10 @@ $(function() {
     $('div[id^="suitelist-collapse"]').on('shown.bs.collapse', function(e) {
         var $item = $('div[id^="suitelist-collapse"]').not($(this));
         $item.prev('.c-suitelist__heading').addClass('opacity');
+
+        $('html, body').animate({
+            scrollTop: $(this).prev('.c-suitelist__heading').offset().top - $('.c-header').height()
+        }, 500);
     }).on('hide.bs.collapse', function(e) {
         var $item = $('div[id^="suitelist-collapse"]').not($(this));
         $item.prev('.c-suitelist__heading').removeClass('opacity');
