0a85b688adf321b1eb698aea28ea39a535adcb53
psoeung <psoeung@sqli.com>
2017-04-27 19:20:44 +0200

    SSC-1466 list suite, dynamization of description tab

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-cruise-suitelist.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-cruise-suitelist.js
@@ -3,15 +3,21 @@ $(function() {
         e.stopPropagation();
     })
 
+    // Re-init Slider inside collapse
+    $('.c-suitelist__heading').on('click', function() {
+        $('.c-slider').slick('unslick').slick();
+        $(window).trigger('resize');
+    })
+
     $('div[id^="suitelist-collapse"]').on('shown.bs.collapse', function(e) {
         var $currentCollapse = $(this);
         var $item = $('div[id^="suitelist-collapse"]').not($currentCollapse);
         $item.prev('.c-suitelist__heading').addClass('opacity');
 
-        var scrollTargetOffsetTop = $.viewportDetect() === 'xs' ? $currentCollapse.offset().top - 20 : $currentCollapse.prev('.c-suitelist__heading').offset().top - 10 ;
-
+        // Scroll page to content
+        var scrollTargetOffsetTop = $.viewportDetect() === 'xs' ? $currentCollapse.offset().top - 20 : $currentCollapse.prev('.c-suitelist__heading').offset().top - 10;
         $('html, body').animate({
-            scrollTop: scrollTargetOffsetTop - $('.c-header').height()
+            scrollTop : scrollTargetOffsetTop - $('.c-header').height()
         }, 500);
     }).on('hide.bs.collapse', function(e) {
         var $item = $('div[id^="suitelist-collapse"]').not($(this));
