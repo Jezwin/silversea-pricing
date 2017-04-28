f686d5c20540779ad6acb0de575bb2b29a63f361
psoeung <psoeung@sqli.com>
2017-04-28 19:25:42 +0200

    SSC-1148 fix modal for image and gallery

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/modal.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/modal.js
@@ -5,7 +5,7 @@ $(function(){
     // Clean modal content on close event
     $(document).on('hide.bs.modal', function(e) {
         // Destroy gallery inside modal
-        $('.c-slider--for, .c-slider--nav').slick('unslick');
+        //$('.c-slider--for, .c-slider--nav').slick('unslick');
 
         $(e.target).removeData('bs.modal');
         var $modalContent = $('body > .modal .modal-content');
@@ -18,10 +18,12 @@ $(function(){
     // Build modal for image
     $('.automatic-modal, .virtual-tour-modal').on('click', function(e) {
         e.preventDefault();
+
         // HTML layout
         var $modalContent = $('<div class="modal-content modal-content--transparent">'
                 + '<div class="modal-header"><button class="close c-btn--close" type="button" data-dismiss="modal" aria-label="Close"></button></div>'
-                + '<div class="modal-body automatic-modal-body"><img class="o-img" /></div>' + '</div>');
+                + '<div class="modal-body automatic-modal-body"><img class="o-img" /></div>'
+                + '</div>');
 
         // Activate Modal
         $($(this).data('target')).modal('show');
@@ -29,7 +31,7 @@ $(function(){
         // Append image inside Modal
         var imagePath = $(this).attr('href');
         $('.modal').on('shown.bs.modal', function(e) {
-            $(this).find('.modal-content').replaceWith($modalContent);
+            $(this).find('.modal-dialog').empty().append($modalContent);
             $(this).find('img').attr('src', imagePath);
         });
     });
@@ -42,27 +44,25 @@ $(function(){
         // Activate Modal
         $($link.data('target')).modal('show');
 
-        // Append gallery inside modal
-        $('.modal').on('shown.bs.modal', function(e) {
-            $(this).find('.modal-content').replaceWith($link.siblings(':hidden').html());
-        });
-    });
+        $($link.data('target')).on('shown.bs.modal', function(e) {
+            var $modal = $(this);
 
-    $('.modal').on('shown.bs.modal', function() {
-        // Build gallery
-        setTimeout(function() {
-            $('.c-slider--for').slick({
+            // Append gallery inside modal
+            $modal.find('.modal-content').replaceWith($link.siblings(':hidden').html());
+
+            // Activate gallery
+            $modal.find('.c-slider--for').slick({
                 slidesToShow : 1,
                 slidesToScroll : 1,
                 asNavFor : '.modal .c-slider--nav'
             });
 
-            $('.c-slider--nav').slick({
+            $modal.find('.c-slider--nav').slick({
                 slidesToShow : 6,
                 slidesToScroll : 1,
                 asNavFor : '.modal .c-slider--for',
                 focusOnSelect : true
             });
-        }, 10);
+        });
     });
 });
