35f86bbcfa8488c58b59e376cc77b171b298dc13
psoeung <psoeung@sqli.com>
2017-04-28 18:06:31 +0200

    SSC-1148 variation asset display, fix gallery modal (issue because of transition.js)

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/modal.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/modal.js
@@ -17,34 +17,35 @@ $(function(){
 
     // Build modal for image
     $('.automatic-modal, .virtual-tour-modal').on('click', function(e) {
-                e.preventDefault();
-                // HTML layout
-                var $modalContent = $('<div class="modal-content modal-content--transparent">'
-                        + '<div class="modal-header"><button class="close c-btn--close" type="button" data-dismiss="modal" aria-label="Close"></button></div>'
-                        + '<div class="modal-body automatic-modal-body"><img class="o-img" /></div>' + '</div>')
-                        
-                // Activate Modal
-                $($(this).data('target')).modal('show');
-                
-                // Append image inside Modal
-                var imagePath = $(this).attr('href');
-                $('.modal').on('shown.bs.modal', function(e) {
-                    $(this).find('.modal-content').replaceWith($modalContent);
-                    $(this).find('img').attr('src', imagePath);
-                });
-            });
+        e.preventDefault();
+        // HTML layout
+        var $modalContent = $('<div class="modal-content modal-content--transparent">'
+                + '<div class="modal-header"><button class="close c-btn--close" type="button" data-dismiss="modal" aria-label="Close"></button></div>'
+                + '<div class="modal-body automatic-modal-body"><img class="o-img" /></div>' + '</div>');
+
+        // Activate Modal
+        $($(this).data('target')).modal('show');
+
+        // Append image inside Modal
+        var imagePath = $(this).attr('href');
+        $('.modal').on('shown.bs.modal', function(e) {
+            $(this).find('.modal-content').replaceWith($modalContent);
+            $(this).find('img').attr('src', imagePath);
+        });
+    });
 
     // Build modal fragment for Gallery
     $('.automatic-gallery-modal').on('click', function(e) {
         e.preventDefault();
         var $link = $(this);
 
+        // Activate Modal
         $($link.data('target')).modal('show');
-        var $modalContent = $('.modal-content:visible');
-        var $modal = $('.modal:visible');
 
         // Append gallery inside modal
-        $modalContent.replaceWith($link.siblings(':hidden').html());
+        $('.modal').on('shown.bs.modal', function(e) {
+            $(this).find('.modal-content').replaceWith($link.siblings(':hidden').html());
+        });
     });
 
     $('.modal').on('shown.bs.modal', function() {
