94fe116131e887adbc5a6ad3bdd89b278241b2e8
psoeung <psoeung@sqli.com>
2017-06-02 15:14:50 +0200

    SSC-1466 cruise page get path of the request quote path

---
+++ silversea-com-core/src/main/java/com/silversea/aem/utils/PathUtils.java
@@ -0,0 +1,35 @@
+package com.silversea.aem.utils;
+
+import java.util.Locale;
+
+import org.apache.sling.api.resource.Resource;
+
+import com.adobe.granite.confmgr.Conf;
+
+public class PathUtils {
+
+    /**
+     * Constructor.
+     */
+    private PathUtils() {
+
+    }
+
+    /**
+     * Return local path to request quote page path from /conf
+     */
+    public static String getRequestQuotePagePath(Resource resource, Locale locale) {
+        String pagePath;
+        // Get width from configuration
+        Resource confRes = resource.adaptTo(Conf.class).getItemResource("/requestquotepage/page");
+
+        if (confRes != null) {
+            pagePath = "/content/silversea-com/" + locale.getLanguage() + confRes.getValueMap().get("reference", String.class);
+            if (resource.getResourceResolver().getResource(pagePath) != null) {
+                return pagePath;
+            }
+        }
+
+        return null;
+    }
+}
