0a2958d231ccbc0af8f91851ca58e9a462b38154
mstalmoudi <mstalmoudi@sqli.com>
2017-06-09 17:54:12 +0200

    SSC-924 sigun offers - install plugin icheck, css && valadiadtion check box && choice list

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-signupforoffers.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-signupforoffers.js
@@ -1,6 +1,25 @@
 $(function() {
 
     $('.modal').on('shown.bs.modal', function (){
+		
+			$(".chosen").chosen();
+            $(".chosen").chosen();
+
+            $('.c-signupforoffer .c-accept').iCheck({
+                checkboxClass: 'icheckbox_minimal',
+                _checked: 'uncheck',
+            });
+        
+            $('.c-signupforoffer [type="submit"]').prop("disabled", true);
+        
+            $('.c-signupforoffer .c-accept').on('ifChanged', function(event){
+                if($(".c-accept").is(":checked")) {
+                    $('.c-signupforoffer [type="submit"]').prop("disabled", false);
+                 }else if($(".checkbox").is(":not(:checked)")) {
+                    $('.c-signupforoffer [type="submit"]').prop("disabled", true);
+                 }
+           
+            });
         
             setTimeout(function(){
                 var userInfo = JSON.parse($.CookieManager.getCookie('userInfo'))
