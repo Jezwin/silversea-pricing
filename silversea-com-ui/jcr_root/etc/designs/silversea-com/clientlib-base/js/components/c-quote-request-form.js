095aef366474197a4e7860047511cd3a5c5f379c
psoeung <psoeung@sqli.com>
2017-06-12 15:03:42 +0200

    SSC-922 javascript done for tel country selector

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-quote-request-form.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-quote-request-form.js
@@ -1,6 +1,5 @@
 $(function() {
-    var phoneActivated = false;
-
+     // Autogrow Textarea
     // Autogrow Textarea
     var textContainer, textareaSize, input;
     var autoSize = function() {
@@ -12,129 +11,50 @@ $(function() {
     autoSize();
     input.addEventListener('input', autoSize);
 
-    // Desktop toggle Country Code
-    (function countryCodeToggle() {
-        var raqm = $('#raqm');
-        var countryCodeSelector = $('.raqm__countrycode_selector');
-        var countryCodeContainer = $('#countryCodeContainer');
-        var countryCode = $('#countryCode');
-        var flag = $('.flag_wrapper .flag');
-        var countrycode_wrapper = $('.countrycode_wrapper > span');
-        if (raqm.length) {
-            countryCodeSelector.click(function(event) {
-                countryCodeContainer.toggle();
-                countryCodeSelector.toggleClass('active');
-                $('body').trigger('click');
-                $('#countryCode').trigger('chosen:open');
-                event.stopPropagation();
-            });
-
-            $('#countryCode').on('chosen:hiding_dropdown', function() {
-                setTimeout(function() {
-                    if (!$('#countryCode_chosen').hasClass('chosen-container-active'))
-                        $('#countryCodeContainer').hide()
-                }, 50);
-            });
-
-            countryCode.on('change', function() {
-                var val = this.options[this.selectedIndex].text;
-                var code = val.substr(val.indexOf("("));
-                flag.removeAttr('class');
-                flag.addClass('flag flag-' + this.value.toLowerCase());
-                countrycode_wrapper.html(code);
-                countryCodeContainer.toggle();
-                countryCodeSelector.toggleClass('active');
-            });
-        }
-    })();
-
-    (function() {
-        var submitFunc = function() {
-            $('#requestQuoteForm').find('input').each(function() {
-                $(this).blur();
-            });
-
-            $('#requestQuoteForm #titleContainer select').trigger('change');
-
-            var container = $('#formContainer');
-            if ($('#raqm #requestQuoteForm .valid_success .valid_icon').length == 5) {
-                updateForm(container.find('*').serialize());
-            }
-        };
-
-        var form = $('#requestQuoteForm');
-        form.find('button.fire-modal').on('click', function(event) {
-            submitFunc(form);
-        });
-
-        var newRaqForm = $('#requestQuoteForm');
-
-        newRaqForm.find('input').on('blur', function(event) {
-            var inputTarget = $('#' + event.target.id);
-
-            if (inputTarget.attr('type') == 'hidden') {
-                return false;
-            }
-
-            var parent = inputTarget.parent();
-
-            if (event.target.id == 'InputTelephoneNumber') {
-                parent = parent.parent();
-            }
-
-            parent.addClass('validating');
-
-            var realtimeValid = parent.find('.valid_icon');
-            var realtimeinValid = parent.find('.invalid_icon');
-            var realtimeError = parent.find('.form-helper-error-text.live');
-
-            if (event.target.id == 'InputTelephoneNumber' || event.target.id == 'InputEmail') {
-                var realtimeEmptyError = parent.find('.form-helper-error-text.live.empty');
-                var realtimeInvalidError = parent.find('.form-helper-error-text.live.incorrect');
-            }
-
-            parent.find('.form-helper-error-text:not(.live)').hide();
-
-            var value = inputTarget.val();
-        });
-    })();
-
-    // init intl Tel Input Plugin
-    $("#InputTelephoneNumber").intlTelInput();
-
-    $('#countryCode').change(function() {
-        $("#InputTelephoneNumber").intlTelInput("setCountry", $(this).val());
-        if ($("#InputTelephoneNumber").val() != '' || $("#InputEmail").val() != '') {
-            $("#InputTelephoneNumber").blur();
-        }
+    // Init intl Tel Input Plugin
+    $('#InputTelephoneNumber').intlTelInput({
+        allowDropdown : false,
+        // geoIpLookup: function(callback) {
+        // $.get("http://ipinfo.io", function() {},
+        // "jsonp").always(function(resp) {
+        // var countryCode = (resp && resp.country) ? resp.country : "";
+        // callback(countryCode);
+        // });
+        // },
+        // initialCountry: 'auto',
+        separateDialCode : true
     });
 
-    $("#InputTelephoneNumber").intlTelInput("setCountry", $('#countryCode').val());
+    $('.countrycode').each(function() {
+        $countryCode = $(this);
 
-    if ($('#countryCode').val() != '') {
-        $('#countryCode').trigger('change');
-        $('.raqm__countrycode_selector').trigger('click');
-        $("#InputTelephoneNumber").intlTelInput("setCountry", $('#countryCode').val());
-        if ($("#InputTelephoneNumber").val() == '') {
-            var inputTarget = $("#InputTelephoneNumber");
-            var parent = inputTarget.parent().parent();
-            var realtimeinValid = parent.find('.invalid_icon');
-            var realtimeEmptyError = parent.find('.form-helper-error-text.live.empty');
-            var realtimeInvalidError = parent.find('.form-helper-error-text.live.incorrect');
-            realtimeEmptyError.hide();
-            realtimeInvalidError.hide();
-            realtimeinValid.hide();
-            inputTarget.removeClass('form-helper-error-field');
-        }
-    }
+        $countryCode.find('#countryCode').on('change', function() {
+            $("#InputTelephoneNumber").intlTelInput("setCountry", $(this).val());
 
-    if ($('#requestQuoteForm #titleContainer select').val() != '') {
-        $('#requestQuoteForm #titleContainer select').trigger('change');
-    }
+            if ($('#InputTelephoneNumber').val() !== '') {
+                $('#InputTelephoneNumber').blur();
+            }
+        });
 
-    $('#requestQuoteForm').find('input').each(function() {
-        if ($(this).val() != '') {
-            $(this).blur();
-        }
+        // Active intlTelInput plugin (combo box with flag)
+        $countryCode.find('#InputTelephoneNumber').intlTelInput('setCountry', $('#countryCode').val());
+
+        // Trigger open chosen event on flag click
+        var openChosen = (function openChosen() {
+            $countryCode.find('.flag-container').on('click', function(event) {
+                event.stopPropagation();
+                $countryCode.find('#countryCode').trigger('chosen:open');
+
+                // Unbind click on flag
+                $countryCode.find('.flag-container').off('click');
+            });
+
+            return openChosen;
+        }());
+
+        // Bind click on flag
+        $countryCode.find('#countryCode').on('chosen:hiding_dropdown', function() {
+            openChosen();
+        })
     });
 });
