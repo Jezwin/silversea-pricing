6df5b1ab2a10cc9b57af67654b8148b1b5db9aaf
mstalmoudi <mstalmoudi@sqli.com>
2017-06-12 19:13:06 +0200

    SSC-924- sign uo offers is_booked validation

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-signupforoffers.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-signupforoffers.js
@@ -1,28 +1,29 @@
 $(function() {
-
+    'use strict';
     $('.modal').on('shown.bs.modal', function (){
-		
+        
             $(".chosen").chosen();
-
-            $('.c-signupforoffer .c-accept').iCheck({
+        
+            $('.c-signupforoffer [name="isbooked"]').iCheck({
                 checkboxClass: 'icheckbox_minimal',
-                _checked: 'uncheck',
             });
-        
-            $('.c-signupforoffer [type="submit"]').prop("disabled", true);
-        
-            $('.c-signupforoffer .c-accept').on('ifChanged', function(event){
-                if($(".c-accept").is(":checked")) {
-                    $('.c-signupforoffer [type="submit"]').prop("disabled", false);
-                 }else if($(".checkbox").is(":not(:checked)")) {
-                    $('.c-signupforoffer [type="submit"]').prop("disabled", true);
-                 }
-           
-            });
-        
+    
             setTimeout(function(){
                 var userInfo = JSON.parse($.CookieManager.getCookie('userInfo'))
-                $('.c-signupforoffer [name="email"]').val(userInfo.email);
+                $('.c-signupforoffer [name="title"]').val( userInfo.title ).trigger('chosen:updated');
+                $('.c-signupforoffer [name="firstname"]').val(userInfo.firstname);
+                $('.c-signupforoffer [name="lastname"]').val(userInfo.lastname); 
+                $('.c-signupforoffer [name="email"]').val(userInfo.email); 
+                    
+                if( userInfo.isbooked == 1 ){
+                    $('.c-signupforoffer [name="isbooked"]').iCheck('check');
+                }
+                
+                /*$('.c-signupforoffer [name="isbooked"]').on('ifChecked ifUnchecked', function(event){
+                    var booked = (event.type == 'ifUnchecked') ? 0 : 1 ;
+                    $('.c-signupforoffer [name="isbooked"]').val(booked);
+                });*/
+                
             },500);
             
             var opt = {
@@ -31,15 +32,13 @@ $(function() {
                   error: 'error'
                 }
             };
-            
             $('.c-formcookie').validator(opt)
                 .on('submit', function(e) {
 
                     if (!e.isDefaultPrevented()) {
-                        $.signUp.signUpOffers(this, e);                    
+                        $.signUp.signUpOffers(this, e);
                     }
 
              });
-        
     });
 });
--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/main.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/main.js
@@ -58,39 +58,47 @@ $(function() {
     $.signUp = {
         signUpOffers: function (elem, event) {
             
-            var cookieValues = [ 'title','firstname', 'lastname', 'email', 'phone', 'description' ];
+            var cookieValues = [ 'title','firstname', 'lastname', 'email', 'phone', 'description', 'isbooked' ];
             
             var pos = document.cookie.indexOf( "userInfo=" );
-            if( pos <= 0){        
+            if( pos <= 0){  
                 $.CookieManager.setCookie('userInfo', JSON.stringify(cookieValues) );
             }
                                        
             var leadApiData = {},
                 currentData = JSON.parse($.CookieManager.getCookie('userInfo'));
+                
             
             for (var i in cookieValues) {
-
                 if (elem[cookieValues[i]] && elem[cookieValues[i]].value !== undefined) {
                     leadApiData[cookieValues[i]] = elem[cookieValues[i]].value;
+                    
+                    if( elem[cookieValues[i]].name == "isbooked" ){ 
+                        var checkbox = document.querySelector('.c-signupforoffer [name="isbooked"]');
+                        leadApiData[elem[cookieValues[i]].name] = checkbox.checked;
+                    }
                 }
             }
             
-  
-            event.preventDefault();
             $.ajax({
                 type : "POST",
-                url : "/content/silversea/aa.lead.json",
+                url : "/content/silversea/cc.lead.json",
                 data : JSON.stringify(leadApiData),
                 contentType : "application/json",
                 dataType : "json",
                 success : function(data) {
                     var obj = {};
+                    
                     cookieValues.forEach(function(dat, index){
-                         obj[cookieValues[index]] = '';
+                        if( currentData[dat] != undefined ){
+                            obj[cookieValues[index]] = currentData[dat];
+                        }
                     });
                     currentData = Object.assign(obj, leadApiData);
                     $.CookieManager.setCookie('userInfo', JSON.stringify(currentData));
-                    $.CookieManager.setCookie('api_indiv_id', data);
+                    if (elem.className.match(/c-formcookie--redirect/) !== null) {
+                        $.CookieManager.setCookie('api_indiv_id', data);
+                    }
                     
                 },
                 failure : function(errMsg) {
@@ -100,7 +108,7 @@ $(function() {
             
             if (elem.className.match(/c-formcookie--redirect/) !== null) {
                 //event.preventDefault();
-                setTimeout(function(){ window.location.href = elem.action; }, 1000);
+                setTimeout(function(){ window.location.href = elem.action; }, 1000000000);
             } else if (elem.className.match(/c-formcookie--modal/) !== null) {
                 event.preventDefault();
                 var target = elem.dataset.target;
