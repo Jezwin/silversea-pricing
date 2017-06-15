460e8314b00ba5debf8a8474aa4e1075136cdfa5
psoeung <psoeung@sqli.com>
2017-06-15 15:58:51 +0200

    SSC-924 add comment, clean code

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-signupforoffers.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-signupforoffers.js
@@ -6,10 +6,12 @@ $(function() {
         $(this).find('.c-signupforoffer').each(function() {
             var $signUpForm = $(this);
 
+            // Call plugin to custom check box look and feel
             $signUpForm.find('.custom-checkbox').iCheck({
                 checkboxClass : 'icheckbox_minimal',
             });
 
+            // Autocomplete form with data from cookie
             var userInfo = JSON.parse($.CookieManager.getCookie('userInfo'));
             $signUpForm.find('[name="title"]').val(userInfo.title).trigger('chosen:updated');
             $signUpForm.find('[name="firstname"]').val(userInfo.firstname);
@@ -19,21 +21,20 @@ $(function() {
             // if( userInfo.isbooked == 1 ){
             // $signUpForm.find('[name="isbooked"]').iCheck('check'); }
 
-            var opt = {
+            // Call plugin validator
+            $('.c-formcookie').validator({
                 focus : false,
                 feedback : {
                     success : 'feedback-success',
                     error : 'feedback-error'
                 }
-            };
-
-            $('.c-formcookie').validator(opt).on('submit', function(e) {
+            }).on('submit', function(e) {
                 if (!e.isDefaultPrevented()) {
                     $.signUp.signUpOffers(this, e);
                 }
             });
 
-            // Launch plugin after modal content loaded.
+            // Call plugin after validator plugin
             $('.chosen').chosen();
         });
     });
