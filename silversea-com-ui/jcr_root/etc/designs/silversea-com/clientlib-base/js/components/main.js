6965efb07c08bfb6ec2a480eb638c445e7be6a0f
psoeung <psoeung@sqli.com>
2017-06-19 16:09:35 +0200

    SSC-922 add missing data in form post

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/main.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/main.js
@@ -70,7 +70,7 @@ $(function() {
             // Cancel synchrone submit
             event.preventDefault();
 
-            var cookieValues = [ 'title', 'firstname', 'lastname', 'email', 'phone', 'description', 'requestDetails', 'subscribe', 'already_travelling' ];
+            var cookieValues = [ 'title', 'firstname', 'lastname', 'email', 'phone', 'comments', 'requestsource', 'att02', 'workingwithagent' ];
             var pos = document.cookie.indexOf("userInfo=");
 
             // Set cookie if not created
