9bd0bd960c6c50a44e2496246dc683ee7ee3dbe6
psoeung <psoeung@sqli.com>
2017-06-26 19:31:54 +0200

    SCC-1629 create function init video

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-video.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-video.js
@@ -1,17 +1,22 @@
-$(function(){
-    $('.c-video').each(function() {
-        var $video = $(this);
-        var asset = $video.data('asset');
-        var s7videoviewer = new s7viewers.VideoViewer({
-            'containerId' : $video.attr('id'),
-            'params' : { 
-                'serverurl' : window.location.origin + '/is/image',
-                'contenturl' : 'https://silversea.assetsadobe.com/',
-                'config' : '/etc/dam/presets/viewer/Video',
-                'videoserverurl': 'https://gateway-eu.assetsadobe.com/DMGateway/public/silversea',
-                'posterimage': asset,
-                'asset' : asset
-            }
-        }).init();
-    });
+$(function() {
+    $.fn.initVideo = function() {
+        this.each(function() {
+            var $video = $(this);
+            var asset = $video.data('asset');
+            var s7videoviewer = new s7viewers.VideoViewer({
+                'containerId' : $video.attr('id'),
+                'params' : {
+                    'serverurl' : window.location.origin + '/is/image',
+                    'contenturl' : 'https://silversea.assetsadobe.com/',
+                    'config' : '/etc/dam/presets/viewer/Video',
+                    'videoserverurl' : 'https://gateway-eu.assetsadobe.com/DMGateway/public/silversea',
+                    'posterimage' : asset,
+                    'asset' : asset
+                }
+            }).init();
+        });
+    };
+
+    // Init video on page load
+    $('.c-video').initVideo();
 });
