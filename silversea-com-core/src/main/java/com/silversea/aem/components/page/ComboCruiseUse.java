c4bc15dd8ec5c5cef29efb536a603b411f012740
lymendoza <lymendoza@sqli.com>
2017-06-30 15:43:55 +0200

    Configured Request a Quote buttons for combo cruise

--- silversea-com-core/src/main/java/com/silversea/aem/components/page/ComboCruiseUse.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/page/ComboCruiseUse.java
@@ -4,15 +4,16 @@ import com.adobe.cq.sightly.WCMUsePojo;
 import com.silversea.aem.components.beans.GeoLocation;
 import com.silversea.aem.models.ComboCruiseModel;
 import com.silversea.aem.services.GeolocationService;
+import com.silversea.aem.utils.PathUtils;
 
 public class ComboCruiseUse extends WCMUsePojo {
 
     private GeolocationService geolocationService;
     private ComboCruiseModel comboCruiseModel;
-    
+
     @Override
     public void activate() throws Exception {
-        
+
         geolocationService = getSlingScriptHelper().getService(GeolocationService.class);
         GeoLocation geoLocation = geolocationService.initGeolocation(getRequest());
         comboCruiseModel = getCurrentPage().adaptTo(ComboCruiseModel.class);
@@ -22,5 +23,12 @@ public class ComboCruiseUse extends WCMUsePojo {
     public ComboCruiseModel getComboCruiseModel() {
         return comboCruiseModel;
     }
-    
+
+    /**
+     * Return path for request quote page
+     */
+    public String getRequestQuotePagePath() {
+        return PathUtils.getRequestQuotePagePath(getResource(), getCurrentPage().getLanguage(false));
+    }
+
 }
