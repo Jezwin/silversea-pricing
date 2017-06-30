e0be798654c9056c7acf38f1474cdb627db981dd
psoeung <psoeung@sqli.com>
2017-06-30 13:00:50 +0200

    SSC-1484 - Combo Cruise component - CC Routes, add delay between tab change and scroll to content

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-combo-cruise.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-combo-cruise.js
@@ -10,22 +10,29 @@ $(function() {
         // init slider inside current tab content
         var $currentSlider;
         $tab.on('shown.bs.tab', function() {
-            $currentSlider = $($(this).attr('href')).find('.c-slider')
+            var $panel = $($(this).attr('href'));
+            $currentSlider = $panel.find('.c-slider')
             $currentSlider.slick('unslick').slick(settingSlider);
+
+            // Scroll to panel
+            $('html').delay(500).animate({
+                scrollTop: $panel.offset().top - $('.c-header').height() - 24
+            }, 500);
         });
     })
 
     // Overview tab : open suite/route tab for the current item
     $('.c-combo-cruise #overview [data-tab-target]').on('click', function() {
         var $trigger = $(this);
-        // Open route tab
-        var tabId = $trigger.data('tab-target');
-        $('a[href="' + tabId + '"]').closest('li').trigger('click');
 
-        // scroll to tab
-        $('html').stop().animate({
+        // scroll to "top" page
+        $('html, body').stop().animate({
             scrollTop: $('.c-combo-cruise .c-combo-cruise-tab').offset().top - $('.c-header').height() - 24
-        }, 500, function() {
+        }, 0, function() {
+            // Open tab
+            var tabId = $trigger.data('tab-target');
+            $('a[href="' + tabId + '"]').closest('li').trigger('click');
+
             if (tabId == '#suitenfare') {
                 // Open current suite only (force close others suite)
                 $(tabId).find('.panel:eq(' + $trigger.index() + ')').find('[role="tab"]').trigger('click');
