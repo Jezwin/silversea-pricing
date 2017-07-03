f96afd2a0d024e5cd2110cb169beaf4b6c3ef279
psoeung <psoeung@sqli.com>
2017-07-03 14:23:03 +0200

    SSC-862 navigation, change backdrop animation and use toggleSlide javascript method instead of maxHeight css animation

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/burgernav.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/burgernav.js
@@ -6,38 +6,46 @@
         this.isActive = false;
 
         /*
-         ** Toggled Classes to the Navigation
+         * Toggled Classes to the Navigation
          */
         this.toggledClass = function() {
             _self.upActivity();
 
-            _self.parent().toggleClass("is-active");
-            $('.c-main-nav').toggleClass("c-main-nav--open");
-            $('.c-main-nav__backdrop').toggleClass("c-main-nav__backdrop--is-active");
-            $('body').toggleClass("c-main-nav--body-fixed");
-            $('.c-header').toggleClass("c-main-nav--nav-expanded");
+            _self.parent().toggleClass('is-active');
+            $('body').toggleClass('c-main-nav--body-fixed');
+            $('.c-header').toggleClass('c-main-nav--nav-expanded');
+            $('.c-main-nav').stop().slideToggle(350, function() {
+                var $mainNav = $(this);
+
+                // Remove inline display value after animation
+                if ($mainNav.css('display') == 'none') {
+                    $mainNav.css('display', '');
+                }
+            });
+            $('.c-main-nav__backdrop').toggleClass('c-main-nav__backdrop--is-active');
         }
 
         /*
-         ** Update the status of navigation
+         * Update the status of navigation
          */
         this.upActivity = function() {
             _self.isActive = !_self.isActive;
 
-            if (!_self.isActive)
+            if (!_self.isActive) {
                 _self.cleanListener();
+            }
         }
 
         /*
-         ** Clean listener for performance
+         * Clean listener for performance
          */
         this.cleanListener = function() {
-            $(window).off("resize");
-            $('.c-main-nav__backdrop').off("click");
+            $(window).off('resize');
+            $('.c-main-nav__backdrop').off('click');
         }
 
         /*
-         ** Click Event to show / hide navigation
+         * Click Event to show / hide navigation
          */
         this.click(function() {
             _self.toggledClass();
@@ -47,6 +55,7 @@
                     _self.toggledClass();
                 });
 
+                // Close navigation on window resize if viewport is not XS/SM
                 $(window).on('resize', function() {
                     if ($(this).width() >= 992 && _self.isActive) {
                         _self.toggledClass();
