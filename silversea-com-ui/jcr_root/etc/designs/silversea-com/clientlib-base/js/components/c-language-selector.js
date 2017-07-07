a0d034e88b5a918e448494b77269889e0e371e96
psoeung <psoeung@sqli.com>
2017-07-07 10:54:46 +0200

    SSC-1414 close language selector on page click

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-language-selector.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-language-selector.js
@@ -1,29 +1,46 @@
 $(function() {
-    var $trigger = $('.open-language-selector');
-    var $languageSelector = $('#c-language-selector');
-    var $triggerContainer = $trigger.closest('.c-main-nav__top');
+    $.fn.initLanguageSelector = function() {
+        var $trigger = $('.open-language-selector'),
+        $languageSelector = $('#c-language-selector'),
+        $triggerContainer = $trigger.closest('.c-main-nav__top');
 
-    OpenLanguageSelector();
-
-    $('body').on('trigger.viewport.changed', function() {
         OpenLanguageSelector();
-    });
 
-    // Open language selector according to the viewport
-    function OpenLanguageSelector() {
-        // "sm" <==> tablet viewport
-        if ($.viewportDetect() == 'sm') {
-            if (!$('.c-language-selector__list.cloned').length) {
-                $languageSelector.clone().addClass('cloned').attr('id', 'c-language-selector-cloned').appendTo('.c-main-nav__top');
+        $('body').on('trigger.viewport.changed', function() {
+            OpenLanguageSelector();
+        });
+
+        // Open language selector according to the viewport
+        function OpenLanguageSelector() {
+            // "sm" <==> tablet viewport
+            if ($.viewportDetect() == 'sm') {
+                if (!$('.c-language-selector__list.cloned').length) {
+                    $languageSelector.clone().addClass('cloned').attr('id', 'c-language-selector-cloned').appendTo('.c-main-nav__top');
+                }
             }
+
+            $trigger.on('click', function(e) {
+                e.preventDefault();
+                var $collapsible = $('#c-language-selector-cloned, #c-language-selector');
+                $collapsible.collapse('toggle');
+                $(this).attr('aria-expanded', $collapsible.attr('aria-expanded'));
+            })
         }
 
-        $trigger.on('click', function(e) {
-            e.preventDefault();
+        this.closeLanguageSelector = function() {
+            $('#c-language-selector').collapse('hide');
+        };
 
-            var $collapsible = $('#c-language-selector-cloned, #c-language-selector');
-            $collapsible.collapse('toggle');
-            $(this).attr('aria-expanded', $collapsible.attr('aria-expanded'));
-        })
-    }
+        return this
+    };
+
+    $.fn.initLanguageSelector();
+
+    // force close language selector on click on the screen.
+    $(document).on('click', function(e) {
+        e.stopPropagation();
+        if ($(e.target).parents('.c-language-selector__list').length === 0) {
+            $.fn.initLanguageSelector().closeLanguageSelector();
+        }
+    });
 });
