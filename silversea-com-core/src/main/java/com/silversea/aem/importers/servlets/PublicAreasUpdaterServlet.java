dfec34717bf9ce63be3f4c62605097b6cc702f9c
Aurélien Olivier <auolivier@sqli.com>
2017-07-09 23:39:45 +0200

    Imports review

---
+++ silversea-com-core/src/main/java/com/silversea/aem/importers/servlets/PublicAreasUpdaterServlet.java
@@ -0,0 +1,35 @@
+package com.silversea.aem.importers.servlets;
+
+import com.silversea.aem.importers.utils.MigrationUtils;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * @author aurelienolivier
+ */
+@SlingServlet(paths = "/bin/publicareas-updater", extensions = "html")
+public class PublicAreasUpdaterServlet extends SlingSafeMethodsServlet {
+
+    @Reference
+    private ResourceResolverFactory resourceResolverFactory;
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
+        final String query = "/jcr:root/content/silversea-com//element(*,cq:Page)" +
+                "[jcr:content/sling:resourceType=\"silversea/silversea-com/components/pages/publicarea\"]";
+
+        final String assetsQuery = "/jcr:root/content/dam/silversea-com/onboard/public-areas"
+                + "//element(*,dam:Asset)[jcr:content/metadata/initialPath]";
+
+        MigrationUtils.updatePagesAfterMigration(resourceResolverFactory,
+                query, assetsQuery, Arrays.asList("assetSelectionReferenceImported"), null);
+    }
+}
