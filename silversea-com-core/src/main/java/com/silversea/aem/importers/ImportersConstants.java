3f108ef75d7c818d894f1cd0afbbffb869a5d03f
Aurelien Olivier <auolivier@sqli.com>
2017-07-14 01:29:06 +0200

    Cleaning unused constants

--- silversea-com-core/src/main/java/com/silversea/aem/importers/ImportersConstants.java
+++ silversea-com-core/src/main/java/com/silversea/aem/importers/ImportersConstants.java
@@ -7,23 +7,6 @@ public interface ImportersConstants {
 
     String SILVERSEA_ROOT = "/content/silversea-com";
 
-    String BASEPATH_PORTS = "/content/silversea-com/en/other-resources/find-a-port";
-
-    String BASEPATH_TRAVEL_AGENCIES = "/content/silversea-com/en/other-resources/find-a-travel-agent";
-
-    String BASEPATH_SPECIAL_OFFERS = "/content/silversea-com/en/exclusive-offers";
-
-    String BASEPATH_SHIP = "/content/silversea-com/en/ships";
-
-    String BASEPATH_FEATURES = "/content/silversea-com/en/features";
-
-    String BASEPATH_BROCHURES = "/content/dam/siversea-com/brochures";
-
-    String BASEPATH_CRUISES = "/content/silversea-com/en/destinations/";
-
-    // Just Add to test
-    String BASEPATH_COUNTRY = "/content/silversea-com/en/country";
-
     String API_DOMAIN = "https://shop.silversea.com";
 
     //Sub service name
@@ -32,7 +15,6 @@ public interface ImportersConstants {
     // Templates
     String CRUISE_TEMPLATE = "/apps/silversea/silversea-com/templates/cruise";
     String CRUISE_SEGEMENT_TEMPLATE = "/apps/silversea/silversea-com/templates/combosegment";
-    String COMBO_CRUISE_TEMPLATE = "/apps/silversea/silversea-com/templates/combocruise";
 
     //Resources type
     String COMBO_CRUISE_RESOURCE_TYPE = "silversea/silversea-com/components/pages/combocruise";
@@ -47,7 +29,6 @@ public interface ImportersConstants {
     String PRICE_WAITLIST = "Waitlist:";
 
     String CRUISES_DAM_PATH = "/content/dam/silversea-com/api-provided/cruises/";
-    String CRUISES_PAGE_RESOURCE_TYPE = "silversea/silversea-com/components/pages/cruise";
     String QUERY_JCR_ROOT_PATH = "/jcr:root";
     String QUERY_CONTENT_PATH = "/jcr:root/content/silversea-com/en";
     String QUERY_TAGS_PATH = "/jcr:root/etc/tags";
@@ -60,14 +41,5 @@ public interface ImportersConstants {
     String LOWEST_PRICES_NODE = "lowest-prices";
     String CRUISES_DESTINATIONS_URL_KEY = "cruisesUrl";
 
-    //API
-    String VOYAGE_URL_KEY = "voyageUrl";
-    String VOYAGE_SPECIAL_OFFERS_URL_KEY = "voyageSpecialOffersUrl";
-    String VOYAGE_LAND_ADVENTURE_URL_KEY = "landAdventuresUrl";
-    String VOYAGE_ITINERARY_HOTELS_URL_KEY = "itineraryHotelsUrl";
-    String VOYAGE_ITINERARY_EXCURSIONS_URL_KEY = "itineraryExcursionsUrl";
-    String VOYAGE_PRICES_URL_KEY = "pricesUrl";
-    String SPECIAL_VOYAGE_URL_KEY = "specialVoyagesUrl";
-    
     String LANGUAGE_EN = "en";
 }
