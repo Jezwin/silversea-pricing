d99cf7b1e52dbf0b8664038a66c83d13a426c644
Aurelien Olivier <auolivier@sqli.com>
2017-07-17 10:53:03 +0200

    Adding API base path configuration

--- silversea-com-core/src/main/java/com/silversea/aem/services/impl/ApiConfigurationServiceImpl.java
+++ silversea-com-core/src/main/java/com/silversea/aem/services/impl/ApiConfigurationServiceImpl.java
@@ -11,19 +11,19 @@ import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component(immediate = true, label = "Silversea.com - Api importer Configuration", metatype = true)
+@Component(immediate = true, label = "Silversea - Api importer Configuration", metatype = true)
 @Service(value = ApiConfigurationService.class)
 public class ApiConfigurationServiceImpl implements ApiConfigurationService {
 
     static final private Logger LOGGER = LoggerFactory.getLogger(ApiConfigurationServiceImpl.class);
 
-    private static final String DEFAULT_API_DOMAIN = "https://shop.silversea.com";
+    private static final String DEFAULT_API_BASE_PATH = "http://shop.silversea.com/apit";
     private static final String DEFAULT_LOGIN = "auolivier@sqli.com";
     private static final String DEFAULT_PASSWORD = "123qweASD";
 
-    @Property(value = DEFAULT_API_DOMAIN, label = "Api Domain", description = "Api domain of swagger")
-    private static final String API_DOMAIN = "apiDomain";
-    private String apiDomain;
+    @Property(value = DEFAULT_API_BASE_PATH, label = "API Base path", description = "API base path, Swagger format http://shop.silversea.com/apit")
+    private static final String API_BASE_PATH = "apiBasePath";
+    private String apiBasePath;
 
     @Property(value = DEFAULT_LOGIN, label = "login swagger", description = "login swagger")
     private static final String LOGIN = "login";
@@ -34,30 +34,7 @@ public class ApiConfigurationServiceImpl implements ApiConfigurationService {
     private String password;
 
     // TODO review naming
-    @Property(description = "SSC API Paths", value = {
-            "shipUrl:/api/v1/ships",
-            "brochureUrl:/api/v1/brochures",
-            "featuresUrl:/api/v1/features",
-            "contriesUrl:/api/v1/countries",
-            "spetialOffersUrl:/api/v1/specialOffers",
-            "landProgramUrl:/api/v1/landAdventures",
-            "shorexUrl:/api/v1/shoreExcursions",
-            "voyageUrl:/api/v1/voyages",
-            "landAdventuresUrl:/api/v1/landAdventures/Itinerary",
-            "voyageSpecialOffersUrl:/api/v1/voyageSpecialOffers",
-            "specialVoyagesUrl:/api/v1/specialVoyages",
-            "itineraryHotelsUrl:/api/v1/hotels/Itinerary",
-            "itineraryExcursionsUrl:/api/v1/shoreExcursions/Itinerary",
-            "pricesUrl:/api/v1/prices",
-            "hotelUrl:/api/v1/shoreExcursions",
-            "agenciesUrl:/api/v1/agencies",
-            "citiesUrl:/api/v1/cities"
-    })
-    private static final String API_PATH = "apiPath";
-    private Map<String, String> apiPath;
-
-    // TODO review naming
-    @Property(description = "SSC API Root Paths", value = {
+    @Property(description = "AEM content root path", value = {
             "shipUrl:/content/silversea-com/en/ships",
             "brochureUrl:/content/dam/siversea-com/brochures",
             "featuresUrl:/content/silversea-com/en/features",
@@ -70,11 +47,11 @@ public class ApiConfigurationServiceImpl implements ApiConfigurationService {
     private static final String API_ROOT_PATH = "apiRootPath";
     private Map<String, String> apiRootPath;
 
-    @Property(value = "100", label = "page size", description = "page size (number of element returned by the API)")
+    @Property(value = "100", label = "page size", description = "Number of items per page from API")
     private static final String PAGE_SIZE = "pageSize";
     private int pageSize;
 
-    @Property(value = "100", label = "Session refresh", description = "Number of element , before we refresh se session save")
+    @Property(value = "100", label = "Session refresh", description = "Number of transactions before saving session")
     private static final String SESSION_REFRESH = "sessionRefresh";
     private int sessionRefresh;
 
@@ -89,12 +66,16 @@ public class ApiConfigurationServiceImpl implements ApiConfigurationService {
 
         login = PropertiesUtil.toString(properties.get(LOGIN), DEFAULT_LOGIN);
         password = PropertiesUtil.toString(properties.get(PASSWORD), DEFAULT_PASSWORD);
-        apiDomain = PropertiesUtil.toString(properties.get(API_DOMAIN), DEFAULT_API_DOMAIN);
-        apiPath = parseToMap(PropertiesUtil.toStringArray(properties.get(API_PATH), new String[]{}));
+        apiBasePath = PropertiesUtil.toString(properties.get(API_BASE_PATH), DEFAULT_API_BASE_PATH);
         apiRootPath = parseToMap(PropertiesUtil.toStringArray(properties.get(API_ROOT_PATH), new String[]{}));
         pageSize = PropertiesUtil.toInteger(properties.get(PAGE_SIZE), 100);
         sessionRefresh = PropertiesUtil.toInteger(properties.get(SESSION_REFRESH), 100);
         timeout = PropertiesUtil.toInteger(properties.get(TIME_OUT), 100);
+    }
+
+    @Override
+    public String apiBasePath() {
+        return apiBasePath;
     }
 
     @Override
@@ -108,18 +89,8 @@ public class ApiConfigurationServiceImpl implements ApiConfigurationService {
     }
 
     @Override
-    public String apiUrlConfiguration(String api) {
-        return apiPath.get(api);
-    }
-
-    @Override
-    public String apiRootPath(String api) {
+    public String apiRootPath(final String api) {
         return apiRootPath.get(api);
-    }
-
-    @Override
-    public String getApiBaseDomain() {
-        return apiDomain;
     }
 
     @Override
