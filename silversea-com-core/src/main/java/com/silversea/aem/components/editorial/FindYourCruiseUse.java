c09a170da1df6a79ef4866cc5369821898ac338b
psoeung <psoeung@sqli.com>
2017-07-19 17:34:44 +0200

    SCC-1841 find your cruise, create configuration for switch V1 <==> V2

--- silversea-com-core/src/main/java/com/silversea/aem/components/editorial/FindYourCruiseUse.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/editorial/FindYourCruiseUse.java
@@ -3,13 +3,18 @@ package com.silversea.aem.components.editorial;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.sling.api.resource.Resource;
+
 import com.adobe.cq.sightly.WCMUsePojo;
+import com.adobe.granite.confmgr.Conf;
 import com.day.cq.tagging.TagConstants;
 import com.day.cq.tagging.TagManager;
+import com.silversea.aem.constants.WcmConstants;
 import com.silversea.aem.models.TagModel;
 
 public class FindYourCruiseUse extends WCMUsePojo {
     private List<TagModel> featureTags;
+    private String type = "v2";
 
     @Override
     public void activate() throws Exception {
@@ -22,6 +27,13 @@ public class FindYourCruiseUse extends WCMUsePojo {
                 featureTags.add(tagManager.resolve(tagId).adaptTo(TagModel.class));
             }
         }
+
+        // Get type from configuration
+        Resource confRes = getResource().adaptTo(Conf.class).getItemResource("/findyourcruise/findyourcruise");
+
+        if (confRes != null) {
+            type = confRes.getValueMap().get("type", String.class);
+        }
     }
 
     /**
@@ -29,5 +41,12 @@ public class FindYourCruiseUse extends WCMUsePojo {
      */
     public List<TagModel> getFeatureTags() {
         return featureTags;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
     }
 }
