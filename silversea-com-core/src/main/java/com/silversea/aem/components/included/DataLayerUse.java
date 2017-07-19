bc393a4cbd5b0fb729c5de4804cce559f7b8bd1e
mbennabi <mbennabi@SQLI2232-4.SQLI>
2017-07-19 15:19:10 +0200

    SSC-XX data layer, add request quote oject

--- silversea-com-core/src/main/java/com/silversea/aem/components/included/DataLayerUse.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/included/DataLayerUse.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.ResourceResolver;
 
 import com.adobe.cq.sightly.WCMUsePojo;
