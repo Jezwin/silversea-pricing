f7d148eee7689637d668acab1f29307cb7554663
mbennabi <mbennabi@SQLI2232-4.SQLI>
2017-07-20 11:31:25 +0200

    SSC-DataLayer, Addin env_chanel

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-data-layer.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-data-layer.js
@@ -3,4 +3,9 @@ $(function() {
     if (tracksuite != undefined) {
         window.dataLayer.page.track_suite = tracksuite.attr('tracksuite');
     }
+    window.dataLayer.env_channel = $.viewportDetect();
+    
+//    $(window).on('resize', function() {
+//        window.dataLayer.env_channel = $.viewportDetect();
+//    });
 });
