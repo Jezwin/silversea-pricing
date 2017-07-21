b549fdd56ae94375247c5974590f491f77a4d283
psoeung <psoeung@sqli.com>
2017-07-21 15:43:17 +0200

    SSC-1145 - Destination, remove code not used

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-destination.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-destination.js
@@ -1,42 +1,20 @@
-$(function(){
-    var html_block_wrapper = jQuery('#destination-html-switchable-block');
-    var html_block_trigger = jQuery('#destination-html-switcher');
-    var fyc_block_wrapper = jQuery('#destination-fyc-switchable-block');
-    var fyc_block_trigger = jQuery('#destination-fyc-switcher');
-    
-    var flag = false;
-    setTimeout(function(){
-        if($('body.is-mobile:not(.is-tablet)').length) {
-            var sdn_desc = ($('#sdn_desc')).detach();
-            $('#sdn_gallery').append(sdn_desc);
-        }
-    }, 300);
-    
-    jQuery("#destination-html-switcher").on('touchstart click', function(){
-        if (!flag) {
-            flag = true;
-            html_block_wrapper.removeClass('hidden');
-            html_block_trigger.addClass('hidden');
-            fyc_block_wrapper.addClass('hidden');
-            fyc_block_trigger.removeClass('hidden');
-            /*jQuery(".lazyLoadImage").lazyload({
-                effect : "fadeIn"
-            }).removeClass('lazyLoadImage');*/
-            setTimeout(function(){ flag = false; }, 100);
-        }
-        return false;
-    });
-    
-    jQuery("#destination-fyc-switcher").on('touchstart click', function(){
-        if (!flag) {
-            flag = true;
-            html_block_wrapper.addClass('hidden');
-            html_block_trigger.removeClass('hidden');
-            fyc_block_wrapper.removeClass('hidden');
-            fyc_block_trigger.addClass('hidden');
-            setTimeout(function(){ flag = false; }, 100);
-        }
-        return false;
+$(function() {
+    var $html_block_wrapper = $('#destination-html-switchable-block'),
+    $html_block_trigger = $('#destination-html-switcher'),
+    $fyc_block_wrapper = $('#destination-fyc-switchable-block'),
+    $fyc_block_trigger = $('#destination-fyc-switcher');
+
+    $html_block_trigger.on('click', function() {
+        $(this).addClass('hidden');
+        $html_block_wrapper.removeClass('hidden');
+        $fyc_block_wrapper.addClass('hidden');
+        $fyc_block_trigger.removeClass('hidden');
     });
 
+    $fyc_block_trigger.on('click', function() {
+        $(this).addClass('hidden');
+        $html_block_wrapper.addClass('hidden');
+        $html_block_trigger.removeClass('hidden');
+        $fyc_block_wrapper.removeClass('hidden');
+    });
 });
