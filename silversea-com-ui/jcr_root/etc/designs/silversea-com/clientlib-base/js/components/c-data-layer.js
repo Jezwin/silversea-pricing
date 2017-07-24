56dd050caabf99d9f835a81a8db77bfbc480355b
mbennabi <mbennabi@SQLI2232-4.SQLI>
2017-07-24 10:11:09 +0200

    SSC-DataLayer Adding env-chanel to the datalayer

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-data-layer.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-data-layer.js
@@ -3,7 +3,7 @@ $(function() {
     if (tracksuite != undefined) {
         window.dataLayer.page.track_suite = tracksuite.attr('tracksuite');
     }
-    window.dataLayer.env_channel = $.viewportDetect();
+    window.dataLayer.page.env_channel = $.viewportDetect();
     
 //    $(window).on('resize', function() {
 //        window.dataLayer.env_channel = $.viewportDetect();
