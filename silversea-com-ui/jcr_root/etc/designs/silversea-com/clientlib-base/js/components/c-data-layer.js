3c8d36cd2560d2ea3907e3c3f9c7d7a4fb8d98df
lymendoza <lymendoza@sqli.com>
2017-07-25 09:58:42 +0200

    SSC-2024 GTM script not loaded after DEV platform

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-data-layer.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-data-layer.js
@@ -1,9 +1,10 @@
 $(function() {
     var tracksuite = $('.datalayer-suite-name');
     if (tracksuite != undefined) {
-        window.dataLayer.page.track_suite = tracksuite.attr('tracksuite');
+        window.dataLayer[0].track_suite = tracksuite.attr('tracksuite');
+        window.dataLayer.push({'track_suite': tracksuite.attr('tracksuite')});
     }
-    window.dataLayer.page.env_channel = $.viewportDetect();
+    window.dataLayer[0].env_channel = $.viewportDetect();
     
 //    $(window).on('resize', function() {
 //        window.dataLayer.env_channel = $.viewportDetect();
