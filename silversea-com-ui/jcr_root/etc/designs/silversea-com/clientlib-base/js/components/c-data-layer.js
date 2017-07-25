5b0a9fc0ca46a3de5dfc77cee983c79f63f1af09
mbennabi <mbennabi@SQLI2232-4.SQLI>
2017-07-25 17:24:14 +0200

    SSC-DataLayer js bug fix

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-data-layer.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-data-layer.js
@@ -2,19 +2,24 @@ $(function() {
     var tracksuite = $('.datalayer-suite-name');
     if (tracksuite != undefined) {
         window.dataLayer[0].track_suite = tracksuite.attr('tracksuite');
-        window.dataLayer.push({'track_suite': tracksuite.attr('tracksuite')});
+        window.dataLayer.push({
+            'track_suite' : tracksuite.attr('tracksuite')
+        });
     }
     window.dataLayer[0].env_channel = $.viewportDetect();
     
-    
-    
     var userInfo = JSON.parse($.CookieManager.getCookie('userInfo'));
-    window.dataLayer[0].user_email = userInfo.email;
+    if (userInfo != undefined) {
+        window.dataLayer[0].user_email = userInfo.email;
+    }
     
-    var apiIndivId = JSON.parse($.CookieManager.getCookie('api_indiv_id'))
-    window.dataLayer[0].api_indiv_id = apiIndivId;
-//    $(window).on('resize', function() {
-//        window.dataLayer.env_channel = $.viewportDetect();
-//        console.log('test resize');
-//    });
+    var apiIndivId = JSON.parse($.CookieManager.getCookie('api_indiv_id'));
+    if (apiIndivId != undefined) {
+        window.dataLayer[0].api_indiv_id = apiIndivId;
+    }
+    
+    // $(window).on('resize', function() {
+    // window.dataLayer.env_channel = $.viewportDetect();
+    // console.log('test resize');
+    //    });
 });
