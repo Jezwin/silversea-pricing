7bf620f776264964c91af348b4deb2e368935717
psoeung <psoeung@sqli.com>
2017-07-26 11:32:55 +0200

    SSC-855 basepage, get hreflang value "valid" (replace '_' by '-')

--- silversea-com-core/src/main/java/com/silversea/aem/helper/CountryHelper.java
+++ silversea-com-core/src/main/java/com/silversea/aem/helper/CountryHelper.java
@@ -1,13 +1,19 @@
 package com.silversea.aem.helper;
 
-import org.apache.cxf.common.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.resource.Resource;
 
 import com.adobe.cq.sightly.WCMUsePojo;
+import com.day.cq.commons.inherit.HierarchyNodeInheritanceValueMap;
+import com.day.cq.commons.inherit.InheritanceValueMap;
+import com.day.cq.commons.jcr.JcrConstants;
+import com.day.cq.wcm.api.Page;
 import com.silversea.aem.services.GeolocationService;
 
 public class CountryHelper extends WCMUsePojo {
 
     private String country;
+    private String hrefLangValid;
     private String localizedPhone;
     private String clickableLocalizedPhone;
 
@@ -25,6 +31,16 @@ public class CountryHelper extends WCMUsePojo {
             // Remove any non-digit
             clickableLocalizedPhone = localizedPhone.replaceAll("\\D", "");
         }
+
+        String pagePath = get("path", String.class);
+        Resource res = getResourceResolver().getResource(pagePath);
+        if (StringUtils.isNotBlank(pagePath)) {
+            if (res != null) {
+                InheritanceValueMap properties = new HierarchyNodeInheritanceValueMap(res);
+                Page page = res.adaptTo(Page.class);
+                hrefLangValid = properties.getInherited(JcrConstants.JCR_LANGUAGE, page.getProperties().get(JcrConstants.JCR_LANGUAGE, String.class)).replaceAll("_", "-");
+            }
+        }
     }
 
     /**
@@ -34,12 +50,24 @@ public class CountryHelper extends WCMUsePojo {
         return country;
     }
 
+    /**
+     * @return the hrefLangValid
+     */
+    public String getHrefLangValid() {
+        return hrefLangValid;
+    }
+
+    /**
+     * @return the localizedPhone
+     */
     public String getLocalizedPhone() {
         return localizedPhone;
     }
 
+    /**
+     * @return the clickableLocalizedPhone
+     */
     public String getClickableLocalizedPhone() {
         return clickableLocalizedPhone;
     }
-
 }
