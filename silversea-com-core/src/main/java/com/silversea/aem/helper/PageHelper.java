a557b780debe70cde6ff5de43af6d37b66d6914c
psoeung <psoeung@sqli.com>
2017-07-29 22:47:38 +0200

    SSC-855 base page, create meta tag for hreflang

--- silversea-com-core/src/main/java/com/silversea/aem/helper/PageHelper.java
+++ silversea-com-core/src/main/java/com/silversea/aem/helper/PageHelper.java
@@ -1,16 +1,30 @@
 package com.silversea.aem.helper;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.jcr.RangeIterator;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 
 import com.adobe.cq.sightly.WCMUsePojo;
+import com.day.cq.commons.Externalizer;
+import com.day.cq.commons.inherit.HierarchyNodeInheritanceValueMap;
+import com.day.cq.commons.inherit.InheritanceValueMap;
+import com.day.cq.commons.jcr.JcrConstants;
 import com.day.cq.wcm.api.NameConstants;
 import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.WCMException;
+import com.day.cq.wcm.msm.api.BlueprintManager;
+import com.day.cq.wcm.msm.api.LiveRelationship;
+import com.day.cq.wcm.msm.api.LiveRelationshipManager;
 
 public class PageHelper extends WCMUsePojo {
 
     private Page page;
     private String thumbnail;
+    private Map<String, String> languagePages;
 
     @Override
     public void activate() throws Exception {
@@ -23,13 +37,58 @@ public class PageHelper extends WCMUsePojo {
         if (resource != null) {
             page = resource.adaptTo(Page.class);
 
-            if(page != null) {
+            if (page != null) {
                 Resource imageRes = page.getContentResource("image");
                 if (imageRes != null) {
                     thumbnail = imageRes.getValueMap().get("fileReference", String.class);
                 }
             }
         }
+
+        // Get hrefLang page
+        languagePages = fillLanguagePages();
+    }
+
+    private Map<String, String> fillLanguagePages() throws WCMException {
+        Resource currentRes = getCurrentPage().adaptTo(Resource.class);
+        LiveRelationshipManager liveRelationshipManager = getResourceResolver().adaptTo(LiveRelationshipManager.class);
+        BlueprintManager bluePrintManager = getResourceResolver().adaptTo(BlueprintManager.class);
+        Externalizer externalizer = getResourceResolver().adaptTo(Externalizer.class);
+        
+        String bluePrintPath = "";
+
+        if (liveRelationshipManager.hasLiveRelationship(currentRes)) {
+            // Current page is a livecopy
+            LiveRelationship liveRelationship = liveRelationshipManager.getLiveRelationship(currentRes, false);
+            // Set blue print path
+            bluePrintPath = liveRelationship.getSourcePath();
+        } else if (bluePrintManager.getContainingBlueprint(getCurrentPage().getPath()) != null) {
+            // Current page is blueprint
+            bluePrintPath = getCurrentPage().getPath();
+        }
+
+        languagePages = new LinkedHashMap<String, String>();
+        Resource bluePrintRes = getResourceResolver().getResource(bluePrintPath);
+
+        // Add blueprint
+        languagePages.put(getHrefLangValid(bluePrintRes), externalizer.externalLink(getResourceResolver(), Externalizer.LOCAL, bluePrintPath));
+
+        RangeIterator liveRelationships = liveRelationshipManager.getLiveRelationships(bluePrintRes, null, null);
+
+        while (liveRelationships.hasNext()) {
+            LiveRelationship liveRelationship = (LiveRelationship) liveRelationships.next();
+            Resource targetRes = getResourceResolver().getResource(liveRelationship.getTargetPath());
+
+            // Add livecopy
+            languagePages.put(getHrefLangValid(targetRes), externalizer.externalLink(getResourceResolver(), Externalizer.LOCAL, liveRelationship.getTargetPath()));
+        }
+
+        return languagePages;
+    }
+
+    private String getHrefLangValid(Resource res) {
+        InheritanceValueMap properties = new HierarchyNodeInheritanceValueMap(res);
+        return properties.getInherited(JcrConstants.JCR_LANGUAGE, page.getProperties().get(JcrConstants.JCR_LANGUAGE, String.class)).replaceAll("_", "-");
     }
 
     /**
@@ -58,12 +117,18 @@ public class PageHelper extends WCMUsePojo {
 
         return path.substring(path.lastIndexOf('/') + 1);
     }
-    
 
     /**
      * @return the page for a given path
      */
     public String getThumbnail() {
         return thumbnail;
+    }
+
+    /**
+     * @return the languagePages
+     */
+    public Map<String, String> getLanguagePages() {
+        return languagePages;
     }
 }
