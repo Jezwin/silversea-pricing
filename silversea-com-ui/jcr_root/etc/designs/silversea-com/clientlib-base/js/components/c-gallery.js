0bf6842f99d3895431682148f402d6c96a573367
psoeung <psoeung@sqli.com>
2017-08-01 15:44:42 +0200

    SSC-1480 gallery cc, Description : Show description on slide change

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-gallery.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-gallery.js
@@ -24,13 +24,21 @@ $(function() {
             // Category tab : Slide to the first image of the current category
             $('.c-gallery--cc__tab__link').on('click', function(e) {
                 e.preventDefault();
-                var $link = $(this),
-                    targetSlideIndex = $slideFor.find('.slick-slide:not(".slick-cloned")[data-category-target="' + $link.data('category') + '"]').index() - 1;
+                var $link = $(this), targetSlideIndex = $slideFor.find('.slick-slide:not(".slick-cloned")[data-category-target="' + $link.data('category') + '"]').index() - 1;
 
-                $slideFor.slick('slickGoTo', targetSlideIndex);
+                $link.closest($component).find($slideFor).slick('slickGoTo', targetSlideIndex);
 
                 // Set Active status on the current tab
                 $link.closest('.c-gallery--cc__tab__item').addClass('active').siblings().removeClass('active');
+            });
+
+            // Description : Show description on slide change
+            $('.active .c-gallery--cc__tab__link').trigger('click'); // force to show description after slider initialization
+            $slideFor.on('afterChange', function(event, slick, currentSlide) {
+                var index = currentSlide,
+                    $captionWrapper = $component.find('.c-gallery--cc__caption'),
+                    $currentSlideElement = $(this).find('[data-slick-index = ' + index + ']');
+                $captionWrapper.text($currentSlideElement.find('.c-gallery--cc__text').text());
             });
         });
     };
