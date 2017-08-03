b564ae5cb28e49faae4e3325279268473a5ef165
psoeung <psoeung@sqli.com>
2017-08-03 12:00:24 +0200

    SSC-889 mozaic slider, clean code

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-slider.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-slider.js
@@ -11,73 +11,68 @@ settingSlider = {
         }
     } ]
 };
-$(function(){
+
+$(function() {
     /***************************************************************************
      * Slider
      **************************************************************************/
     // On page load
-    if ($.viewportDetect() === "xs") {
-        $(".c-slider:not(.c-slider--first-slide-only, .c-slider--for, .c-slider--nav)").slick(settingSlider);
+    if ($.viewportDetect() === 'xs') {
+        $('.c-slider:not(.c-slider--first-slide-only, .c-slider--for, .c-slider--nav)').slick(settingSlider);
     } else {
-        $(".c-slider:not(.c-slider--for, .c-slider--nav)").slick(settingSlider);
+        $('.c-slider:not(.c-slider--for, .c-slider--nav)').slick(settingSlider);
     }
 
     // Filter
-    $(".c-slider.slick-initialized").slick("slickFilter", ":not(cq, .new.section)");
+    $('.c-slider.slick-initialized').slick('slickFilter', ':not(cq, .new.section)');
 
     // On page resize
-    $(window).on("resize", function() {
-        if ($.viewportDetect() === "xs") {
+    $(window).on('resize', function() {
+        if ($.viewportDetect() === 'xs') {
             // Disabled slider if the viewport is XS after page resizes
-            $(".c-slider.c-slider--first-slide-only.slick-initialized").slick("unslick");
+            $('.c-slider.c-slider--first-slide-only.slick-initialized').slick('unslick');
         } else {
             // Enable slider if the viewport is not XS after page resize
-            $(".c-slider.c-slider--first-slide-only:not(.slick-initialized)").slick(settingSlider);
+            $('.c-slider.c-slider--first-slide-only:not(.slick-initialized)').slick(settingSlider);
         }
     });
 
-    //$(".c-slider.c-slider--first-slide-only.slick-initialized").slick("unslick");
+    /***************************************************************************
+     * Mozaic Slider
+     **************************************************************************/
     settingSlider['fade'] = true;
     settingSlider['dots'] = true;
     settingSlider['autoplaySpeed'] = 5000;
+
     $('.mozaicslider').slick(settingSlider);
-    
-    /*$('.mozaic-slider .mozaicItemText').css('height', $('.mozaic-slider .slick-slide img').first().height() - 2);
-    $(window).on("resize", function() {
-        $('.mozaic-slider .mozaicItemText').css('height', $('.mozaic-slider .slick-slide img').first().height() - 2);
-    });*/
 
     //mozaic slider on mobile viewport
-
-    $(".mozaic-slider").each(function(index) {
+    $('.mozaic-slider').each(function(index) {
 
         var $slider_mosaic = $(this);
-        $slider_mosaic.after( "<div class='mobileSlider " + 'mobileSlider_'+ index + "'></div>" );
+        $slider_mosaic.after("<div class='mobileSlider " + 'mobileSlider_' + index + "'></div>");
 
         var mozaicItem = document.createElement('div');
-        mozaicItem.className = "mozaicHeader";
+        mozaicItem.className = 'mozaicHeader';
 
         $slider_mosaic.parent().find('.mobileSlider').before(mozaicItem);
 
         var $cureent_mozaic_header = $slider_mosaic.parent().find('.mozaicHeader');
 
-        $slider_mosaic.find('.c-mozaicslider__title').first().clone().appendTo( $cureent_mozaic_header );
-        $slider_mosaic.find('.c-mozaicslider__descr').first().clone().appendTo( $cureent_mozaic_header );
+        $slider_mosaic.find('.c-mozaicslider__title').first().clone().appendTo($cureent_mozaic_header);
+        $slider_mosaic.find('.c-mozaicslider__descr').first().clone().appendTo($cureent_mozaic_header);
 
-        $slider_mosaic.find(".c-mozaicslider:not(.slick-cloned)").find('.mozaicItem').each( function() {
-            $slider_mosaic.parent().find('.mobileSlider').append( $(this).html() );
+        $slider_mosaic.find(".c-mozaicslider:not(.slick-cloned)").find('.mozaicItem').each(function() {
+            $slider_mosaic.parent().find('.mobileSlider').append($(this).html());
         });
 
-        $('.mobileSlider_'+ index).slick({
-            dots: true,
-            infinite: true,
-            speed: 500,
-            fade: true,
+        $('.mobileSlider_' + index).slick({
+            dots : true,
+            infinite : true,
+            speed : 500,
+            fade : true,
         });
-        $slider_mosaic.find('.link__wrapper').first().clone().appendTo( '.mobileSlider_'+ index );
-     });
 
-
+        $slider_mosaic.find('.link__wrapper').first().clone().appendTo('.mobileSlider_' + index);
+    });
 });
-
-
