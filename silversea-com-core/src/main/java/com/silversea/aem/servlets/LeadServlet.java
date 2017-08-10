65b9ab21957f550af5584693e9d4c8e6ceb571c5
Aurelien Olivier <auolivier@sqli.com>
2017-08-10 16:05:03 +0200

    Cleaning

--- silversea-com-core/src/main/java/com/silversea/aem/servlets/LeadServlet.java
+++ silversea-com-core/src/main/java/com/silversea/aem/servlets/LeadServlet.java
@@ -1,7 +1,8 @@
 package com.silversea.aem.servlets;
 
-import javax.servlet.ServletException;
-
+import com.silversea.aem.components.beans.Lead;
+import com.silversea.aem.technical.json.JsonMapper;
+import com.silversea.aem.ws.lead.service.LeadService;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
@@ -9,15 +10,12 @@ import org.apache.sling.api.SlingHttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.silversea.aem.components.beans.Lead;
-import com.silversea.aem.technical.json.JsonMapper;
-import com.silversea.aem.ws.lead.service.LeadService;
+import javax.servlet.ServletException;
 
 @SlingServlet(methods = "POST", paths = "/bin/lead", extensions = "json")
 public class LeadServlet extends ScServlet {
 
-    private static final  Logger LOGGER = LoggerFactory.getLogger(LeadServlet.class);
-    private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = LoggerFactory.getLogger(LeadServlet.class);
 
     @Reference
     private LeadService leadService;
@@ -25,10 +23,14 @@ public class LeadServlet extends ScServlet {
     protected final void doPost(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws ServletException {
         // Retrieve body content from request
         String body = getBodyFromRequest(request);
+
         LOGGER.debug("Lead service request {}", body);
+
         Lead lead = JsonMapper.getDomainObject(body, Lead.class);
         String leadResponse = leadService.sendLead(lead);
+
         LOGGER.debug("Lead service response {}", leadResponse);
+
         writeDomainObject(response, leadResponse);
     }
 }
