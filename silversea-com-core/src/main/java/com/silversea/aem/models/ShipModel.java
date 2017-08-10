b24e490f0806066b9adbc9a9de5925b05bc47a01
psoeung <psoeung@sqli.com>
2017-08-10 15:40:07 +0200

    SSC-926 add missing properties

--- silversea-com-core/src/main/java/com/silversea/aem/models/ShipModel.java
+++ silversea-com-core/src/main/java/com/silversea/aem/models/ShipModel.java
@@ -1,18 +1,18 @@
 package com.silversea.aem.models;
 
-import com.day.cq.commons.jcr.JcrConstants;
-import com.day.cq.wcm.api.Page;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
-import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-import java.util.ArrayList;
-import java.util.List;
+import com.day.cq.commons.jcr.JcrConstants;
+import com.day.cq.wcm.api.Page;
 
 @Model(adaptables = Page.class)
 public class ShipModel {
@@ -42,6 +42,39 @@ public class ShipModel {
 
     @Inject @Named(JcrConstants.JCR_CONTENT + "/deckPlan") @Optional
     private String deckPlan;
+
+    @Inject @Named(JcrConstants.JCR_CONTENT + "/guestsCapacity") @Optional
+    private String guestsCapacity;
+
+    @Inject @Named(JcrConstants.JCR_CONTENT + "/crewCapacity") @Optional
+    private String crewCapacity;
+
+    @Inject @Named(JcrConstants.JCR_CONTENT + "/builtDate") @Optional
+    private String builtDate;
+
+    @Inject @Named(JcrConstants.JCR_CONTENT + "/refurbDate") @Optional
+    private String refurbDate;
+
+    @Inject @Named(JcrConstants.JCR_CONTENT + "/tonnage") @Optional
+    private String tonnage;
+
+    @Inject @Named(JcrConstants.JCR_CONTENT + "/lengthFt") @Optional
+    private String lengthFt;
+
+    @Inject @Named(JcrConstants.JCR_CONTENT + "/lengthM") @Optional
+    private String lengthM;
+    
+    @Inject @Named(JcrConstants.JCR_CONTENT + "/width") @Optional
+    private String width;
+    
+    @Inject @Named(JcrConstants.JCR_CONTENT + "/speed") @Optional
+    private String speed;
+    
+    @Inject @Named(JcrConstants.JCR_CONTENT + "/registry") @Optional
+    private String registry;
+
+    @Inject @Named(JcrConstants.JCR_CONTENT + "/image/fileReference") @Optional
+    private String thumbnail;
 
     @Inject @Named("dinings")
     private List<DiningModel> dinings;
@@ -94,5 +127,53 @@ public class ShipModel {
 
     public List<SuiteModel> getSuites() {
         return suites;
+    }
+
+    public static Logger getLogger() {
+        return LOGGER;
+    }
+
+    public String getGuestsCapacity() {
+        return guestsCapacity;
+    }
+
+    public String getCrewCapacity() {
+        return crewCapacity;
+    }
+
+    public String getBuiltDate() {
+        return builtDate;
+    }
+
+    public String getRefurbDate() {
+        return refurbDate;
+    }
+
+    public String getTonnage() {
+        return tonnage;
+    }
+
+    public String getLengthFt() {
+        return lengthFt;
+    }
+
+    public String getLengthM() {
+        return lengthM;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public String getRegistry() {
+        return registry;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
     }
 }
