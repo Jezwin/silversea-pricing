4823a604b53bd84043a51ad37a5d9ae5ab747c9d
Aurélien Olivier <auolivier@sqli.com>
2017-08-22 17:02:05 +0200

    Cleaning legacy cruise import

--- silversea-com-core/src/main/java/com/silversea/aem/importers/services/CruisesUpdateImporter.java
+++
@@ -1,9 +0,0 @@
-package com.silversea.aem.importers.services;
-
-import java.io.IOException;
-
-@Deprecated
-public interface CruisesUpdateImporter {
-    
-    void importData() throws IOException;
-}
