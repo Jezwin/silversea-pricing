5c9429f2b35770d13b62d34f855bb18343998ad3
psoeung <psoeung@sqli.com>
2017-08-25 11:29:30 +0200

    SSC-2176 search cruise, search hero, send request to find your cruise page

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-search-cruise-hero.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-search-cruise-hero.js
@@ -1,5 +1,25 @@
 $(function() {
-    $('.c-search-hero__form, .c-search-cruise__form').each(function() {
+    $('.c-fyc-light__form').each(function() {
+        var $form = $(this);
+        var requestUrl;
+        $form.on('change', function(e) {
+            e.preventDefault();
+            var $filterValue = $($form.serializeArray());
+            requestUrl = $form.attr('action');
+
+            $filterValue.each(function(i, field) {
+                // Add filter
+                requestUrl += '.' + field.name + '_' + field.value.replace(/\//g, 'forwardSlash');
+            });
+
+            requestUrl += '.html'
+            $form.attr('action', requestUrl);
+        });
+
+        $form.on('submit', function(e) {
+            e.preventDefault();
+            document.location.href = $form.attr('action');
+        });
 
     })
 });
