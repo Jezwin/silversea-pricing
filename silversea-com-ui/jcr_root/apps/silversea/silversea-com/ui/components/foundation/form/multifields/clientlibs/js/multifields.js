148601bdd2d9dff99a90632c6766332f6fa613f4
psoeung <psoeung@sqli.com>
2017-08-25 17:42:17 +0200

    SSC-XX multifields, fix ui component for chrome

--- silversea-com-ui/jcr_root/apps/silversea/silversea-com/ui/components/foundation/form/multifields/clientlibs/js/multifields.js
+++ silversea-com-ui/jcr_root/apps/silversea/silversea-com/ui/components/foundation/form/multifields/clientlibs/js/multifields.js
@@ -2,26 +2,24 @@
 
     // collect data from widgets in multifield and POST them to CRX as JSON
     var collectDataFromFields = function() {
-        $(document).on('click', 'button', function(e) {
-            if ($(e.target).attr('id') === 'shell-propertiespage-saveactivator' || $(e.target).hasClass('cq-dialog-submit')) {
-                var record;
+        $(document).on('click', 'button#shell-propertiespage-saveactivator, button.cq-dialog-submit', function(e) {
+            var record;
 
-                // Clean
-                $('input.hidden-multifiels').remove();
+            // Clean
+            $('input.hidden-multifiels').remove();
 
-                // parse field and build json string
-                $('.multifields-wrapper').each(function(i, section) {
-                    record = {};
-                    var $fields = $(section).find('input[name],select[name],textarea[name]'), arrayValues = $fields.serializeArray();
+            // parse field and build json string
+            $('.multifields-wrapper').each(function(i, section) {
+                record = {};
+                var $fields = $(section).find('input[name],select[name],textarea[name]'), arrayValues = $fields.serializeArray();
 
-                    $(arrayValues).each(function(i, item) {
-                        record[item.name] = item.value;
-                    });
-
-                    // Record json string
-                    $('<input />').attr('type', 'hidden').attr('name', $(section).data('name')).attr('class', 'hidden-multifiels').attr('value', JSON.stringify(record)).appendTo($(section));
+                $(arrayValues).each(function(i, item) {
+                    record[item.name] = item.value;
                 });
-            }
+
+                // Record json string
+                $('<input />').attr('type', 'hidden').attr('name', $(section).data('name')).attr('class', 'hidden-multifiels').attr('value', JSON.stringify(record)).appendTo($(section));
+            });
         });
     };
 
