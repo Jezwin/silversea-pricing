d1d5e2e8f929d26cad28512dbb32b86742edecdf
psoeung <psoeung@sqli.com>
2017-08-28 16:43:11 +0200

    SSC-2021 fix search position in main navigation

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-search.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-search.js
@@ -1,60 +1,23 @@
 $(function() {
+    $('[data-toggle-search]').on('click', function(e) {
+        e.preventDefault();
+        var $trigger = $(this);
+        var $targetExpand = $($trigger.data('toggle-search'));
+        var isOpen = $targetExpand.hasClass('open');
+        var maxWidth = $targetExpand.find('.search-expand__input').outerWidth() + $targetExpand.find('.search-expand__btn').outerWidth()
 
-    var wrapperSearch = $('.c-main-nav__search');
-    var submitIcon = wrapperSearch.find('.c-main-nav__search__searchbox-icon');
-    var inputBox = wrapperSearch.find('.c-main-nav__search__searchbox-input');
-    var searchBoxSubmit = wrapperSearch.find('.c-main-nav__search__searchbox-submit');
-    var searchBox = wrapperSearch.find('.c-main-nav__search__searchbox');
-    var isOpen = false;
+        $targetExpand.css('width', isOpen ? '' : maxWidth);
 
-    submitIcon.click(function(){
-        if(isOpen == false){
-            wrapperSearch.css({'z-index': '0', 'width': '300'});
-            searchBox.css({'witdh': '300'});
-            $(this).removeClass('c-main-nav__search__searchbox-icon');
-            $(this).addClass('glyphicon-search-left');
-            searchBox.addClass('searchbox-open');
-            searchBoxSubmit.css('display','block');
-            searchBoxSubmit.css('background-color','#999');
-            inputBox.focus();
-            isOpen = true;
-            inputBox.css('display','block');
-            inputBox.css('margin-left','100%');
-        } else {
-            wrapperSearch.css({'z-index': '-1', 'width': '100'});
-            searchBox.animate({
-                witdh: "50",
-            }, 1500 );
-            $(this).addClass('c-main-nav__search__searchbox-icon');
-            $(this).removeClass('glyphicon-search-left');
-            searchBox.removeClass('searchbox-open');
-            inputBox.focusout();
-            searchBoxSubmit.css('display','none');
-            isOpen = false;
-            inputBox.animate({
-                marginLeft: "0%",
-            }, 1500 );
-            inputBox.css('display','none');
-        }
+        $targetExpand.toggleClass('open');
+
+        // Close search on page click
+        $(document).on('click', function(e) {
+            e.stopPropagation();
+            if ($targetExpand.hasClass('open') && $(e.target).closest('.search-expand').length === 0) {
+                $trigger.trigger('click');
+            } else {
+                return false;
+            }
+        })
     });
-    submitIcon.mouseup(function(){
-        return false;
-    });
-    searchBox.mouseup(function(){
-        return false;
-    });
-
-    $(document).mouseup(function(){
-        if(isOpen == true){
-            submitIcon.css('display','block');
-            submitIcon.click();
-        }
-    });
-
-
-    $( ".c-search-result__search-trigger" ).click(function() {
-        $( ".c-search-result__expand" ).toggle();
-        $(this).find('i').toggleClass('fa-angle-up fa-angle-down');
-    });
-
 });
