ab2cee2706a77196bb90b7ca56c0581c53bc4144
Aurélien Olivier <auolivier@sqli.com>
2017-08-29 10:38:33 +0200

    Countries import diff

--- silversea-com-core/src/main/java/com/silversea/aem/importers/services/CountriesImporter.java
+++ silversea-com-core/src/main/java/com/silversea/aem/importers/services/CountriesImporter.java
@@ -1,15 +1,12 @@
 package com.silversea.aem.importers.services;
 
+import com.silversea.aem.importers.ImporterException;
 import com.silversea.aem.importers.services.impl.ImportResult;
 import org.apache.sling.commons.json.JSONObject;
 
-import java.io.IOException;
-
 public interface CountriesImporter {
 
-    ImportResult importData() throws IOException;
-
-    void importCountry(final String id);
+    ImportResult importData(final boolean update) throws ImporterException;
 
     JSONObject getCountriesMapping();
 }
