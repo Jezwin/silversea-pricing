e6cfb0004bf7a0aec1b6d48fcb3c013291e7486f
psoeung <psoeung@sqli.com>
2017-09-14 13:12:58 +0200

    SSC-2357 IE 11 : Not able to submit a bro

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-quote-request-form.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-quote-request-form.js
@@ -7,7 +7,7 @@ $(function() {
     requestForm();
 });
 
-function requestForm() {
+function requestForm() { 
     // Customize checkebox
     $('.custom-checkbox').iCheck({
         checkboxClass : 'icheckbox_minimal'
@@ -121,7 +121,6 @@ function requestForm() {
         }
     }).off('input.bs.validator change.bs.validator').on('submit', function(e) {
         $(this).find('[name="phone"]').val($('#InputTelephoneNumber').intlTelInput("getNumber"));
-
         if (!e.isDefaultPrevented()) {
             $.signUp.signUpOffers(this, e);
         }
