ab86dc9b4d4bfca134d391fcd539826a81771787
Giuseppe Scaramuzzino <giuseppes@silversea.com>
2018-01-19 17:18:18 +0100

    create lightbox to show detail of suites

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-modal.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-modal.js
@@ -46,6 +46,148 @@ $(function() {
     
     
     /***************************************************************************
+     * Build modal for suite detail
+     **************************************************************************/
+    $('.automatic-modal-suite-detail').on('click', function(e) {
+        e.preventDefault();
+
+        if (!$('body').hasClass("viewport-xs")) { //check if not mobile
+        	
+        	$('body').addClass('modal-open');
+        	var $modalContent = window.templateSuiteDetail;
+        	var id = $(this).attr('id');
+        	if (window.hasOwnProperty("cruiseItem") && window.cruiseItem != null) {
+        		
+        		//label i18n
+        		var navDescription = ($(this).parent().data('nav-description') != null) ? $(this).parent().data('nav-description')   : "";
+        		var navPlan = ($(this).parent().data('nav-plan') != null) ? $(this).parent().data('nav-plan')   : "";
+        		var navFeatures = ($(this).parent().data('nav-features') != null) ? $(this).parent().data('nav-features')   : "";
+        		var navLocation = ($(this).parent().data('nav-location') != null) ? $(this).parent().data('nav-location')   : "";
+        		var navVirtualTour = ($(this).parent().data('nav-virtual-tour') != null) ? $(this).parent().data('nav-virtual-tour')  : "";
+
+        		var note = ($(this).parent().data('note') != null) ? $(this).parent().data('note')   : "";
+        		var requestQuote = ($(this).parent().data('request-quote') != null) ? $(this).parent().data('request-quote')   : "";
+        		var earlyBonus = ($(this).parent().data('early-bonus') != null) ? $(this).parent().data('early-bonus')   : "";
+        		var waitlist = ($(this).parent().data('waitlist') != null) ? $(this).parent().data('waitlist')   : "";
+        		var deckLabel = ($(this).parent().data('deck') != null) ? $(this).parent().data('deck')   : "";
+        		var from = "";
+        		
+        		//suite values
+        		var title = (window.cruiseItem[id].title != null) ? window.cruiseItem[id].title : "";
+        		var assetSelectionReference = (window.cruiseItem[id].assetSelectionReference != null) ? window.cruiseItem[id].assetSelectionReference : "";
+        		var longDescription = (window.cruiseItem[id].longDescription != null) ? window.cruiseItem[id].longDescription : "";
+        		var bedroomsInformation = (window.cruiseItem[id].bedroomsInformation != null) ? window.cruiseItem[id].bedroomsInformation : "";
+        		var currency = ($(this).data('currency') != null) ? $(this).data('currency')   : "";
+        		var raqLink = $("#"+id).find(".link-request-quote-card").attr("href");
+        		var early = window.cruiseItem[id].early;
+        		var priceEarlyBookingBonus = (window.cruiseItem[id].priceBookingBonus != null) ? currency + " " + window.cruiseItem[id].priceBookingBonus : "";
+        		var suitePlan =  (window.cruiseItem[id].plan != null) ? window.cruiseItem[id].plan : "";
+        		var features =  (window.cruiseItem[id].features != null) ? window.cruiseItem[id].features : "";
+        		var locationImage =  (window.cruiseItem[id].locationImage != null) ? window.cruiseItem[id].locationImage : "";
+        		var deck =(window.cruiseItem[id].deck != null) ? window.cruiseItem[id].deck : "";
+        		
+        		var price = waitlist;
+        		
+        		if (window.cruiseItem[id].price != null) {
+        			price = currency + " " + window.cruiseItem[id].price;
+        			from =  ($(this).parent().data('from') != null) ? $(this).parent().data('from')   : "";
+        		}
+        		
+        		//HTML layout label 
+        		$modalContent  = $modalContent.replace("c-suitelist-nav-description-placeholder", navDescription);
+        		$modalContent  = $modalContent.replace("c-suitelist-nav-plan-placeholder", navPlan);
+        		$modalContent  = $modalContent.replace("c-suitelist-nav-features-placeholder", navFeatures);
+        		$modalContent  = $modalContent.replace("c-suitelist-nav-location-placeholder", navLocation);
+        		$modalContent  = $modalContent.replace("c-suitelist-nav-virtual-tour-placeholder", navVirtualTour);
+        		$modalContent  = $modalContent.replace("c-suitelist-note-placeholder", note);
+        		$modalContent  = $modalContent.replace("c-suitelist-raq-placeholder", requestQuote);
+        		$modalContent  = $modalContent.replace("c-suitelist-from-placeholder", from);
+        		
+        		// HTML layout values
+        		$modalContent  = $modalContent.replace("c-suitelist-title-placeholder", title);
+        		$modalContent  = $modalContent.replace("c-suitelist-longDescription-placeholder", longDescription);
+        		$modalContent  = $modalContent.replace("c-suitelist-bedroomsInformation-placeholder", bedroomsInformation);
+        		$modalContent  = $modalContent.replace("c-suitelist-raq-link-placeholder", raqLink);
+        		$modalContent  = $modalContent.replace("c-suitelist-price-placeholder", price);
+        		$modalContent  = $modalContent.replace("c-suitelist-price-booking-bonus-placeholder", priceEarlyBookingBonus);
+        		$modalContent  = $modalContent.replace("c-suitelist-plan-placeholder", suitePlan);
+        		if (early) {
+        			$modalContent  = $modalContent.replace("c-suitelist-early-bonus-placeholder", earlyBonus);
+        		} else {
+        			$modalContent  = $modalContent.replace("c-suitelist-early-bonus-placeholder", "");
+        		}
+        		
+        		// HTML layout objects list
+        		var featuresList = features.split("#next#");
+        		var featuresToRender = "";
+        		for(item in featuresList) {
+        			featuresToRender += "<li>"+featuresList[item]+"</li>";
+        		}
+        		$modalContent  = $modalContent.replace("c-suitelist-features-placeholder", featuresToRender);
+        		
+        		var deckList = deck.split("#next#");
+        		var deckToRender = "";
+        		for(item in deckList) {
+        			var classItem = "btn btn-thin show-deck-image";
+        			var idItem = "deck-"+item;
+        			if (item == 0) {
+        				classItem += " activeDeck";
+        			}
+        			deckToRender += '<a id="'+idItem+'" class="'+classItem+'" href="#"> <span> '+ deckLabel + ' ' +deckList[item] +'</span></a>';
+        		}
+        		$modalContent  = $modalContent.replace("c-suitelist-deck-placeholder", deckToRender);
+        		   
+        		var locationImageList = locationImage.split("#next#");
+        		var locationImageToRender = "";
+        		var style="";
+        		for(item in locationImageList) {
+        			var idItem = "deck-"+item+"-image";
+        			if (item != 0) {
+        				style = "style='display:none'";
+        			}
+        			locationImageToRender += '<img id="'+idItem+'" src="'+ locationImageList[item] +'"?hei=930&wid=930&fit=constrain" alt="Suite plan" title="Suite plan" class="o-img"' + style + '/>';
+        		}
+        		
+    			$modalContent  = $modalContent.replace("c-suitelist-location-placeholder", locationImageToRender);
+    			
+    			var assetSelectionReferenceList = assetSelectionReference.split("#next#");
+        		var assetSelectionToRender = "";
+        		for(item in assetSelectionReferenceList) {
+        			 //assetSelectionReferenceList[item] += "?hei=930&wid=930&fit=constrain"; 
+        			assetSelectionToRender += '<div class="slider-item"><div class="ratio lazy" style="display:block;background-size: cover;background-position: center;background-repeat: no-repeat;background-image:url('+ assetSelectionReferenceList[item] +'?wid=930&fit=hfit,1)"></div></div>';
+        		}
+        		
+    			$modalContent  = $modalContent.replace("c-suitelist-description-placeholder", assetSelectionToRender);
+    			
+        		// Activate Modal
+        		$($(this).data('target')).modal('show');
+        		
+        		$('.modal').on('shown.bs.modal', function(e) {
+        			$(this).find('.modal-dialog').empty().append($modalContent);
+        			 var $slideFor = $(this).find('.c-slider').slick({
+                           slidesToShow : 1,
+                           slidesToScroll : 1
+                       });
+        			$(this).find(".show-deck-image").on("click", function(e) {	
+				    	e.preventDefault();
+				    	
+				    	var idDeck = $(this).attr("id");
+				    	var idDeckImage = idDeck + "-image";
+				    	
+				    	$("#suite-location a:not(#"+idDeck+")").removeClass("activeDeck");
+				    	$("#suite-location #"+idDeck).addClass("activeDeck");
+				    	
+				    	$("#suite-location img:not(#"+idDeckImage+")").css("display","none");
+				    	$("#suite-location #"+idDeckImage).css("display", "block");
+				    	
+				    });
+        		});
+        	}
+        }
+    });
+    
+    
+    /***************************************************************************
      * Modal virtual tour
      **************************************************************************/
     $('.modal-virtual-tour').on('click', function(e) {
