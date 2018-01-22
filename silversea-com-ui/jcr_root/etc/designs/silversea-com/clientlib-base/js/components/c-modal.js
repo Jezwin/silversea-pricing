072b73a824b08d01836f769f44f76134fda1b1eb
Giuseppe Scaramuzzino <giuseppes@silversea.com>
2018-01-22 15:40:57 +0100

    add virtual tour and description tab

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-modal.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-modal.js
@@ -3,7 +3,6 @@ $(function() {
      * Modal : Clean modal content on close event
      **************************************************************************/
     $(document).on('hide.bs.modal', function(e) {
-
     	$(e.target).removeData('bs.modal');
 
         $('body').removeClass('modal-open');
@@ -74,16 +73,18 @@ $(function() {
         		
         		//suite values
         		var title = (window.cruiseItem[id].title != null) ? window.cruiseItem[id].title : "";
-        		var assetSelectionReference = (window.cruiseItem[id].assetSelectionReference != null) ? window.cruiseItem[id].assetSelectionReference : "";
+        		var assetSelectionReference = (window.cruiseItem[id].assetSelectionReference != null) ? window.cruiseItem[id].assetSelectionReference : null;
         		var longDescription = (window.cruiseItem[id].longDescription != null) ? window.cruiseItem[id].longDescription : "";
         		var bedroomsInformation = (window.cruiseItem[id].bedroomsInformation != null) ? window.cruiseItem[id].bedroomsInformation : "";
+        		var virtualTourImage = (window.cruiseItem[id].virtualTour != null) ? window.cruiseItem[id].virtualTour : null;
+        		
         		var currency = ($(this).data('currency') != null) ? $(this).data('currency')   : "";
         		var raqLink = $("#"+id).find(".link-request-quote-card").attr("href");
         		var early = window.cruiseItem[id].early;
         		var priceEarlyBookingBonus = (window.cruiseItem[id].priceBookingBonus != null) ? currency + " " + window.cruiseItem[id].priceBookingBonus : "";
-        		var suitePlan =  (window.cruiseItem[id].plan != null) ? window.cruiseItem[id].plan : "";
+        		var suitePlan =  (window.cruiseItem[id].plan != null) ? window.cruiseItem[id].plan : null;
         		var features =  (window.cruiseItem[id].features != null) ? window.cruiseItem[id].features : "";
-        		var locationImage =  (window.cruiseItem[id].locationImage != null) ? window.cruiseItem[id].locationImage : "";
+        		var locationImage =  (window.cruiseItem[id].locationImage != null) ? window.cruiseItem[id].locationImage : null;
         		var deck =(window.cruiseItem[id].deck != null) ? window.cruiseItem[id].deck : "";
         		
         		var price = waitlist;
@@ -94,10 +95,7 @@ $(function() {
         		}
         		
         		//HTML layout label 
-        		$modalContent  = $modalContent.replace("c-suitelist-nav-description-placeholder", navDescription);
         		$modalContent  = $modalContent.replace("c-suitelist-nav-plan-placeholder", navPlan);
-        		$modalContent  = $modalContent.replace("c-suitelist-nav-features-placeholder", navFeatures);
-        		$modalContent  = $modalContent.replace("c-suitelist-nav-location-placeholder", navLocation);
         		$modalContent  = $modalContent.replace("c-suitelist-nav-virtual-tour-placeholder", navVirtualTour);
         		$modalContent  = $modalContent.replace("c-suitelist-note-placeholder", note);
         		$modalContent  = $modalContent.replace("c-suitelist-raq-placeholder", requestQuote);
@@ -110,64 +108,148 @@ $(function() {
         		$modalContent  = $modalContent.replace("c-suitelist-raq-link-placeholder", raqLink);
         		$modalContent  = $modalContent.replace("c-suitelist-price-placeholder", price);
         		$modalContent  = $modalContent.replace("c-suitelist-price-booking-bonus-placeholder", priceEarlyBookingBonus);
-        		$modalContent  = $modalContent.replace("c-suitelist-plan-placeholder", suitePlan);
+        		
+        		
         		if (early) {
         			$modalContent  = $modalContent.replace("c-suitelist-early-bonus-placeholder", earlyBonus);
         		} else {
         			$modalContent  = $modalContent.replace("c-suitelist-early-bonus-placeholder", "");
         		}
         		
-        		// HTML layout objects list
-        		var featuresList = features.split("#next#");
-        		var featuresToRender = "";
-        		for(item in featuresList) {
-        			featuresToRender += "<li>"+featuresList[item]+"</li>";
-        		}
-        		$modalContent  = $modalContent.replace("c-suitelist-features-placeholder", featuresToRender);
-        		
-        		var deckList = deck.split("#next#");
-        		var deckToRender = "";
-        		for(item in deckList) {
-        			var classItem = "btn btn-thin show-deck-image";
-        			var idItem = "deck-"+item;
-        			if (item == 0) {
-        				classItem += " activeDeck";
-        			}
-        			deckToRender += '<a id="'+idItem+'" class="'+classItem+'" href="#"> <span> '+ deckLabel + ' ' +deckList[item] +'</span></a>';
-        		}
-        		$modalContent  = $modalContent.replace("c-suitelist-deck-placeholder", deckToRender);
-        		   
-        		var locationImageList = locationImage.split("#next#");
-        		var locationImageToRender = "";
-        		var style="";
-        		for(item in locationImageList) {
-        			var idItem = "deck-"+item+"-image";
-        			if (item != 0) {
-        				style = "style='display:none'";
-        			}
-        			locationImageToRender += '<img id="'+idItem+'" src="'+ locationImageList[item] +'"?hei=930&wid=930&fit=constrain" alt="Suite plan" title="Suite plan" class="o-img"' + style + '/>';
-        		}
-        		
-    			$modalContent  = $modalContent.replace("c-suitelist-location-placeholder", locationImageToRender);
+        		//description tab
+        		if (assetSelectionReference != null) {
+    				var assetSelectionReferenceList = assetSelectionReference.split("#next#");
+    				var assetSelectionToRender = "";
+    				for(item in assetSelectionReferenceList) {
+    					assetSelectionToRender += '<div class="slider-item">';
+    					assetSelectionToRender += '<div class="ratio lazy" style="display:block;background-size: cover;background-position: center;background-repeat: no-repeat;background-image:url('+ assetSelectionReferenceList[item] +'?wid=930&fit=hfit,1)"></div>';
+    					assetSelectionToRender += '</div>'; 
+    				}
+    				$modalContent  = $modalContent.replace("c-suitelist-description-placeholder", assetSelectionToRender);
+    				$modalContent  = $modalContent.replace("c-suitelist-nav-description-placeholder", navDescription);
+    			}
     			
-    			var assetSelectionReferenceList = assetSelectionReference.split("#next#");
-        		var assetSelectionToRender = "";
-        		for(item in assetSelectionReferenceList) {
-        			 //assetSelectionReferenceList[item] += "?hei=930&wid=930&fit=constrain"; 
-        			assetSelectionToRender += '<div class="slider-item"><div class="ratio lazy" style="display:block;background-size: cover;background-position: center;background-repeat: no-repeat;background-image:url('+ assetSelectionReferenceList[item] +'?wid=930&fit=hfit,1)"></div></div>';
+        		//suitetab
+        		if (suitePlan != null) {
+        			$modalContent  = $modalContent.replace("c-suitelist-plan-placeholder", suitePlan);
         		}
         		
-    			$modalContent  = $modalContent.replace("c-suitelist-description-placeholder", assetSelectionToRender);
+        		//features tab
+        		if (navFeatures != null) {
+        			var featuresList = features.split("#next#");
+        			var featuresToRender = "";
+        			for(item in featuresList) {
+        				featuresToRender += "<li>"+featuresList[item]+"</li>";
+        			}
+        			$modalContent  = $modalContent.replace("c-suitelist-features-placeholder", featuresToRender);
+            		$modalContent  = $modalContent.replace("c-suitelist-nav-features-placeholder", navFeatures);
+        		}
+        		
+        		//location tab
+        		if (locationImage != null) {
+        			var deckList = deck.split("#next#");
+        			var deckToRender = "";
+        			for(item in deckList) {
+        				var classItem = "btn btn-thin show-deck-image";
+        				var idItem = "deck-"+item;
+        				if (item == 0) {
+        					classItem += " activeDeck";
+        				}
+        				deckToRender += '<a id="'+idItem+'" class="'+classItem+'" href="#"> <span> '+ deckLabel + ' ' +deckList[item] +'</span></a>';
+        			}
+        			$modalContent  = $modalContent.replace("c-suitelist-deck-placeholder", deckToRender);
+        			
+        			var locationImageList = locationImage.split("#next#");
+        			var locationImageToRender = "";
+        			var style="";
+        			for(item in locationImageList) {
+        				var idItem = "deck-"+item+"-image";
+        				if (item != 0) {
+        					style = "style='display:none'";
+        				}
+        				locationImageToRender += '<img id="'+idItem+'" src="'+ locationImageList[item] +'"?hei=930&wid=930&fit=constrain" alt="Suite plan" title="Suite plan" class="o-img"' + style + '/>';
+        			}
+        			
+        			$modalContent  = $modalContent.replace("c-suitelist-location-placeholder", locationImageToRender);
+            		$modalContent  = $modalContent.replace("c-suitelist-nav-location-placeholder", navLocation);
+        		} 
+        		
+        		//virtual tour tab
+        		if (virtualTourImage != null) {
+        			var virtualTourToRender = '<div id="c-suite-detail-modal-virtual-tour-containerDiv" class="c-suite-detail-modal-virtual-tour-container" data-image="'+virtualTourImage+'"></div>';
+        			$modalContent  = $modalContent.replace("c-suitelist-virtual-tour-placeholder", virtualTourToRender);
+            		$modalContent  = $modalContent.replace("c-suitelist-nav-virtual-tour-placeholder", navVirtualTour);
+        		}
+    			
     			
         		// Activate Modal
         		$($(this).data('target')).modal('show');
         		
         		$('.modal').on('shown.bs.modal', function(e) {
+        			
+       			  var $modal = $(this);
+       	            $modal.off('shown.bs.modal'); //fix bug with modal gallery
+        		    
         			$(this).find('.modal-dialog').empty().append($modalContent);
+        			var numTab = 0; 
+        			if (assetSelectionReference != null) {
+            			$(".c-suite-detail-modal #descr-tab").removeClass("hidden");
+            			$(".c-suite-detail-modal #descr-tab").addClass("active");
+        				numTab++;
+        			}
+        			if (suitePlan != null) {
+            			$(".c-suite-detail-modal #suite-tab").removeClass("hidden");
+            			if (numTab == 0) {
+            				$(".c-suite-detail-modal #suite-tab").addClass("active");
+            			}
+            			numTab++;
+            		}
+        			if (navFeatures != null) {
+                		$(".c-suite-detail-modal #features-tab").removeClass("hidden");
+            			if (numTab == 0) {
+            				$(".c-suite-detail-modal #features-tab").addClass("active");
+            			}
+            			numTab++;
+            		}
+        			
+            		if (locationImage != null) {
+	            		$(".c-suite-detail-modal #location-tab").removeClass("hidden");
+	        			if (numTab == 0) {
+	        				$(".c-suite-detail-modal #location-tab").addClass("active");
+	        			}
+	        			numTab++;
+            		}
+            		
+            		if (virtualTourImage != null) {
+	            		$(".c-suite-detail-modal #virtualtour-tab").removeClass("hidden");
+	        			if (numTab == 0) {
+	        				$(".c-suite-detail-modal #virtualtour-tab").addClass("active");
+	        			}
+	        			numTab++;
+            		}
+            		
+            		$(".c-suite-detail-modal-navtabs li").css("width", (100 / numTab) + "%");
+        			
         			 var $slideFor = $(this).find('.c-slider').slick({
-                           slidesToShow : 1,
-                           slidesToScroll : 1
-                       });
+                       slidesToShow : 1,
+                       slidesToScroll : 1
+                    });
+        			// Show / calc counter
+                    var slideTotalItem = $slideFor.find('.slick-slide:not(.slick-cloned)').length;
+                    $('.modal').find('.c-suite-detail-modal-description__counter .slide-item-current').html(1);
+                    // Set total number of slide
+                    $('.modal').find('.c-suite-detail-modal-description__counter .slide-item-total').html(slideTotalItem);
+                    
+                    $slideFor.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+                        var $slider = $(this);
+                        // Set counter according to the current slide
+                        $('.modal').find('.c-suite-detail-modal-description__counter .slide-item-current').html(nextSlide + 1);
+
+                        //$video.find('.s7playpausebutton[selected="false"]').trigger('click');
+                        //$video.attr('class', 'c-video').empty();
+                    });
+                    
+ 
         			$(this).find(".show-deck-image").on("click", function(e) {	
 				    	e.preventDefault();
 				    	
@@ -181,6 +263,44 @@ $(function() {
 				    	$("#suite-location #"+idDeckImage).css("display", "block");
 				    	
 				    });
+        			
+        			$(this).find("#descr-tab").on("click", function(e) {
+        				//workaround for bug virtual tour full screen
+        				if ($slideFor != null) {
+        					$slideFor.slick('slickGoTo', 0);
+        				}
+        			});
+        			
+        			$(this).find(".c-suite-detail-virtual-tour-lightbox").on("click", function(e) {
+    			        e.preventDefault();
+    			        if (!window.hasOwnProperty('virtualTour') || window.virtualTour == null) {
+    			        	var imagePath = $("#c-suite-detail-modal-virtual-tour-containerDiv").data().image;
+    			     		var intervalDiv = setInterval(function(){
+    			    			if ($("#virtualtour-tab").hasClass("active")) {
+    			    				clearInterval(intervalDiv);
+    			    				if (imagePath != null) {
+    			    					window.virtualTour = PhotoSphereViewer({
+    	    			        			container: 'c-suite-detail-modal-virtual-tour-containerDiv',
+    			    						panorama: imagePath,
+    			    						anim_speed: '0.4rpm',
+    			    						move_speed: 1.0,
+    			    						time_anim: '1000',
+    			    						min_fov: 10,
+    			    						default_fov: 179,
+    			    						navbar: [
+    			    							'autorotate', 
+    			    							'zoom',
+    			    							'spacer-1',
+    			    							'caption',
+    			    							'gyroscope',
+    			    							'fullscreen'
+    			    							]
+    			    					});
+    			    				}
+    			    			} 
+    			    		},500);
+    					}
+    			    });
         		});
         	}
         }
