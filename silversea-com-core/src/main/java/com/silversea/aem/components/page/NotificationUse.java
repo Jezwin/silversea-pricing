0162ba7008efc77f1ef87658c1c166e2aa8c44f7
Nikhil <nikhil.singhal1990@gmail.com>
2018-02-13 12:13:23 +0530

    Using ssc-importer instead of anonymous resolver to read through /var sensing permissions issues.

--- silversea-com-core/src/main/java/com/silversea/aem/components/page/NotificationUse.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/page/NotificationUse.java
@@ -1,15 +1,22 @@
 package com.silversea.aem.components.page;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.jcr.LoginException;
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
 
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.adobe.cq.sightly.WCMUsePojo;
+import com.silversea.aem.importers.ImportersConstants;
 
 /**
  * The file to display the relevant details to be submitted on the notification
@@ -26,15 +33,28 @@ public class NotificationUse extends WCMUsePojo {
 	
 	@Override
 	public void activate() throws Exception {
-		leadData = new ArrayList<String>();
-		Node rootNode = getResourceResolver().getResource(LEAD_DATA_PATH).adaptTo(Node.class);
-		
-		NodeIterator childrenNodes = rootNode.getNodes();
-
-		while (childrenNodes.hasNext()) {
-			Node next = childrenNodes.nextNode();
-			leadData.add(next.getName());
-		}
+		final Map<String, Object> authenticationParams = new HashMap<>();
+		authenticationParams.put(ResourceResolverFactory.SUBSERVICE,
+				ImportersConstants.SUB_SERVICE_IMPORT_DATA);
+		try (final ResourceResolver adminResolver = getSlingScriptHelper().getService(ResourceResolverFactory.class)
+				.getServiceResourceResolver(authenticationParams)) {
+			
+			if (null != adminResolver) {
+				leadData = new ArrayList<String>();
+				Node rootNode = adminResolver.getResource(LEAD_DATA_PATH).adaptTo(Node.class);
+				NodeIterator childrenNodes = rootNode.getNodes();
+				while (childrenNodes.hasNext()) {
+					Node next = childrenNodes.nextNode();
+					leadData.add(next.getName());
+				} 
+			}
+		} catch (LoginException loginException) {
+			LOGGER.error(
+					"Exception while retrieving admin access for Resolver: {}",
+					loginException.getMessage());
+		} catch (RepositoryException repositoryException) {
+			LOGGER.error("Repository Exception: {}", repositoryException);
+		} 		
 		LOGGER.debug("The current lead data is:- {}", leadData);
 	}
 
