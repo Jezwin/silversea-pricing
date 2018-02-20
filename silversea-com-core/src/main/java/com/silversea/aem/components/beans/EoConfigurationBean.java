90c959d290a1ee5e5d179a94b72dd89f8784c642
Giuseppe Scaramuzzino <giuseppes@silversea.com>
2018-02-20 15:37:35 +0100

    add remaing option in EoConfigurationBean

--- silversea-com-core/src/main/java/com/silversea/aem/components/beans/EoConfigurationBean.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/beans/EoConfigurationBean.java
@@ -8,6 +8,14 @@ public class EoConfigurationBean {
 	private boolean descriptionMain;
 	private boolean shortDescriptionMain;
 	
+	private boolean activeGreysSystem;
+	private boolean imageLightbox;
+	private boolean titleLigthbox;
+	private boolean descriptionLigthbox;
+	
+	private boolean titleVoyage;
+	private boolean descriptionVoyage;
+	
 	private boolean descriptionTnC;
 
 	public boolean isActiveSystem() {
@@ -16,6 +24,38 @@ public class EoConfigurationBean {
 
 	public void setActiveSystem(boolean activeSystem) {
 		this.activeSystem = activeSystem;
+	}
+
+	public boolean isTitleLigthbox() {
+		return titleLigthbox;
+	}
+
+	public void setTitleLigthbox(boolean titleLigthbox) {
+		this.titleLigthbox = titleLigthbox;
+	}
+
+	public boolean isDescriptionLigthbox() {
+		return descriptionLigthbox;
+	}
+
+	public void setDescriptionLigthbox(boolean descriptionLigthbox) {
+		this.descriptionLigthbox = descriptionLigthbox;
+	}
+
+	public boolean isTitleVoyage() {
+		return titleVoyage;
+	}
+
+	public void setTitleVoyage(boolean titleVoyage) {
+		this.titleVoyage = titleVoyage;
+	}
+
+	public boolean isDescriptionVoyage() {
+		return descriptionVoyage;
+	}
+
+	public void setDescriptionVoyage(boolean descriptionVoyage) {
+		this.descriptionVoyage = descriptionVoyage;
 	}
 
 	public boolean isTitleMain() {
@@ -48,6 +88,22 @@ public class EoConfigurationBean {
 
 	public void setDescriptionTnC(boolean descriptionTnC) {
 		this.descriptionTnC = descriptionTnC;
+	}
+	
+	public boolean isActiveGreysSystem() {
+		return activeGreysSystem;
+	}
+
+	public void setActiveGreysSystem(boolean activeGreysSystem) {
+		this.activeGreysSystem = activeGreysSystem;
+	}
+
+	public boolean isImageLightbox() {
+		return imageLightbox;
+	}
+
+	public void setImageLightbox(boolean imageLightbox) {
+		this.imageLightbox = imageLightbox;
 	}
 	
 
