2ba3e44504410bc7e938762458447cfa9c3b38ea
Giuseppe Scaramuzzino <giuseppes@silversea.com>
2018-03-07 18:54:26 +0100

    - create modaldetail bean to fetch and display the right informations

---
+++ silversea-com-core/src/main/java/com/silversea/aem/components/beans/ModalDetailBean.java
@@ -0,0 +1,69 @@
+package com.silversea.aem.components.beans;
+
+public class ModalDetailBean {
+
+	private String title;
+	private String longDescription;
+	private String assetSelectionReference;
+	private String bedroomsInformation;
+	private String plan;
+	private String locationImage;
+	private String virtualTour;
+	private String[] features;
+	private String fileReference;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getLongDescription() {
+		return longDescription;
+	}
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
+	public String getAssetSelectionReference() {
+		return assetSelectionReference;
+	}
+	public void setAssetSelectionReference(String assetSelectionReference) {
+		this.assetSelectionReference = assetSelectionReference;
+	}
+	public String getBedroomsInformation() {
+		return bedroomsInformation;
+	}
+	public void setBedroomsInformation(String bedroomsInformation) {
+		this.bedroomsInformation = bedroomsInformation;
+	}
+	public String getPlan() {
+		return plan;
+	}
+	public void setPlan(String plan) {
+		this.plan = plan;
+	}
+	public String getLocationImage() {
+		return locationImage;
+	}
+	public void setLocationImage(String locationImage) {
+		this.locationImage = locationImage;
+	}
+	public String getVirtualTour() {
+		return virtualTour;
+	}
+	public void setVirtualTour(String virtualTour) {
+		this.virtualTour = virtualTour;
+	}
+	public String[] getFeatures() {
+		return features;
+	}
+	public void setFeatures(String[] features) {
+		this.features = features;
+	}
+	public String getFileReference() {
+		return fileReference;
+	}
+	public void setFileReference(String fileReference) {
+		this.fileReference = fileReference;
+	}
+}
--- silversea-com-core/src/main/java/com/silversea/aem/components/included/ModalDetailUse.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/included/ModalDetailUse.java
@@ -1,17 +1,58 @@
 package com.silversea.aem.components.included;
 
 import com.adobe.cq.sightly.WCMUsePojo;
+import com.silversea.aem.components.beans.ModalDetailBean;
+import com.silversea.aem.models.DiningModel;
+import com.silversea.aem.models.PublicAreaModel;
+import com.silversea.aem.models.SuiteVariationModel;
 
 public class ModalDetailUse extends WCMUsePojo {
-	
+
+	private ModalDetailBean detail;
+
 	@Override
 	public void activate() throws Exception {
-		
-	}
-	
-	public String getC() {
-		return getCurrentPage().getPath();
-	}
-	
 
+		if (getCurrentPage().getPath().contains("/suites/")) {
+			SuiteVariationModel suiteVariation = getCurrentPage().adaptTo(SuiteVariationModel.class);
+			if (suiteVariation != null) {
+				detail = new ModalDetailBean();
+				detail.setTitle(suiteVariation.getTitle());
+				detail.setLongDescription(suiteVariation.getLongDescription());
+				detail.setBedroomsInformation(suiteVariation.getBedroomsInformation());
+				detail.setAssetSelectionReference(suiteVariation.getAssetSelectionReference());
+				detail.setPlan(suiteVariation.getPlan());
+				detail.setLocationImage(suiteVariation.getLocationImage());
+				detail.setVirtualTour(suiteVariation.getVirtualTour());
+				detail.setFileReference(suiteVariation.getSuiteReference());
+				detail.setFeatures(suiteVariation.getFeatures());
+			}
+		} else if (getCurrentPage().getPath().contains("/dinings/")) {
+			PublicAreaModel publicArea = getCurrentPage().adaptTo(PublicAreaModel.class);
+			if (publicArea != null) {
+				detail = new ModalDetailBean();
+				detail.setTitle(publicArea.getTitle());
+				detail.setLongDescription(publicArea.getLongDescription());
+				detail.setAssetSelectionReference(publicArea.getAssetSelectionReference());
+				detail.setVirtualTour(publicArea.getVirtualTour());
+				detail.setFileReference(publicArea.getThumbnail());
+			}
+		} else if (getCurrentPage().getPath().contains("/public-areas/")) {
+			DiningModel dining = getCurrentPage().adaptTo(DiningModel.class);
+			if (dining != null) {
+				detail = new ModalDetailBean();
+				detail.setTitle(dining.getTitle());
+				detail.setLongDescription(dining.getLongDescription());
+				detail.setAssetSelectionReference(dining.getAssetSelectionReference());
+				detail.setVirtualTour(dining.getVirtualTour());
+				detail.setFileReference(dining.getThumbnail());
+			}
+		}
+
+	}
+
+	public ModalDetailBean getDetail() {
+		return detail;
+	}
+	
 }
