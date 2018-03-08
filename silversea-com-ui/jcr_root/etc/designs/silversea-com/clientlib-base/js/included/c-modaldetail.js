7cc66d5823945ca3d2bfc5e89c9c3cd69c4401a4
Giuseppe Scaramuzzino <giuseppes@silversea.com>
2018-03-08 18:25:44 +0100

    - add security check in js

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/included/c-modaldetail.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/included/c-modaldetail.js
@@ -1,11 +1,5 @@
 $(function() {
-	$(window).on('resize', function() {
-		var href = $(".c-modaldetail-link").data("href");
-		var target = $(".c-modaldetail-link").data("target");
-		$.get(href, function(data) {
-			$(target + " .modal-content").html(data);
-		});
-	});
+	
 
 });
 
