cd51ccd4eb5708193d9ac789236a8b963341c5e1
Giuseppe Scaramuzzino <giuseppes@silversea.com>
2018-03-09 12:33:03 +0100

    - add logic to avoid issue with scroll ios

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/included/c-modaldetail.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/included/c-modaldetail.js
@@ -1,4 +1,78 @@
 $(function () {
+	
+	window.linkEventToModalDetailMobile = function linkEventToModalDetailMobile() {
+		 var modalBody = $(".modal-body");
+	      //only mobile
+        if (modalBody.hasClass("automatic-modal-body-modal-detail-mobile")) {
+			$(".automatic-modal-body-modal-detail-mobile").parent().parent().parent().css("display", "block"); //remove when modal is close
+			$(".automatic-modal-body-modal-detail-mobile").parent().parent().parent().css("overflow-y", "hidden"); //remove when modal is close
+			$(".automatic-modal-body-modal-detail-mobile").css("overflow-y", "scroll");
+			$(".automatic-modal-body-modal-detail-mobile").css("max-height", "100vh");
+			
+        	if(window.scrollSupport != null && window.scrollSupport) { 
+  				window.iNoBounce.enable();
+  			}
+  			
+            //action for view all view less features
+            $(this).find(".modal-detail-features-expand a.view_all").on("click", function (e) {
+                e.preventDefault();
+                $('.expand-ul li:nth-of-type(1n+10)').stop().css('display', 'list-item').hide().slideDown();
+                $(".modal-detail-features-expand a.view_less").show();
+                $(this).hide();
+            });
+
+            $(this).find(".modal-detail-features-expand a.view_less").on("click", function (e) {
+                e.preventDefault();
+                $(".modal-detail-features-expand a.view_all").show();
+                $('.expand-ul li:nth-of-type(1n+10)').stop().slideUp();
+                $(this).hide();
+            });
+            /*$(this).find(".close").on("click", function (e) {
+                e.preventDefault();
+                $('.modal').modal('hide');
+            });*/
+        }
+	};//linkEventToModalDetailMobile
+	
+	window.linkEventToModalDetailDesktop = function() {
+		var modalBody = $(".modal-body");
+	    //only desktop
+        if (modalBody.hasClass("automatic-modal-body-modal-detail-desktop")) {
+            //create virtual tour on desktop
+            $(this).find(".c-modal-detail-virtual-tour-lightbox").on("click", function (e) {
+                e.preventDefault();
+                if (!window.hasOwnProperty('virtualTour') || window.virtualTour == null) {
+                    var imagePath = $("#c-modal-detail-modal-virtual-tour-containerDiv").data().image;
+                    var intervalDiv = setInterval(function () {
+                        if ($("#virtualtour-tab").hasClass("active")) {
+                            clearInterval(intervalDiv);
+                            if (imagePath != null) {
+                                window.virtualTour = PhotoSphereViewer({
+                                    container: 'c-modal-detail-modal-virtual-tour-containerDiv',
+                                    panorama: imagePath,
+                                    anim_speed: '0.4rpm',
+                                    move_speed: 1.0,
+                                    time_anim: '1000',
+                                    min_fov: 10,
+                                    usexmpdata: false,
+                                    default_fov: 179,
+                                    navbar: [
+	    		    							'autorotate',
+	    		    							'zoom',
+	    		    							'spacer-1',
+	    		    							'caption',
+	    		    							'gyroscope',
+	    		    							'fullscreen'
+	    		    							]
+                                });
+                            }
+                        }
+                    }, 500);
+                }
+            });
+        }
+	};//linkEventToModalDetailDesktop
+	
     $(window).on('resize', function () {
         var $modal = $(".modal");
         var $modalBody = $modal.find(".modal-body");
@@ -6,6 +80,7 @@ $(function () {
 
             var href = $(".c-modaldetail-link").data("href");
             var target = $(".c-modaldetail-link").data("target");
+            $(target + " .modal-content").empty();
             $.get(href, function (data) {
                 $(target + " .modal-content").html(data);
             });
@@ -48,73 +123,6 @@ $(function () {
                     $slideFor.slick('slickGoTo', 0);
                 }
             });
-
-            //only mobile
-            if (modalBody.hasClass("automatic-modal-body-modal-detail-mobile")) {
-				$(".automatic-modal-body-modal-detail-mobile").parent().parent().parent().css("display", "block"); //remove when modal is close
-				$(".automatic-modal-body-modal-detail-mobile").parent().parent().parent().css("overflow-y", "hidden"); //remove when modal is close
-				$(".automatic-modal-body-modal-detail-mobile").css("overflow-y", "scroll");
-				$(".automatic-modal-body-modal-detail-mobile").css("max-height", "100vh");
-				
-            	if(window.scrollSupport != null && window.scrollSupport) { 
-      				window.iNoBounce.enable();
-      			}
-      			
-                //action for view all view less features
-                $(this).find(".modal-detail-features-expand a.view_all").on("click", function (e) {
-                    e.preventDefault();
-                    $('.expand-ul li:nth-of-type(1n+10)').stop().css('display', 'list-item').hide().slideDown();
-                    $(".modal-detail-features-expand a.view_less").show();
-                    $(this).hide();
-                });
-
-                $(this).find(".modal-detail-features-expand a.view_less").on("click", function (e) {
-                    e.preventDefault();
-                    $(".modal-detail-features-expand a.view_all").show();
-                    $('.expand-ul li:nth-of-type(1n+10)').stop().slideUp();
-                    $(this).hide();
-                });
-                $(this).find(".close").on("click", function (e) {
-                    e.preventDefault();
-                    $('.modal').modal('hide');
-                });
-            }
-
-            //only desktop
-            if (modalBody.hasClass("automatic-modal-body-modal-detail-desktop")) {
-                //create virtual tour on desktop
-                $(this).find(".c-modal-detail-virtual-tour-lightbox").on("click", function (e) {
-                    e.preventDefault();
-                    if (!window.hasOwnProperty('virtualTour') || window.virtualTour == null) {
-                        var imagePath = $("#c-modal-detail-modal-virtual-tour-containerDiv").data().image;
-                        var intervalDiv = setInterval(function () {
-                            if ($("#virtualtour-tab").hasClass("active")) {
-                                clearInterval(intervalDiv);
-                                if (imagePath != null) {
-                                    window.virtualTour = PhotoSphereViewer({
-                                        container: 'c-modal-detail-modal-virtual-tour-containerDiv',
-                                        panorama: imagePath,
-                                        anim_speed: '0.4rpm',
-                                        move_speed: 1.0,
-                                        time_anim: '1000',
-                                        min_fov: 10,
-                                        usexmpdata: false,
-                                        default_fov: 179,
-                                        navbar: [
-		    		    							'autorotate',
-		    		    							'zoom',
-		    		    							'spacer-1',
-		    		    							'caption',
-		    		    							'gyroscope',
-		    		    							'fullscreen'
-		    		    							]
-                                    });
-                                }
-                            }
-                        }, 500);
-                    }
-                });
-            }
         }
     });
 });
