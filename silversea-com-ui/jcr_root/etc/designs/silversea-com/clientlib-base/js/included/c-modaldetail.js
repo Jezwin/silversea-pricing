73265d2cb0e7ebeb2efb74175d861ca7aea43fbb
Sylvain Mura <sylvainm@silversea.com>
2018-03-14 18:05:20 +0100

    Changelog :  - Fix slider init on new Ship revamp modal

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/included/c-modaldetail.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/included/c-modaldetail.js
@@ -117,19 +117,34 @@ $(function () {
         }
     });
  
-    $('.modal').on('shown.bs.modal', function (e) {
-        var $modal = $(this);
-        var modalBody = $modal.find(".modal-body");
-        
-        if (modalBody.hasClass("automatic-modal-body-modal-detail")) {
-            $('html').addClass("no-scroll-html");
-            $('body').addClass("no-scroll-body");
+    $('.modal-detail-link-ajax').on('click', function(e) {
+        e.preventDefault();
+        var $link = $(this),
+            ajaxContentPath = $link.attr('href'),
+            modalTarget = $link.data('target'),
+            $modalContent = $(modalTarget);
 
-            window.location.hash = "#modal";
-			
-            createSlider(modalBody);
-        }
+        // Activate Modal
+        $modalContent.modal('show');
+
+        // Wait for modal opening
+        $modalContent.on('shown.bs.modal', function(e) {
+            var $modal = $(this);
+            $modal.off('shown.bs.modal');
+
+            // Append html response inside modal
+            $modal.find('.modal-content').load(ajaxContentPath, function() {
+                var modalBody = $modal.find(".modal-body");
+            	 $('html').addClass("no-scroll-html");
+                 $('body').addClass("no-scroll-body");
+
+                 window.location.hash = "#modal";
+                 history.pushState(null, null, "#modal"); // push state that hash into the url
+                 createSlider(modalBody);
+            });
+        });
     });
+    
     
     function createSlider(modalBody){
     	//create slider
@@ -172,7 +187,6 @@ $(function () {
         	$(this).addClass("activeDeck");
         	$("#deck-"+id).removeClass("hidden");
         });
-
         
     };//createSlider
 
