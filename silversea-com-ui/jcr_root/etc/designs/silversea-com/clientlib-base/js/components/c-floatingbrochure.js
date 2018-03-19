df174897410e65465622f6791d97bb67e1d5af61
Nikhil <nikhil.singhal1990@gmail.com>
2018-03-19 13:27:32 +0530

    Changes to brochure : -
    1. i18n changes
    2. brochure link dynamic
    3. css selector dialog changes
    4. JS changes
    5. classic to touch ui changes

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-floatingbrochure.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-floatingbrochure.js
@@ -1,53 +1,72 @@
 $(document).ready(function(){
     if($(window).width() > 767){
+
         $('.floating-opener').off('click').on('click', function(){
             $(this).parent().toggleClass('visible');
         });
+
+        var $floatingBrochure = $('.floating-form'),
+            whitelisted_tags = $floatingBrochure.data('flyout-tag-whitelist'),
+            blacklist_tags = $floatingBrochure.data('flyout-tag-blacklist'),
+            minScroll = $floatingBrochure.data('scrolltop') || 0,
+            whitelisted_elems,
+            blacklist_elems,
+            timer;
     
+        if(!$floatingBrochure.length){return;}
+
         function isInView(element){
-            var pageTop = $(window).scrollTop();
-            var pageBottom = pageTop + $(window).height();
-            var elementTop = $(element).offset().top;
-            var elementBottom = elementTop + $(element).height();
-    
+            var pageTop = $(window).scrollTop(),
+                pageBottom = pageTop + $(window).height(),
+                elementTop = $(element).offset().top,
+                elementBottom = elementTop + $(element).height();
+
             return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
         }
-        var whitelisted_tags = $('[data-flyout-tag-list]').data('flyout-tag-list'),
-            whitelisted_elems,
-            timer,
-            isInViewport;
-        if(whitelisted_tags || $.trim(whitelisted_tags) === ''){
-            if($.trim(whitelisted_tags) === ''){
-                $('.floating-form').show();
-            } else {
-                whitelisted_tags = whitelisted_tags.split(',').map(function(item){
-                    return '.'+$.trim(item);
-                });
-                whitelisted_elems = $(whitelisted_tags.join(','));
-                $(window).scroll(function() {
-                    if(timer) {
-                        window.clearTimeout(timer);
-                    }
-                
-                    timer = window.setTimeout(function() {
-                        isInViewport = false;
-                        $.each(whitelisted_elems, function(){
-                            if(isInView(this)){
-                                isInViewport = true;
-                                return;
-                            }
-                        });
-                        if(isInViewport){
-                            $('.floating-form').show();
-                        } else {
-                            $('.floating-form').hide();
-                        }
-                    }, 100);
-                });
-            }
-        } else {
-            $('.floating-form').remove();
+
+        function getJqElems(tags){
+            return tags ? tags.split(',').map(function(item){
+                return '.'+$.trim(item);
+            }).join(',') : '';
         }
+
+        function areElemsInView(elems){
+            var isInViewport = false;
+            elems.length && $.each(elems, function(){
+                if(isInView(this)){
+                    isInViewport = true;
+                    return;
+                }
+            });
+            return isInViewport;
+        }
+
+        whitelisted_elems = getJqElems(whitelisted_tags);
+        blacklist_elems = getJqElems(blacklist_tags);
+
+        $(window).scroll(function() {
+            if(timer) {
+                window.clearTimeout(timer);
+            }
+        
+            timer = window.setTimeout(function() {
+                isInViewport = false;
+
+                if(blacklist_elems.length && areElemsInView($(blacklist_elems))){
+                    $floatingBrochure.hide();
+                } else { 
+                    if(whitelisted_elems.length && areElemsInView($(whitelisted_elems))){
+                        $floatingBrochure.show();
+                    } else {
+                        if(window.scrollY >= minScroll){
+                            $floatingBrochure.show();
+                        } else {
+                            $floatingBrochure.hide();
+                        }
+                    }
+                }
+            }, 100);
+        });
         $(window).trigger('scroll');
-	}
+ }
 });
