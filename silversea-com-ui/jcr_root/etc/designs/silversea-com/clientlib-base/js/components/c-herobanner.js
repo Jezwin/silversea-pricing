4e63cc20be31327ba28135f8489e39e454a7990f
Sylvain Mura <sylvainm@silversea.com>
2018-03-29 12:02:03 +0200

    Fix Video IG in HB

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-herobanner.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-herobanner.js
@@ -37,26 +37,28 @@ $(function() {
         });
     }
 
-    $('.c-hero-banner:not(.inline-gallery-included) .video-link').on('click', function(e) {
-        e.preventDefault();
-        var $link = $(this),
-            ajaxContentPath = $link.attr('href'),
-            modalTarget = $link.data('target'),
-            $modalContent = $(modalTarget);
-
-        // Activate Modal
-        $modalContent.modal('show');
-
-        // Wait for modal opening
-        $modalContent.on('shown.bs.modal', function(e) {
-            var $modal = $(this);
-            $modal.off('shown.bs.modal');
-
-            // Append html response inside modal
-            $modal.find('.modal-dialog').load(ajaxContentPath, function() {
-                // init Video after modal load
-                $(this).find('.c-video').initVideo();
-            });
-        });
+    $('.c-hero-banner .video-link').on('click', function(e) {
+    	if(!$($(".c-hero-banner")[0]).hasClass("inline-gallery-included") || $(window).width() > 767){
+	        e.preventDefault();
+	        var $link = $(this),
+	            ajaxContentPath = $link.attr('href'),
+	            modalTarget = $link.data('target'),
+	            $modalContent = $(modalTarget);
+	
+	        // Activate Modal
+	        $modalContent.modal('show');
+	
+	        // Wait for modal opening
+	        $modalContent.on('shown.bs.modal', function(e) {
+	            var $modal = $(this);
+	            $modal.off('shown.bs.modal');
+	
+	            // Append html response inside modal
+	            $modal.find('.modal-dialog').load(ajaxContentPath, function() {
+	                // init Video after modal load
+	                $(this).find('.c-video').initVideo();
+	            });
+	        });
+    	}
     });
 });
