6d5a0cdd9b03d0bcf77a6d0444ae694c90f4406f
Giuseppe Scaramuzzino <giuseppes@silversea.com>
2018-05-30 15:31:17 +0200

    - fix steve mccurry slider

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-steveandsilversea.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-steveandsilversea.js
@@ -4,11 +4,11 @@
 			if ($(".modal .c-steveandsilversea__gallery .modal-body").length > 0) {
 				clearInterval(t);
 				var counter = $(".modal .c-steveandsilversea__gallery .slide-item-total").html();
-				if (parseInt(counter) > 1) {
-					$(".modal .c-steveandsilversea__gallery  .c-gallery__counter").show();
-					$(".modal .c-steveandsilversea__gallery  .c-slider--nav .slick-list").css("visibility","visible");
+				if (parseInt(counter) == 1) {
+					$(".modal .c-steveandsilversea__gallery  .c-gallery__counter").hide();
+					$(".modal .c-steveandsilversea__gallery  .c-slider--nav").hide();
 				}
 			}
-		},100);
+		},10);
 	});
 })();
