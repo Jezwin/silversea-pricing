fb2d376c3fbf9a3da1483624e8902752d2bbeac4
Sylvain Mura <sylvainm@silversea.com>
2018-06-06 06:01:37 +0000

    Update c-search-cruise-hero.js

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-search-cruise-hero.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/components/c-search-cruise-hero.js
@@ -30,8 +30,12 @@ $(function() {
 
                     // Disabled option not available
                     $optionList.each(function() {
-                        var $option = $(this);
-                        $option.attr('disabled', filterAvailableObj[$option.val()] !== true);
+                        try {
+                            var $option = $(this);
+                            $option.attr('disabled', filterAvailableObj[$option.val()] !== true);
+                        } catch (error) {
+                            console.log('init issue');
+                        }
                     });
                 }
             });
