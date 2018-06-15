aa60deae566cf04742bd4901096f44669a682986
Giuseppe Scaramuzzino <giuseppes@silversea.com>
2018-06-15 11:27:30 +0200

    - add numberDays in ItineraryModel - calc the numberday from departDate - date in CruiseUse.java - add function in ListHelper.java to create a 'fake' htl loop - add new i18n Overnights in port

--- silversea-com-core/src/main/java/com/silversea/aem/helper/ListHelper.java
+++ silversea-com-core/src/main/java/com/silversea/aem/helper/ListHelper.java
@@ -1,5 +1,6 @@
 package com.silversea.aem.helper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.adobe.cq.sightly.WCMUsePojo;
@@ -8,15 +9,25 @@ import com.google.common.collect.Lists;
 public class ListHelper extends WCMUsePojo {
 
     private List<List<Object>> objectListGroup;
+    private String[] objectListLoop;
+
 
     @Override
     public void activate() throws Exception {
         Integer size = get("size", Integer.class);
         List<Object> assetList = get("list", List.class);
+        
+        Integer sizeLoop = get("sizeLoop", Integer.class);
+
 
         if (size != null && assetList != null) {
             objectListGroup = Lists.partition(assetList, size);
         }
+        
+        if(sizeLoop != null) {
+        	objectListLoop = new String[sizeLoop];
+        }
+        
 
     }
 
@@ -25,5 +36,12 @@ public class ListHelper extends WCMUsePojo {
      */
     public List<List<Object>> getObjectListGroup() {
         return objectListGroup;
+    }
+    
+    /**
+     * @return List of sub list
+     */
+    public String[] getObjectListLoop() {
+        return objectListLoop;
     }
 }
