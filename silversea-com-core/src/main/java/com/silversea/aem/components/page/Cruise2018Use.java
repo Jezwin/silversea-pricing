1825157827b3feb100a09419e3a6dc032c51d716
Fabio Filippi <fabiof@silversea.com>
2018-07-25 15:44:15 +0200

    Moved files. Using pagecontent and head.html

--- silversea-com-core/src/main/java/com/silversea/aem/components/page/Cruise2018Use.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/page/Cruise2018Use.java
@@ -1,6 +1,18 @@
 package com.silversea.aem.components.page;
 
 import com.silversea.aem.components.AbstractGeolocationAwareUse;
+import com.silversea.aem.models.CruiseModel;
 
 public class Cruise2018Use extends AbstractGeolocationAwareUse {
+
+    @Override
+    public void activate() throws Exception {
+        super.activate();
+    }
+
+    private CruiseModel cruiseModel;
+
+    public CruiseModel getCruiseModel() {
+        return cruiseModel;
+    }
 }
