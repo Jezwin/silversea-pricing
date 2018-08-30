d0705e0eb3c859ed83971c6aa81929b7b0551027
Giuseppe Scaramuzzino <giuseppes@silversea.com>
2018-08-30 10:57:59 +0200

    - refactor style and js ship slider

--- silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/pages/cruise2018/clientlibs/js/ship/ship-overview.js
+++ silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/pages/cruise2018/clientlibs/js/ship/ship-overview.js
@@ -1,37 +1,34 @@
 $(function () {
+    var $mainSlider = $('.ship-slider-overview .main-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
 
-    function createLigthboxGallerySlider() {
-        var $mainSlider = $('.lightbox-gallery-assets .main-slider').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1
-        });
+    // Init video on click
+    $('.ship-slider .video-link').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).next('.c-video').initVideo();
+    });
 
-        // Init video on click
-        $('.lightbox-gallery-assets .video-link').on('click', function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-            $(this).next('.c-video').initVideo();
-        });
+    $mainSlider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        var $slider = $(this);
+        // Kill video if current slide contains video
+        var $video = $slider.find('.slick-current .c-video');
+        $video.find('.s7playpausebutton[selected="false"]').trigger('click');
+        $video.attr('class', 'c-video').empty();
+        // Call lazy load for 2 previous and 2 next slides
+    }).on('afterChange', function (event, slick, currentSlide) {
+        // Call lazy load for 2 previous and 2 next slides
+        loadLazyImage($(this));
+    });
 
-        $mainSlider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-            var $slider = $(this);
-            // Kill video if current slide contains video
-            var $video = $slider.find('.slick-current .c-video');
-            $video.find('.s7playpausebutton[selected="false"]').trigger('click');
-            $video.attr('class', 'c-video').empty();
-            // Call lazy load for 2 previous and 2 next slides
-        }).on('afterChange', function (event, slick, currentSlide) {
-            // Call lazy load for 2 previous and 2 next slides
-            loadLazyImage($(this));
-        });
-
-        loadLazyImage($mainSlider);
-    };//createLigthboxGallerySlider
+    loadLazyImage($mainSlider);
 
     function loadLazyImage($slider) {
-        var $sliderActive = $slider.closest('.lightbox-gallery-assets').find('.slick-active');
-        $(".lightbox-gallery-assets .main-slider").slick("slickSetOption", "draggable", true, false);
-        $(".lightbox-gallery-assets .main-slider").slick("slickSetOption", "swipe", true, false);
+        var $sliderActive = $slider.closest('.ship-slider-overview').find('.slick-active');
+        $(".ship-slider-overview .main-slider").slick("slickSetOption", "draggable", true, false);
+        $(".ship-slider-overview .main-slider").slick("slickSetOption", "swipe", true, false);
         // call lazy loading for active image
         $sliderActive.find('.lazy').lazy();
 
@@ -45,6 +42,4 @@ $(function () {
         }, 50);
 
     };//loadLazyImage
-
-    createLigthboxGallerySlider();
 });
