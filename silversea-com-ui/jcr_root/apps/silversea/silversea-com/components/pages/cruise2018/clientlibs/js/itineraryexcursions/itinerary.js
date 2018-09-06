07fb73995e5e668b10ca8839f8389b334ca2172c
Giuseppe Scaramuzzino <giuseppes@silversea.com>
2018-09-06 15:57:30 +0200

    - fix DESKTOP SIL-1385: missing DAY label
    missing cursor pointer inside arrow
    
    missing click on row to show the detail
    
    hide line when expand the detail
    
    fix style title excursion
    
    remove height 95px and put padding-top, padding-bottom 15px
    
    fix hover view all and close
    
    remove horizontal line last item timeline

--- silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/pages/cruise2018/clientlibs/js/itineraryexcursions/itinerary.js
+++ silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/pages/cruise2018/clientlibs/js/itineraryexcursions/itinerary.js
@@ -2,7 +2,7 @@ $(function () {
 
     function toggle(e) {
         e && e.preventDefault();
-        const $this = $(this);
+        var  $this = $(this);
         if ($this.data('ssc-accordion') === 'show') {
             $this.parent().find('.cruise-2018-excursions-accordion').slideDown(200, 'linear');
             $this.data('ssc-accordion', 'hide');
@@ -16,20 +16,22 @@ $(function () {
 
     function toggleExcursions(e) {
         e && e.preventDefault();
-        const $this = $(this);
-        if ($this.data('ssc-accordion') === 'show') {
-            $this.parent().parent().find('.cruise-2018-itineraries-excursions-accordion').slideDown(200, 'linear');
-            $this.data('ssc-accordion', 'hide');
-        } else if ($this.data('ssc-accordion') === 'hide') {
-            $this.parent().parent().find('.cruise-2018-itineraries-excursions-accordion').slideUp();
-            $this.data('ssc-accordion', 'show');
+        var  $this = $(this);
+        var $arrow = $this.find(".cruise-2018-itineraries-itinerary-row-date-arrow");
+        if ($arrow.data('ssc-accordion') === 'show') {
+            $this.parent().find('.cruise-2018-itineraries-excursions-accordion').slideDown(200, 'linear');
+            $arrow.data('ssc-accordion', 'hide');
+            $this.css("border-bottom-color","transparent");
+        } else if ($arrow.data('ssc-accordion') === 'hide') {
+            $this.parent().find('.cruise-2018-itineraries-excursions-accordion').slideUp();
+            $arrow.data('ssc-accordion', 'show');
+            $this.css("border-bottom-color","rgba(152, 152, 155, 0.5)");
         }
-        $this.blur();
-        $this.find('i').toggle();
-
+        $arrow.blur();
+        $arrow.find('i').toggle();
     }
 
     $(".cruise-2018-itineraries-port-excursions-btn").on('click touchstart', toggle);
 
-    $(".cruise-2018-itineraries-itinerary-row-date-arrow").on('click touchstart', toggleExcursions);
+    $(".cruise-2018-itineraries-itinerary-row-container-with-excursion").on('click touchstart', toggleExcursions);
 });
