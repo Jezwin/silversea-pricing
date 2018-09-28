92af51583d0eb22b5c0909d83dc3a6b7e94a081f
Fabio Filippi <fabiof@silversea.com>
2018-09-28 15:02:28 +0200

    Fixed footer sticky overview and margin

--- silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/pages/cruise2018/clientlibs/js/footer.js
+++ silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/pages/cruise2018/clientlibs/js/footer.js
@@ -4,39 +4,25 @@ function Utils() {
 
 Utils.prototype = {
     constructor: Utils,
-    isElementInView: function (element, fullyInView) {
-        if (element != null && element.length > 0) {
-            var isVisible = false;
-            for (var i = 0; i < element.length; i++) {
-                var item = element[i];
-                var pageTop = $(window).scrollTop();
-                var pageBottom = pageTop + $(window).height();
-                if ($(item).offset() != null) {
-                    var elementTop = $(item).offset().top;
-                    var elementBottom = elementTop + $(item).height();
-                    if (fullyInView === true) {
-                        isVisible = ((pageTop < elementTop) && (pageBottom > elementBottom));
-                    } else {
-                        isVisible = ((elementTop <= pageBottom) && (elementBottom >= pageTop));
-                    }
-                    if (isVisible && fullyInView) {
-                        return isVisible;
-                    }
-                }
-            }
-            return isVisible;
-        }
+    isScrolledIntoView: function isScrolledIntoView(elem, offset) {
+        var docViewTop = $(window).scrollTop() + this.headerHeight() + $(".ssc-sticky-top").height() + offset;
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).outerHeight(true);
+        return ((elemBottom >= docViewTop) && (docViewTop >= elemTop));
+    },
+    headerHeight: function headerHeight() {
+        return $("#header").height() + $(".c-main-nav__bottom").height();
     }
 };
 
-var Utils = new Utils();
+var utils = new Utils();
 
 function onScrollFixedFooter() {
     if ($(window).width() < 768) {
-        var $selectForAllRaq = $("[data-sscclicktype=clic-RAQ]:not(#fixed-footer-2018-raq)");
+        var $selectForAllRaq = $("[data-sscclicktype=clic-RAQ]:not(#fixed-footer-2018-raq), #cruise2018overview");
         //different behaviour if we want to play only witht the first raq
         var $selectTheFirstRaq = $("#cruise-2018-rqa:not(#fixed-footer-2018-raq)");
-        var isElementInView = Utils.isElementInView($selectForAllRaq, true);
+        var isElementInView = utils.isScrolledIntoView($selectForAllRaq, 50);
         var $footerfixed = $(".fixed-footer-cruise");
         if (!isElementInView && !$footerfixed.is(":visible")) {
             $footerfixed.slideDown();
