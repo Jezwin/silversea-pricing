a4f621d3056abad76a2104d7fdd6975b674406bd
Fabio Filippi <fabiof@silversea.com>
2018-10-15 17:53:21 +0200

    First version withtout frontend test

--- silversea-com-core/src/main/java/com/silversea/aem/components/editorial/findyourcruise2018/FilterLabel.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/editorial/findyourcruise2018/FilterLabel.java
@@ -5,27 +5,32 @@ import com.silversea.aem.models.CruiseModelLight;
 import com.silversea.aem.models.FeatureModelLight;
 import com.silversea.aem.models.PortItem;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
 import java.util.function.Function;
 
-import static java.util.stream.Collectors.joining;
+import static java.util.Collections.singleton;
+import static java.util.stream.Collectors.toSet;
 
 public enum FilterLabel {
-    DESTINATION("destination", cruise -> cruise.getDestination().getName()),
-    DEPARTURE("departure", cruise -> "TODO"),//TODO
-    DURATION("duration", CruiseModelLight::getDuration),
-    SHIP("ship", cruise -> cruise.getShip().getName()),
-    TYPE("type", CruiseModelLight::getCruiseType),
+    DESTINATION("destination", cruise -> singleton(cruise.getDestination().getName())),
+    DEPARTURE("departure", cruise -> singleton("TODO")),//TODO
+    DURATION("duration", cruiseModelLight -> singleton(cruiseModelLight.getDuration())),
+    SHIP("ship", cruise -> singleton(cruise.getShip().getName())),
+    TYPE("type", cruiseModelLight -> singleton(cruiseModelLight.getCruiseType())),
     PORT("port", cruise -> cruise.getPorts().stream().map(PortItem::getName).filter(name -> !"day-at-sea".equals(name))
-            .collect(joining(":"))),
+            .collect(toSet())),
     FEATURES("features", cruise -> cruise.getFeatures().stream().map(FeatureModelLight::getName)
-            .filter(string -> !Strings.isNullOrEmpty(string)).collect(joining(":")));
+            .filter(string -> !Strings.isNullOrEmpty(string)).collect(toSet()));
 
     private String param;
     private String label;
+    private Function<CruiseModelLight, Set<String>> mapper;
 
-    private Function<CruiseModelLight, String> mapper;
+    public static final Collection<FilterLabel> VALUES = Arrays.asList(values());
 
-    FilterLabel(String label, Function<CruiseModelLight, String> mapper) {
+    FilterLabel(String label, Function<CruiseModelLight, Set<String>> mapper) {
         this.param = label;
         this.label = label;
         this.mapper = mapper;
@@ -44,7 +49,8 @@ public enum FilterLabel {
         return label;
     }
 
-    public Function<CruiseModelLight, String> getMapper() {
+    public Function<CruiseModelLight, Set<String>> getMapper() {
         return mapper;
     }
+
 }
