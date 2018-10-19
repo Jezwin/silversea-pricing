f174caeb29e2ea0557717cb91657ad9202c0d5a5
Fabio Filippi <fabiof@silversea.com>
2018-10-19 15:45:26 +0200

    Added dumm filters

--- silversea-com-core/src/main/java/com/silversea/aem/components/editorial/findyourcruise2018/AbstractFilter.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/editorial/findyourcruise2018/AbstractFilter.java
@@ -1,28 +1,25 @@
 package com.silversea.aem.components.editorial.findyourcruise2018;
 
 import com.google.common.base.Objects;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.silversea.aem.models.CruiseModelLight;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Stream;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.silversea.aem.components.editorial.findyourcruise2018.FilterRowState.*;
-import static java.util.Objects.requireNonNull;
-import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 
 public abstract class AbstractFilter<T> {
     private final String kind;
-    private List<FilterRow<T>> rows;
+    private Set<FilterRow<T>> rows;
 
     AbstractFilter(String kind) {
         this.kind = kind;
     }
 
-    protected abstract Collection<ValueLabel<T>> valueLabels(CruiseModelLight cruiseModelLight);
+    protected abstract Collection<FilterRow<T>> rows(CruiseModelLight cruiseModelLight);
 
 
     public Collection<FilterRow<T>> getRows() {
@@ -37,9 +34,9 @@ public abstract class AbstractFilter<T> {
         return rows.stream().anyMatch(row -> CHOSEN.equals(row.getState()));
     }
 
-    public final FilterRowState retrieveState(String label) {
-        return rows.stream().filter(row -> row.getLabel().equals(label)).findAny().map(FilterRow::getState)
-                .orElseThrow(() -> new IllegalArgumentException("Could not find " + label));
+    public final FilterRowState retrieveState(String key) {
+        return rows.stream().filter(row -> row.getKey().equals(key)).findAny().map(FilterRow::getState)
+                .orElseThrow(() -> new IllegalArgumentException("Could not find " + key));
     }
 
     final boolean matches(CruiseModelLight cruise) {
@@ -50,8 +47,8 @@ public abstract class AbstractFilter<T> {
         return true;
     }
 
-    void setLabels(Set<String> enabledLabels) {
-        rows.forEach(row -> row.setState(enabledLabels.contains(row.getLabel()) ? CHOSEN : ENABLED));
+    void setEnabled(Set<String> enabledKeys) {
+        rows.forEach(row -> row.setState(enabledKeys.contains(row.getKey()) ? CHOSEN : ENABLED));
     }
 
     void disableMissingLabels(Collection<CruiseModelLight> cruises) {
@@ -60,31 +57,28 @@ public abstract class AbstractFilter<T> {
     }
 
     final void initAllValues(List<CruiseModelLight> cruises) {
-        this.rows = cruises.stream().flatMap(this::extractRows).collect(toList());
+        this.rows =
+                cruises.stream().flatMap(cruise -> rows(cruise).stream())
+                        .collect(Collectors.toCollection(() -> new TreeSet<>(comparator())));//keep the order
+    }
+
+    protected Comparator<FilterRow<T>> comparator() {
+        return Comparator.naturalOrder();
     }
 
     private boolean matches(Set<FilterRow<T>> selectedRows, CruiseModelLight cruiseModelLight) {
-        //cartesian product, could be better
-        for (FilterRow<T> selectedRow : selectedRows) {
-            String selectedLabel = selectedRow.getLabel();
-            for (ValueLabel<T> cruiseRow : valueLabels(cruiseModelLight)) {
-                String cruiseLabel = cruiseRow.getLabel();
-                if (selectedLabel.equals(cruiseLabel)) {
-                    return true;
-                }
+        Collection<FilterRow<T>> cruiseRows = rows(cruiseModelLight);
+        for (FilterRow<T> cruiseRow : cruiseRows) {
+            if (selectedRows.contains(cruiseRow)) {
+                return true;
             }
         }
         return false;
     }
 
     private boolean cruiseMatchesTheRow(Collection<CruiseModelLight> cruises, FilterRow<T> row) {
-        return cruises.stream().flatMap(cruise -> valueLabels(cruise).stream()).map(ValueLabel::getLabel)
-                .anyMatch(row.getLabel()::equals);
-    }
-
-    private Stream<? extends FilterRow<T>> extractRows(CruiseModelLight cruise) {
-        return valueLabels(cruise).stream()
-                .map(valueLabel -> new FilterRow<>(valueLabel.getValue(), valueLabel.getLabel(), ENABLED));
+        return cruises.stream().flatMap(cruise -> rows(cruise).stream()).map(FilterRow::getKey)
+                .anyMatch(row.getKey()::equals);
     }
 
     @Override
@@ -100,33 +94,14 @@ public abstract class AbstractFilter<T> {
         return Objects.hashCode(kind);
     }
 
-
-    static class ValueLabel<T> {
-        private final String label;
-        private final T value;
-
-        ValueLabel(String label, T value) {
-            requireNonNull(value, "value must not be null, label was " + label);
-            requireNonNull(label, "Label must not be null,  value was of class " + value.getClass());
-            this.label = label;
-            this.value = value;
-        }
-
-        static <T> Collection<ValueLabel<T>> singleton(String label, T value) {
-            return Collections.singletonList(new ValueLabel<>(label, value));
-        }
-
-        static Collection<ValueLabel<String>> singleton(String value) {
-            return Collections.singletonList(new ValueLabel<>(value, value));
-        }
-
-        public String getLabel() {
-            return label;
-        }
-
-        public T getValue() {
-            return value;
-        }
+    @Override
+    public String toString() {
+        return toJson().toString();
     }
 
+    public JsonElement toJson() {
+        JsonArray array = new JsonArray();
+        rows.forEach(row -> array.add(row.toJson()));
+        return array;
+    }
 }
--- silversea-com-core/src/main/java/com/silversea/aem/components/editorial/findyourcruise2018/FilterRow.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/editorial/findyourcruise2018/FilterRow.java
@@ -1,22 +1,38 @@
 package com.silversea.aem.components.editorial.findyourcruise2018;
 
 import com.google.common.base.Objects;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.util.Collections;
+import java.util.Set;
 
 import static com.silversea.aem.components.editorial.findyourcruise2018.FilterRowState.CHOSEN;
 import static com.silversea.aem.components.editorial.findyourcruise2018.FilterRowState.DISABLED;
 import static com.silversea.aem.components.editorial.findyourcruise2018.FilterRowState.ENABLED;
 
-public class FilterRow<T> {
+public class FilterRow<T> implements Comparable<FilterRow<T>> {
 
     private final T value;
     private final String label;
+    private final String key;
 
     private FilterRowState state;
 
     public FilterRow(T value, String label, FilterRowState state) {
         this.value = value;
         this.label = label;
+        this.key = label;
         this.state = state;
+    }
+
+    public FilterRow(T value, String label, String key, FilterRowState state) {
+        this.value = value;
+        this.label = label;
+        this.key = key;
+        this.state = state;
+
     }
 
     public T getValue() {
@@ -29,6 +45,10 @@ public class FilterRow<T> {
 
     public String getLabel() {
         return label;
+    }
+
+    public String getKey() {
+        return key;
     }
 
     public boolean isEnabled() {
@@ -47,9 +67,30 @@ public class FilterRow<T> {
         this.state = state;
     }
 
+    static <T> Set<FilterRow<T>> singleton(T value, String label) {
+        return singleton(value, label, label);
+    }
+
+    static Set<FilterRow<String>> singleton(String value) {
+        return singleton(value, value, value);
+    }
+
+    static <T> Set<FilterRow<T>> singleton(T value, String label, String key) {
+        return Collections.singleton(new FilterRow<>(value, label, key, ENABLED));
+    }
+
+
+    public JsonElement toJson() {
+        JsonObject json = new JsonObject();
+        json.addProperty("label", label);
+        json.addProperty("key", key);
+        json.addProperty("state", state.toString());
+        return json;
+    }
+
     @Override
     public int hashCode() {
-        return Objects.hashCode(value);
+        return Objects.hashCode(key);
     }
 
     @Override
@@ -57,6 +98,13 @@ public class FilterRow<T> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         FilterRow<?> filterRow = (FilterRow<?>) o;
-        return Objects.equal(label, filterRow.label);
+        return Objects.equal(key, filterRow.key);
     }
+
+    @Override
+    public int compareTo(FilterRow<T> other) {
+        return this.getKey().compareTo(other.getKey());
+    }
+
+
 }
