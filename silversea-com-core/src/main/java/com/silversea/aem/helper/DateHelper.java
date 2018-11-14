cffe963ebc1264af21212c26e16fd0bf9ba4071b
Fabio Filippi <fabiof@silversea.com>
2018-11-14 15:48:01 +0100

    Using request for language

--- silversea-com-core/src/main/java/com/silversea/aem/helper/DateHelper.java
+++ silversea-com-core/src/main/java/com/silversea/aem/helper/DateHelper.java
@@ -6,7 +6,8 @@ import java.util.Calendar;
 import java.util.Locale;
 import java.util.TimeZone;
 
-import org.apache.commons.lang3.StringUtils;
+import com.google.common.base.Strings;
+import com.silversea.aem.utils.CruiseUtils;
 
 import com.adobe.cq.sightly.WCMUsePojo;
 
@@ -16,72 +17,94 @@ import com.adobe.cq.sightly.WCMUsePojo;
  */
 public class DateHelper extends WCMUsePojo {
 
-	public String value;
+    private static final Locale BRAZIL = new Locale("pt", "BR");
+    private static final Locale SPAIN = new Locale("es");
+    public String value;
 
-	private Calendar calendar = Calendar.getInstance();
+    private Calendar calendar = Calendar.getInstance();
 
-	@Override
-	public void activate() throws Exception {
-		Calendar date = get("date", Calendar.class);
-		String format = get("format", String.class);
-		String month = get("month", String.class);
-		String localeParam = get("locale", String.class);
-		String time = get("time", String.class);
 
-		Locale locale = StringUtils.isNotBlank(localeParam) ? new Locale(localeParam)
-				: getCurrentPage().getLanguage(false);
-		SimpleDateFormat formatter;
+    private Locale getLocale(String localeParam) {
+        if (!Strings.isNullOrEmpty(localeParam)) {
+            return new Locale(localeParam);
+        }
+        String language = CruiseUtils.firstNonNull(LanguageHelper.getLanguage(getRequest()), "en");
+        switch (language) {
+            case "es":
+                return SPAIN;
+            case "pt-br":
+                return BRAZIL;
+            case "de":
+                return Locale.GERMAN;
+            case "fr":
+                return Locale.FRENCH;
+            case "en":
+            default:
+                return Locale.ENGLISH;
+        }
+    }
 
-		if (date != null && format != null) {
-			formatter = new SimpleDateFormat(format, locale);
-			formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
-			value = formatter.format(date.getTime());
-		}
+    @Override
+    public void activate() throws Exception {
+        Calendar date = get("date", Calendar.class);
+        String format = get("format", String.class);
+        String month = get("month", String.class);
+        String localeParam = get("locale", String.class);
+        String time = get("time", String.class);
+        Locale locale = getLocale(localeParam);
 
-		if (month != null) {
-			DateFormatSymbols symbols = new DateFormatSymbols(locale);
-			String[] monthNames = symbols.getMonths();
-			value = monthNames[Integer.parseInt(month) - 1];
-		}
-		if (time != null && !time.equalsIgnoreCase("")) {
-			value = convertTime(time, locale.getLanguage());
-		}
-	}
+        SimpleDateFormat formatter;
 
-	private String convertTime(String time, String language) {
-		String result = null;
-		if (language.equalsIgnoreCase("en")) {
-			String closingTime = time.replace(":", "");
-			calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(closingTime.substring(0, 2)));
-			// For display purposes only We could just return the last two substring or
-			// format Calender.MINUTE as shown below
-			calendar.set(Calendar.MINUTE, Integer.parseInt(closingTime.substring(2, 4)));
-			int hour = calendar.get(Calendar.HOUR);
-			String minute = String.format("%02d", calendar.get(Calendar.MINUTE));
+        if (date != null && format != null) {
+            formatter = new SimpleDateFormat(format, locale);
+            formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
+            value = formatter.format(date.getTime());
+        }
 
-			String AM_PM = calendar.get(Calendar.AM_PM) == 0 ? "AM" : "PM";
-			// workournd from 00:00pm to 12:00pm
-			if (hour == 00 && AM_PM.equalsIgnoreCase("PM")) {
-				hour = 12;
-			}
-			result = hour + ":" + minute + " " + AM_PM;
-		} else {
-			result = time;
-		}
-		return result;
-	}
+        if (month != null) {
+            DateFormatSymbols symbols = new DateFormatSymbols(locale);
+            String[] monthNames = symbols.getMonths();
+            value = monthNames[Integer.parseInt(month) - 1];
+        }
+        if (time != null && !time.equalsIgnoreCase("")) {
+            value = convertTime(time, locale.getLanguage());
+        }
+    }
 
-	public String getValue() {
-		return value;
-	}
+    private String convertTime(String time, String language) {
+        String result = null;
+        if (language.equalsIgnoreCase("en")) {
+            String closingTime = time.replace(":", "");
+            calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(closingTime.substring(0, 2)));
+            // For display purposes only We could just return the last two substring or
+            // format Calender.MINUTE as shown below
+            calendar.set(Calendar.MINUTE, Integer.parseInt(closingTime.substring(2, 4)));
+            int hour = calendar.get(Calendar.HOUR);
+            String minute = String.format("%02d", calendar.get(Calendar.MINUTE));
 
-	public static void main(String args[]) {
-		DateHelper dateHelper = new DateHelper();
-		String value = dateHelper.convertTime("12:00", "en");
-		System.out.println(value);
-		value = dateHelper.convertTime("13:00", "fr");
-		System.out.println(value);
-		value = dateHelper.convertTime("13:00", "en");
-		System.out.println(value);
-	}
+            String AM_PM = calendar.get(Calendar.AM_PM) == 0 ? "AM" : "PM";
+            // workournd from 00:00pm to 12:00pm
+            if (hour == 0 && AM_PM.equalsIgnoreCase("PM")) {
+                hour = 12;
+            }
+            result = hour + ":" + minute + " " + AM_PM;
+        } else {
+            result = time;
+        }
+        return result;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static void main(String args[]) {
+        DateHelper dateHelper = new DateHelper();
+        String value = dateHelper.convertTime("12:00", "en");
+        System.out.println(value);
+        value = dateHelper.convertTime("13:00", "fr");
+        System.out.println(value);
+        value = dateHelper.convertTime("13:00", "en");
+        System.out.println(value);
+    }
 }
