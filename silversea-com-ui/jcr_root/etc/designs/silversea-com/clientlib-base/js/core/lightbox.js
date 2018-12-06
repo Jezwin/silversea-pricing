0a2aa25ff050e19b806eda8d92f2c309faff03a5
Fabio Filippi <fabiof@silversea.com>
2018-12-06 15:36:30 +0100

    Added anchor to venentian socieity

--- silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/core/lightbox.js
+++ silversea-com-ui/jcr_root/etc/designs/silversea-com/clientlib-base/js/core/lightbox.js
@@ -4,7 +4,7 @@ $(document).on('shown.bs.modal', function (e) {
     var $modalContent = $(this).find(".modal-content");
     var $lightboxClose = $(this).find(".lightbox-close");
 
-    if ($modalBody.hasClass("cruise-2018-offers")) {
+    if ($modalBody.hasClass("cruise-2018-offers") || $modalBody.find(".cruise-2018").length > 0) {
         setTopLightboxModal(0);
         $(".cruise-2018-modal-exclusive-offer-title .open, .cruise-2018-modal-exclusive-offer-title .times, .cruise-2018-offers .title").each(function () {
             $(this).html($(this).html().toLowerCase());
