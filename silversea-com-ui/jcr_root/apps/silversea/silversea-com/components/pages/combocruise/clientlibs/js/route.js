382cf3040b49536081143fe190432525c8702c6c
Fabio Filippi <fabiof@silversea.com>
2019-01-11 11:49:18 +0100

    Fixed link of routes

--- silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/pages/combocruise/clientlibs/js/route.js
+++ silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/pages/combocruise/clientlibs/js/route.js
@@ -2,7 +2,7 @@ $(function () {
 
     var activeRoute = (function ($route) {
         $(".route-content.route-active").removeClass("route-active");
-        $route.parent().addClass("route-active");
+        $route.find('.route-content').addClass("route-active");
     });
 
     var addSpinnerOnItineraryDetail = (function () {
@@ -16,12 +16,9 @@ $(function () {
     });
 
     var onClickSlideArrowMobile = function () {
-        $(".slick-current.slick-active .change-route-onclick").click();
+        $(".change-route-onclick").click();
     };//onClickSlideArrowMobile
 
-    /*
-    * Make POST ajax call to RouteUse and get the cruise model and itineray
-     */
     var onClickChangeRoute = (function (e) {
         e && e.preventDefault();
         e && e.stopPropagation();
@@ -40,11 +37,11 @@ $(function () {
 
 
     var iAmInComboCruisePage = $("body").hasClass("combocruise"),
-        iHaveRoutesDiv = $(".route-content .change-route-onclick").length > 0,
+        iHaveRoutesDiv = $(".change-route-onclick").length > 0,
         isMobile = $(window).width() < 768;
 
     if (iAmInComboCruisePage && iHaveRoutesDiv) {
-        $(".route-content .change-route-onclick").on("click", onClickChangeRoute);
+        $(".change-route-onclick").on("click", onClickChangeRoute);
         if (isMobile) {
             $(".route-slider").on("click",".slick-arrow", onClickSlideArrowMobile);
         }
