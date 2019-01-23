19db27ed4594d6acda3e714b47ad7898f4f24861
Giuseppe Scaramuzzino <giuseppes@silversea.com>
2019-01-23 10:00:38 +0100

    add tablet style

--- silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/editorial/splitteddynamicteaser/clientlibs/files/splitteddynamicteaser.js
+++ silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/editorial/splitteddynamicteaser/clientlibs/files/splitteddynamicteaser.js
@@ -1,5 +1,35 @@
+
 $(function () {
     "use strict";
-    if (true) {
+    var isSplittedDynamicTeaseronTablet = $(".splitteddynamicteaser").length > 0 && isTablet;
+
+    if (isSplittedDynamicTeaseronTablet) {
+        //$(".splitteddynamicteaser").each(addEventClickBothSide);
     }
+
+    function addEventClickBothSide() {
+        var $splitteddynamicteaser = $(this);
+        $splitteddynamicteaser.find(".splitteddynamicteaser-left").on("click", addEventClickLeftSide);
+        $splitteddynamicteaser.find(".splitteddynamicteaser-left").addClass("splitteddynamicteaser-disable-click");
+        $splitteddynamicteaser.find(".splitteddynamicteaser-right").on("click", addEventClickRightSide);
+        $splitteddynamicteaser.find(".splitteddynamicteaser-right").addClass("splitteddynamicteaser-disable-click");
+    };//addEventClickBothSide
+
+    function addEventClickLeftSide() {
+        var $splitteddynamicteaserLeft = $(this),
+            $splitteddynamicteaserFullImage =  $splitteddynamicteaserLeft.parent();
+        $splitteddynamicteaserLeft.removeClass("splitteddynamicteaser-disable-click");
+        $splitteddynamicteaserFullImage.find(".splitteddynamicteaser-right").addClass("splitteddynamicteaser-disable-click");
+    };//addEventClickLeftSide
+
+    function addEventClickRightSide() {
+        var $splitteddynamicteaserRight = $(this),
+            $splitteddynamicteaserFullImage =  $splitteddynamicteaserRight.parent();
+        $splitteddynamicteaserRight.removeClass("splitteddynamicteaser-disable-click");
+        $splitteddynamicteaserFullImage.find(".splitteddynamicteaser-left").addClass("splitteddynamicteaser-disable-click");
+    };//addEventClickRightSide
+
+    function isTablet() {
+        return $(window).width() >= 768 && $(window).width() < 992;
+    };//isTablet
 });
