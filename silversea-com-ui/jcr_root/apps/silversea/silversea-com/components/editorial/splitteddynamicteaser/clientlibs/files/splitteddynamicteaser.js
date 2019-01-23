cf5d665f7cb1fc411804b35b517a8df472f0ac6d
Giuseppe Scaramuzzino <giuseppes@silversea.com>
2019-01-23 11:56:56 +0100

    fix ipad

--- silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/editorial/splitteddynamicteaser/clientlibs/files/splitteddynamicteaser.js
+++ silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/editorial/splitteddynamicteaser/clientlibs/files/splitteddynamicteaser.js
@@ -1,35 +1,63 @@
-
 $(function () {
     "use strict";
-    var isSplittedDynamicTeaseronTablet = $(".splitteddynamicteaser").length > 0 && isTablet;
+    var isSplittedDynamicTeaseronTablet = $(".splitteddynamicteaser").length > 0 && $(window).width() >= 768 && $(window).width() < 992;
 
     if (isSplittedDynamicTeaseronTablet) {
-        //$(".splitteddynamicteaser").each(addEventClickBothSide);
+        console.log("r");
+        $(".splitteddynamicteaser").each(addEventClickBothSide);
     }
 
     function addEventClickBothSide() {
         var $splitteddynamicteaser = $(this);
-        $splitteddynamicteaser.find(".splitteddynamicteaser-left").on("click", addEventClickLeftSide);
-        $splitteddynamicteaser.find(".splitteddynamicteaser-left").addClass("splitteddynamicteaser-disable-click");
-        $splitteddynamicteaser.find(".splitteddynamicteaser-right").on("click", addEventClickRightSide);
-        $splitteddynamicteaser.find(".splitteddynamicteaser-right").addClass("splitteddynamicteaser-disable-click");
+        //left side - remove href and create data-href to disable desktop behaviour on tablet
+        var $leftSide = $splitteddynamicteaser.find(".splitteddynamicteaser-left"),
+            attrHrefLeft = $leftSide.attr("href");
+        $leftSide.attr("data-href", attrHrefLeft);
+        $leftSide.removeAttr("href");
+        $leftSide.on("click", addEventClickLeftSide);
+
+        //right side - remove href and create data-href to disable desktop behaviour on tablet
+        var $rightSide = $splitteddynamicteaser.find(".splitteddynamicteaser-right"),
+            attrHrefRight = $rightSide.attr("href");
+        $rightSide.attr("data-href", attrHrefRight);
+        $rightSide.removeAttr("href");
+        $rightSide.on("click", addEventClickRightSide);
     };//addEventClickBothSide
 
-    function addEventClickLeftSide() {
-        var $splitteddynamicteaserLeft = $(this),
-            $splitteddynamicteaserFullImage =  $splitteddynamicteaserLeft.parent();
-        $splitteddynamicteaserLeft.removeClass("splitteddynamicteaser-disable-click");
-        $splitteddynamicteaserFullImage.find(".splitteddynamicteaser-right").addClass("splitteddynamicteaser-disable-click");
+    function addEventClickLeftSide(e) {
+        var $leftSide = $(this),
+            $fullImage = $leftSide.parent(),
+            $rightSide = $fullImage.find(".splitteddynamicteaser-right"),
+            attrHrefLeft = $leftSide.attr("data-href");
+        var isSecondClick = $leftSide.attr("href") != null;
+        if (!isSecondClick) {
+            e.preventDefault();
+            e.stopPropagation();
+            $leftSide.attr("href", attrHrefLeft);
+            //workaround for real ipad
+            $leftSide.find(".splitteddynamicteaser-text-top, .splitteddynamicteaser-text-bottom").addClass("splitteddynamicteaser-color-white");
+            $rightSide.removeAttr("href");
+            $rightSide.find(".splitteddynamicteaser-text-top, .splitteddynamicteaser-text-bottom").removeClass("splitteddynamicteaser-color-white");
+        }
     };//addEventClickLeftSide
 
-    function addEventClickRightSide() {
-        var $splitteddynamicteaserRight = $(this),
-            $splitteddynamicteaserFullImage =  $splitteddynamicteaserRight.parent();
-        $splitteddynamicteaserRight.removeClass("splitteddynamicteaser-disable-click");
-        $splitteddynamicteaserFullImage.find(".splitteddynamicteaser-left").addClass("splitteddynamicteaser-disable-click");
+    function addEventClickRightSide(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var $rightSide = $(this),
+            $fullImage = $rightSide.parent(),
+            $leftSide = $fullImage.find(".splitteddynamicteaser-left"),
+            attrHrefRight = $rightSide.attr("data-href");
+        var isSecondClick = $rightSide.attr("href") != null;
+        if (!isSecondClick) {
+            e.preventDefault();
+            e.stopPropagation();
+            $rightSide.attr("href", attrHrefRight);
+            //workaround for real ipad
+            $rightSide.find(".splitteddynamicteaser-text-top, .splitteddynamicteaser-text-bottom").addClass("splitteddynamicteaser-color-white");
+            $leftSide.removeAttr("href");
+            $leftSide.find(".splitteddynamicteaser-text-top, .splitteddynamicteaser-text-bottom").removeClass("splitteddynamicteaser-color-white");
+        }
     };//addEventClickRightSide
 
-    function isTablet() {
-        return $(window).width() >= 768 && $(window).width() < 992;
-    };//isTablet
 });
