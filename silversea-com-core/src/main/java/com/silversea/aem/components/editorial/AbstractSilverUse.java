fc0b0c3c9bf7b3d2817bd0fa3907dfe65660890b
Fabio Filippi <fabiof@silversea.com>
2019-01-28 11:12:27 +0100

    Fixed use

--- silversea-com-core/src/main/java/com/silversea/aem/components/editorial/AbstractSilverUse.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/editorial/AbstractSilverUse.java
@@ -1,14 +1,14 @@
 package com.silversea.aem.components.editorial;
 
-import com.adobe.cq.commerce.common.ValueMapDecorator;
 import com.adobe.cq.sightly.WCMUsePojo;
 import com.google.common.base.Strings;
 import com.google.common.collect.Sets;
-import com.silversea.aem.models.CustomCss;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ValueMap;
 
-import java.util.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -18,7 +18,6 @@ import static java.util.stream.StreamSupport.stream;
 
 public abstract class AbstractSilverUse extends WCMUsePojo {
 
-    protected static Set<String> IGNORED_KEYS = Sets.newHashSet("jcr", "sling", "cq", "sscUUID", "cssDesktop", "cssTablet", "cssMobile");
     public static final String TABLET = "Tablet";
     public static final String MOBILE = "Mobile";
     public static final String DESKTOP = "Desktop";
@@ -49,85 +48,6 @@ public abstract class AbstractSilverUse extends WCMUsePojo {
 
     }
 
-    protected ValueMap getComponentPropsValueMap() {
-        return new ValueMapDecorator(getComponentProps());
-    }
-
-    protected Map<String, Object> getComponentProps() {
-        Map<String, Object> componentProp = new HashMap<>();
-        getProperties().forEach((key, value) -> {
-            if (!IGNORED_KEYS.contains(key)) {
-                componentProp.put(key, value);
-            }
-        });
-        return componentProp;
-    }
-
-    /*
-      Set tablet values from desktop if value is not present
-    */
-    protected <T> Map setComponentPropsTabletValuesFromDesktop(Map properties) {
-        return setComponentPropsValuesDevices(properties, "Tablet", "Devices");
-    }
-
-    /*
-     Set mobile values from desktop if value is not present
-    */
-    protected <T> Map setComponentPropsMobileValuesFromDesktop(Map properties) {
-        return setComponentPropsValuesDevices(properties, "Mobile", "Devices");
-    }
-
-    private <T> Map setComponentPropsValuesDevices(Map<String, Object> properties, String device, String defaultDevice) {
-        Map componentProp = new HashMap<>();
-        properties.forEach((key, value) -> {
-            if (key.contains(defaultDevice)) {
-                String keyOfDevice = key.replace(defaultDevice, device);
-                if (!properties.containsKey(keyOfDevice)) {
-                    properties.put(keyOfDevice, value);
-                }
-            }
-        });
-        return componentProp;
-    }
-
-    /*
-     Retrieve all css custom styles for desktop inside the multifield putting the prefixID
-     * */
-    protected <T> Map getCustomCssValuesDesktop(String prefixID) {
-        return getCustomCssValueDevice(prefixID, "cssDesktop");
-    }
-
-    /*
-     Retrieve all css custom styles for tablet inside the multifield putting the prefixID
-     * */
-    protected <T> Map getCustomCssValuesTablet(String prefixID) {
-        return getCustomCssValueDevice(prefixID, "cssTablet");
-    }
-
-    /*
-     Retrieve all css custom styles for mobile inside the multifield putting the prefixID
-     * */
-    protected <T> Map getCustomCssValuesMobile(String prefixID) {
-        return getCustomCssValueDevice(prefixID, "cssMobile");
-    }
-
-    /*
-     Retrieve all css custom style by device inside the multifield
-     Example:#splitteddynamicteaser--892277276 .title { font-size: 20px; }  #splitteddynamicteaser--892277276 .description{ font-size: 20px; }
-     * */
-    private <T> Map getCustomCssValueDevice(String prefixID, String device) {
-        Map componentProp = new HashMap<>();
-        getProp("sscUUID").ifPresent(id -> {
-            String cssDevice = "";
-            List<CustomCss> cssList = retrieveMultiField(device, CustomCss.class);
-            for (CustomCss customCss : cssList) {
-                cssDevice += " " + prefixID + id + " " + customCss.getCss() + " ";
-            }
-            componentProp.put(device, cssDevice);
-        });
-        return componentProp;
-    }
-
 
     private <T> T fromVarArgs(T[] args, int index) {
         if (args == null || args.length == 0) {
@@ -142,27 +62,22 @@ public abstract class AbstractSilverUse extends WCMUsePojo {
 
 
     protected DeviceProperty<String> getDeviceProp(String key, String... defaultValue) {
-        return new DeviceProperty<>(
-                getProp(key + DESKTOP).filter(Strings::isNullOrEmpty).orElse(fromVarArgs(defaultValue, 0)),
-                getProp(key + TABLET).filter(Strings::isNullOrEmpty).orElse(fromVarArgs(defaultValue, 0)),
-                getProp(key + MOBILE).filter(Strings::isNullOrEmpty).orElse(fromVarArgs(defaultValue, 0))
-        );
+        return getDeviceProp(key, String.class, defaultValue);
     }
 
     protected DeviceProperty<Integer> getDeviceProp(String key, Integer... defaultValue) {
-        return new DeviceProperty<>(
-                getInt(key + DESKTOP, fromVarArgs(defaultValue, 0)),
-                getInt(key + TABLET, fromVarArgs(defaultValue, 1)),
-                getInt(key + MOBILE, fromVarArgs(defaultValue, 2))
-        );
+        return getDeviceProp(key, Integer.class, defaultValue);
     }
 
     protected DeviceProperty<Boolean> getDeviceProp(String key, Boolean... defaultValue) {
+        return getDeviceProp(key, Boolean.class, defaultValue);
+    }
+
+    protected <T> DeviceProperty<T> getDeviceProp(String key, Class<T> typeClass, T[] defaults) {
         return new DeviceProperty<>(
-                getBoolean(key + DESKTOP, fromVarArgs(defaultValue, 0)),
-                getBoolean(key + TABLET, fromVarArgs(defaultValue, 1)),
-                getBoolean(key + MOBILE, fromVarArgs(defaultValue, 2))
-        );
+                getProp(key + DESKTOP, typeClass, fromVarArgs(defaults, 0)),
+                getProp(key + TABLET, typeClass, fromVarArgs(defaults, 1)),
+                getProp(key + MOBILE, typeClass, fromVarArgs(defaults, 2)));
     }
 
     protected <T> List<T> retrieveMultiField(String child, Class<T> adaptable) {
--- silversea-com-core/src/main/java/com/silversea/aem/components/editorial/SplittedDynamicTeaserUse.java
+++ silversea-com-core/src/main/java/com/silversea/aem/components/editorial/SplittedDynamicTeaserUse.java
@@ -1,13 +1,21 @@
 package com.silversea.aem.components.editorial;
 
+import com.google.common.collect.Sets;
+import com.silversea.aem.models.CustomCss;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.api.wrappers.ValueMapDecorator;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 
 public class SplittedDynamicTeaserUse extends AbstractSilverUse {
 
     private ValueMap componentProp;
     private static final String PREFIX_ID_COMPONENT = "#splitteddynamicteaser-";
+    protected static Set<String> IGNORED_KEYS = Sets.newHashSet("jcr", "sling", "cq", "sscUUID", "cssDesktop", "cssTablet", "cssMobile");
 
     @Override
     public void activate() throws Exception {
@@ -19,6 +27,79 @@ public class SplittedDynamicTeaserUse extends AbstractSilverUse {
 
 
     public ValueMap getComponentProp() {
+        return componentProp;
+    }
+
+    protected ValueMap getComponentPropsValueMap() {
+        return new ValueMapDecorator(getComponentProps());
+    }
+
+    protected Map<String, Object> getComponentProps() {
+        Map<String, Object> componentProp = new HashMap<>();
+        getProperties().forEach((key, value) -> {
+            if (!IGNORED_KEYS.contains(key)) {
+                componentProp.put(key, value);
+            }
+        });
+        return componentProp;
+    }
+
+    /*
+      Set tablet values from desktop if value is not present
+    */
+    protected Map<String, String> setComponentPropsTabletValuesFromDesktop(Map<String, Object> properties) {
+        return setComponentPropsValuesDevices(properties, "Tablet", "Devices");
+    }
+
+    /*
+     Set mobile values from desktop if value is not present
+    */
+    protected Map<String, String> setComponentPropsMobileValuesFromDesktop(Map<String, Object> properties) {
+        return setComponentPropsValuesDevices(properties, "Mobile", "Devices");
+    }
+
+    private Map<String, String> setComponentPropsValuesDevices(Map<String, Object> properties, String device, String defaultDevice) {
+        Map<String, String> componentProp = new HashMap<>();
+        properties.forEach((key, value) -> {
+            if (key.contains(defaultDevice)) {
+                String keyOfDevice = key.replace(defaultDevice, device);
+                if (!properties.containsKey(keyOfDevice)) {
+                    properties.put(keyOfDevice, value);
+                }
+            }
+        });
+        return componentProp;
+    }
+
+    /*
+     Retrieve all css custom styles for desktop inside the multifield putting the prefixID
+     * */
+    protected Map<String, String> getCustomCssValuesDesktop(String prefixID) {
+        return getCustomCssValueDevice(prefixID, "cssDesktop");
+    }
+
+    protected Map<String, String> getCustomCssValuesTablet(String prefixID) {
+        return getCustomCssValueDevice(prefixID, "cssTablet");
+    }
+
+    protected Map<String, String> getCustomCssValuesMobile(String prefixID) {
+        return getCustomCssValueDevice(prefixID, "cssMobile");
+    }
+
+    /*
+     Retrieve all css custom style by device inside the multifield
+     Example:#splitteddynamicteaser--892277276 .title { font-size: 20px; }  #splitteddynamicteaser--892277276 .description{ font-size: 20px; }
+     */
+    private Map<String, String> getCustomCssValueDevice(String prefixID, String device) {
+        Map<String, String> componentProp = new HashMap<>();
+        getProp("sscUUID").ifPresent(id -> {
+            StringBuilder cssDevice = new StringBuilder();
+            List<CustomCss> cssList = retrieveMultiField(device, CustomCss.class);
+            for (CustomCss customCss : cssList) {
+                cssDevice.append(" ").append(prefixID).append(id).append(" ").append(customCss.getCss()).append(" ");
+            }
+            componentProp.put(device, cssDevice.toString());
+        });
         return componentProp;
     }
 
