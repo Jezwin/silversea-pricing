cf6ec253a3f6a8babb87ab258197f16f4e5b409f
Giuseppe Scaramuzzino <giuseppes@silversea.com>
2019-02-21 12:50:58 +0100

    add sec check lightbox map SIL-2073

--- silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/included/lightboxes/lightbox-map/clientlibs/files/lightbox-map.js
+++ silversea-com-ui/jcr_root/apps/silversea/silversea-com/components/included/lightboxes/lightbox-map/clientlibs/files/lightbox-map.js
@@ -12,38 +12,44 @@ $(function () {
             }
             setTimeout(function (args) {
                 changeRotationIcon(++numIteration);
-            },800);
+            }, 800);
         }
     });//changeRotationIcon
 
     /***************************************************************************
      * Lightbox Map for "cruise page" 2018"
      **************************************************************************/
-    $(".cruise-2018-itineraries-container,#cruise2018overview").on("click", ".open-lightbox-map", function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        var $link = $(this),
-            ajaxContentPath = $link.closest('[data-lightbox-map-path]').data('lightbox-map-path'),
-            modalTarget = $link.data('target'),
-            $modalContent = $(modalTarget);
-        $modalContent.find(".modal-content").addClass("modal-content--transparent");
-        $modalContent.find(".modal-dialog").addClass("modal-larger");
-        $modalContent.find(".modal-content").addClass("lightbox-gallery-assets-content");
-        $modalContent.addClass("lightbox-no-scroll");
-        $modalContent.modal('show');
 
-        // Wait for modal opening
-        $modalContent.on('shown.bs.modal', function (e) {
+    var isContainerVisible = $(".cruise-2018-itineraries-container,#cruise2018overview").length > 0;
+
+    if (isContainerVisible) {
+        $(".cruise-2018-itineraries-container,#cruise2018overview").on("click", ".open-lightbox-map", function (e) {
             e.preventDefault();
             e.stopPropagation();
-            var $modal = $(this);
-            $modal.off('shown.bs.modal');
-            // Append html response inside modal
-            $modal.find('.modal-content').load(ajaxContentPath, function (e) {
-                $(".lightbox-close").addClass("close-full-width");
-                setTopLightboxModal(0);
-                changeRotationIcon(0);
+            var $link = $(this),
+                ajaxContentPath = $link.closest('[data-lightbox-map-path]').data('lightbox-map-path'),
+                modalTarget = $link.data('target'),
+                $modalContent = $(modalTarget);
+            $modalContent.find(".modal-content").addClass("modal-content--transparent");
+            $modalContent.find(".modal-dialog").addClass("modal-larger");
+            $modalContent.find(".modal-content").addClass("lightbox-gallery-assets-content");
+            $modalContent.addClass("lightbox-no-scroll");
+            $modalContent.modal('show');
+
+            // Wait for modal opening
+            $modalContent.on('shown.bs.modal', function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                var $modal = $(this);
+                $modal.off('shown.bs.modal');
+                // Append html response inside modal
+                $modal.find('.modal-content').load(ajaxContentPath, function (e) {
+                    $(".lightbox-close").addClass("close-full-width");
+                    setTopLightboxModal(0);
+                    changeRotationIcon(0);
+                });
             });
         });
-    });
+    }
+
 });
