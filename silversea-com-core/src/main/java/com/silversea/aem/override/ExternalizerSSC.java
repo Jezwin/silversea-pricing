aeecb8733b6202e8ebc9eda3501e1c3495e71e16
Sylvain Mura <sylvainm@silversea.com>
2019-02-27 17:27:27 +0100

    Revert temp the externalizer - and manage the redirection via config only !

--- silversea-com-core/src/main/java/com/silversea/aem/override/ExternalizerSSC.java
+++ silversea-com-core/src/main/java/com/silversea/aem/override/ExternalizerSSC.java
@@ -10,8 +10,8 @@ public class ExternalizerSSC  {
 
     public static String publishLink(ResourceResolver resourceResolver, String s) {
 
-        String portNameRequested = getPortNameRequested(s);
-        if (portNameRequested != null) return portNameRequested;
+      /*  String portNameRequested = getPortNameRequested(s);
+        if (portNameRequested != null) return portNameRequested;*/
 
         Externalizer externalizer = resourceResolver.adaptTo(Externalizer.class);
 
@@ -20,46 +20,46 @@ public class ExternalizerSSC  {
 
     public static String externalLink(ResourceResolver resourceResolver, String s, String s1){
 
-        String portNameRequested = getPortNameRequested(s1);
-        if (portNameRequested != null) return portNameRequested;
+       /* String portNameRequested = getPortNameRequested(s1);
+        if (portNameRequested != null) return portNameRequested;*/
 
         Externalizer externalizer = resourceResolver.adaptTo(Externalizer.class);
 
         return externalizer.externalLink(resourceResolver,s,s1);
     }
 
-    private static String getPortNameRequested(String s) {
+  /*  private static String getPortNameRequested(String s) {
         try {
             if (s.contains("/en/other-resources/find-a-port")) {
                 String portNameRequested = s.substring(s.lastIndexOf('/') + 1);
-                if(portNameRequested.length() > 2)
+                if(portNameRequested.length() > 2 && !portNameRequested.equals("find-a-port"))
                     return "https://www.silversea.com" + "/cruise-to/" + portNameRequested;
             } else if (s.contains("/fr/other-resources/find-a-port")) {
                 String portNameRequested = s.substring(s.lastIndexOf('/') + 1);
-                if(portNameRequested.length() > 2)
+                if(portNameRequested.length() > 2 && !portNameRequested.equals("find-a-port"))
                     return "https://www.silversea.com" + "/fr/croisieres/" + portNameRequested;
             } else if (s.contains("/es/other-resources/find-a-port")) {
                 String portNameRequested = s.substring(s.lastIndexOf('/') + 1);
-                if(portNameRequested.length() > 2)
+                if(portNameRequested.length() > 2 && !portNameRequested.equals("find-a-port"))
                     return "https://www.silversea.com" + "/es/cruceros/" + portNameRequested;
             } else if (s.contains("/pt-br/other-resources/find-a-port")) {
                 String portNameRequested = s.substring(s.lastIndexOf('/') + 1);
-                if(portNameRequested.length() > 2)
+                if(portNameRequested.length() > 2 && !portNameRequested.equals("find-a-port"))
                     return "https://www.silversea.com" + "/pt-br/cruzeiros/" + portNameRequested;
             } else if (s.contains("/de/other-resources/find-a-port")) {
                 String portNameRequested = s.substring(s.lastIndexOf('/') + 1);
-                if(portNameRequested.length() > 2)
+                if(portNameRequested.length() > 2 && !portNameRequested.equals("find-a-port"))
                     return "https://www.silversea.com" + "/de/kreuzfahrten/" + portNameRequested;
             }
         }catch (Exception e){
 
         }
         return null;
-    }
+    }*/
 
     public static String relativeLink(SlingHttpServletRequest request, String path, ResourceResolver resourceResolver) {
-        String portNameRequested = getPortNameRequested(path);
-        if (portNameRequested != null) return portNameRequested;
+       /* String portNameRequested = getPortNameRequested(path);
+        if (portNameRequested != null) return portNameRequested;*/
 
         Externalizer externalizer = resourceResolver.adaptTo(Externalizer.class);
 
