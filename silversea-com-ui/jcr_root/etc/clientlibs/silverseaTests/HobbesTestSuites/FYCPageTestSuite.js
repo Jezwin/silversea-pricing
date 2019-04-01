9c36c322a07c01878dcbc64188681053d3c24c24
sachin.n <sachin.n@27technologies.com>
2019-04-01 13:39:46 +0530

    Update for cruise2018 results

--- silversea-com-ui/jcr_root/etc/clientlibs/silverseaTests/HobbesTestSuites/FYCPageTestSuite.js
+++ silversea-com-ui/jcr_root/etc/clientlibs/silverseaTests/HobbesTestSuites/FYCPageTestSuite.js
@@ -7,7 +7,7 @@
         .addTestCase(new hobs.TestCase("Navigate to FYC Result Page")
           .navigateTo("%fycPageUrl%")
     	  .click(".c-search-hero__form button.btn",{expectNav: true})        
-		  .asserts.exists(".findyourcruise .c-fyc-v2__result__link", true, {timeout: 5000})
+		  .asserts.exists(".findyourcruise2018 .c-fyc-v2__result__link", true, {timeout: 5000})
         )
 
     	.addTestCase(new hobs.TestCase("Navigate to Cruise Detail Page")
@@ -15,15 +15,34 @@
 		  .asserts.exists(".cruise-2018-overview-description", true, {timeout: 5000})
         )
 
-    	.addTestCase(new hobs.TestCase("Set Filter")
-            .navigateTo("%fycPageUrl%")         
-            .click(".c-fyc-light__form .ships .chosen-container a:first")	
-            .click(".c-search-hero__form button.btn",{expectNav: true})
-            .wait(5000) 
-			.click("#v2-filter-collapse .destination-tracking b")
-            .execFct(function() {
-                (hobs.context().document.getElementsByTagName('select')[0]).selectedIndex = 1; 
-    		})   
+		// Cruise 2018 style
+   		.addTestCase(new hobs.TestCase("Navigate to filter page")
+    	  .navigateTo("%fycPageUrl%")         
+          .click(".c-fyc-light__form .ships .chosen-container a:first")	
+          .click(".c-search-hero__form button.btn",{expectNav: true})
+          .wait(3000)  
+        )
+
+    	.addTestCase(new hobs.TestCase("Set Filter", { params: { initialResultValue:  function(){hobs.find(".findyourcruise2018.v2 .fyc2018-header-container div:nth-child(2)").text()} } })
+            .click(".filter-destination .fyc2018-filter-value-container div.filter-no-selected:first")
+            .click(".filter-destination .fyc2018-filter-clear")
+            .click(".filter-departure .fyc2018-filter-value-container div.filter-no-selected:first")
+            .click(".filter-departure .fyc2018-filter-clear") 
+            .click(".filter-duration .fyc2018-filter-value-container div.filter-no-selected:first")
+            .click(".filter-duration .fyc2018-filter-clear") 
+            .click(".filter-ship .fyc2018-filter-value-container div.filter-no-selected:first")
+            .click(".filter-ship .fyc2018-filter-clear") 
+            .click(".filter-cruise-type .fyc2018-filter-value-container div.filter-no-selected:first")
+            .click(".filter-cruise-type .fyc2018-filter-clear") 
+            .click(".filter-port .fyc2018-filter-value-container div.filter-no-selected:first")
+            .click(".filter-port .fyc2018-filter-clear")  
+            .wait(3000)         
+            .asserts.isTrue(function(){ return hobs.find(".findyourcruise2018.v2 .fyc2018-header-container div:nth-child(2)").text() != "%initialResultValue%";})      
+
+			//Clear all filters
+			.click(".findyourcruise2018 div.fyc2018-header-reset-all:first")   
+            .wait(3000)         
+            .asserts.isTrue(function(){ return hobs.find(".findyourcruise2018.v2 .fyc2018-header-container div:nth-child(2)").text() != "%initialResultValue%";})            
         );
 }(window, hobs));
 
