16ec30a1a1077883efe999875d64f769f31345c9
Nick Tune <nick.tune@intersection.com>
2019-08-08 23:46:03 +0200

    bring back settings - adobe profile needed

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -15,7 +15,7 @@ echo "Building ssc-api-client"
 cd ../ssc-api-client && mvn clean install
 
 echo "Building and deploying silversea-com"
-cd ../silversea-com && mvn clean install -Pauto-install-package -Dcq.password="${PASSWORD}" -Dcq.user="${USER}" -Dcq.url="${AEM_URL}" 
+cd ../silversea-com && mvn --settings ../maven-settings-ci.xml clean install -Pauto-install-package -Dcq.password="${PASSWORD}" -Dcq.user="${USER}" -Dcq.url="${AEM_URL}" 
 
 echo "Building and deploying silversea-ssc"
-cd ../silversea-ssc && mvn clean install -Pauto-install-package -Dcq.password="${PASSWORD}" -Dcq.user="${USER}" -Dcq.url="${AEM_URL}"
+cd ../silversea-ssc && mvn --settings ../maven-settings-ci.xml clean install -Pauto-install-package -Dcq.password="${PASSWORD}" -Dcq.user="${USER}" -Dcq.url="${AEM_URL}"
