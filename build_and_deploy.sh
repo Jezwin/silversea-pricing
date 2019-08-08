605349959754b610163131b4c5cb0ed2be265d4c
Nick Tune <nick.tune@intersection.com>
2019-08-08 22:38:32 +0200

    remove  maven settings

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -11,7 +11,7 @@ echo "Building silversea aem parent...."
 cd silversea-aem-parent && mvn clean install
 
 echo "Building and deploying silversea-com"
-cd ../silversea-com && mvn --settings ../maven-settings-ci.xml clean install -Pauto-install-package -Dcq.password="${PASSWORD}" -Dcq.user=ci-user -Dcq.url="${AEM_URL}" -DapiClientVersion="[3.0, 4.0]"
+cd ../silversea-com && mvn clean install -Pauto-install-package -Dcq.password="${PASSWORD}" -Dcq.user=ci-user -Dcq.url="${AEM_URL}" -DapiClientVersion="[3.0, 4.0]"
 
 echo "Building and deploying silversea-ssc"
-cd ../silversea-ssc && mvn --settings ../maven-settings-ci.xml clean install -Pauto-install-package -Dcq.password="${PASSWORD}" -Dcq.user=ci-user -Dcq.url="${AEM_URL}" -DapiClientVersion="[3.0, 4.0]"
+cd ../silversea-ssc && mvn clean install -Pauto-install-package -Dcq.password="${PASSWORD}" -Dcq.user=ci-user -Dcq.url="${AEM_URL}" -DapiClientVersion="[3.0, 4.0]"
