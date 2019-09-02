18db54e77da5b324ada043210372941babc67dd8
Nick Tune <nicholast@silversea.com>
2019-09-02 10:30:11 +0000

    Merged in DDD-160-detect-errors-when-deploying (pull request #24)
    DDD-160 detect errors when deploying
    
    Approved-by: Orlando Perri

--- build_and_test_local.sh
+++ build_and_test_local.sh
@@ -5,21 +5,9 @@ set -e
 RELEASE="${1:-2.1.S13-SNAPSHOT}"
 
 echo "Building and testing silversea.com mono-repo version: ${RELEASE}"
-
-echo "Building silversea aem parent...."
-cd silversea-aem-parent && mvn clean install
-
-echo "Building ssc-api-client"
-cd ../ssc-api-client && mvn clean install
-
-echo "Building and testing silversea-com"
-cd ../silversea-com && mvn --settings ../maven-settings-ci.xml clean install -Drevision="${RELEASE}"
-
-echo "Building and testing silversea-ssc"
-cd ../silversea-ssc && mvn --settings ../maven-settings-ci.xml clean install -Drevision="${RELEASE}"
+mvn install --settings maven-settings-ci.xml -Drevision="${RELEASE}"
 
 echo "Copying packages to packages directory"
-cd ../
 rm -rf crx-packages
 mkdir crx-packages
 cp "silversea-com/silversea-com-configs/target/silversea-com-configs-${RELEASE}.zip" crx-packages/
--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,25 @@ if [ ! -d "$PKGS_DIR" ]; then
     exit 1;
 fi
 
-echo "Uploading all zip packages in ${PKGS_DIR} to ${CRX_URL}"
+echo "Uploading all packages in ${PKGS_DIR} to ${CRX_URL}"
 
 for filename in "$PKGS_DIR"/*; do
     echo "Uploading ${filename}"
-    curl -v --fail -u "${USER}:${PASSWORD}" -F file=@"${filename}" -F name="${filename}" -F force=true -F install=true "http://${CRX_HOST_PORT}/crx/packmgr/service.jsp"
-    # This does not work. I'm leaving it here to show I tried to use the plugin.
-    #mvn content-package:install -Dvault.targetURL="http://${CRX_HOST_PORT}" -Dvault.file="${filename}" -Dvault.userId="${USER}" -Dvault.password="${PASSWORD}" -Dvault.verbose="true" -Dvault.failOnError="true" -Dvault.useProxy="false" -Dvault.install="false" --settings ../maven-settings-ci.xml
+    curl --fail -u "${USER}:${PASSWORD}" -F file=@"${filename}" -F name="${filename}" -F force=true -F install=true "http://${CRX_HOST_PORT}/crx/packmgr/service.jsp"
+done
+
+echo "Checking packages installed correctly..."
+
+curl --fail -o installed-packages.txt -u "${USER}:${PASSWORD}" "http://${CRX_HOST_PORT}/crx/packmgr/service.jsp?cmd=ls"
+
+upload_failed() {
+    echo "Upload failed."
+    exit 1;
+}
+
+for filename in "$PKGS_DIR"/*; do
+    echo "Checking ${filename} was correctly installed..."
+    pkg_file="$(basename "$filename")"
+    grep -A2 -B2 "<downloadName>${pkg_file}</downloadName>" installed-packages.txt || upload_failed
+    echo "${filename} was correctly installed..."
 done
