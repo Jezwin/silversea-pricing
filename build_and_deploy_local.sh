c354406969b3ae5d76dae2bab41d0ee7827a43f5
James Morcom <morcs@morcs.com>
2019-09-03 12:34:59 +0100

    Add separate port definition for smoke test.

--- build_and_deploy_local.sh
+++ build_and_deploy_local.sh
@@ -9,5 +9,5 @@ echo "Building and deploying to ${HOST}"
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 bash ${__dir}/build_and_test_local.sh 
-bash ${__dir}/deploy.sh "localhost:4502" "crx-packages" admin admin
+bash ${__dir}/deploy.sh "localhost" "4502" "4502" "crx-packages" admin admin
 
--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-CRX_HOST_PORT=$1
-PKGS_DIR=$2
-USER=$3
-PASSWORD=$4
+CRX_HOST=$1
+CRX_PORT=$2
+CRX_TEST_PORT=$3
+PKGS_DIR=$4
+USER=$5
+PASSWORD=$6
 
 if [ ! -d "$PKGS_DIR" ]; then
     echo "Package directory: ${PKGS_DIR} does not exist. Aborting."
@@ -16,12 +18,12 @@ echo "Uploading all packages in ${PKGS_DIR} to ${CRX_URL}"
 
 for filename in "$PKGS_DIR"/*; do
     echo "Uploading ${filename}"
-    curl --fail -u "${USER}:${PASSWORD}" -F file=@"${filename}" -F name="${filename}" -F force=true -F install=true "http://${CRX_HOST_PORT}/crx/packmgr/service.jsp"
+    curl --fail -u "${USER}:${PASSWORD}" -F file=@"${filename}" -F name="${filename}" -F force=true -F install=true "http://${CRX_HOST}:${CRX_PORT}/crx/packmgr/service.jsp"
 done
 
 echo "Checking packages installed correctly..."
 
-curl --fail -o installed-packages.txt -u "${USER}:${PASSWORD}" "http://${CRX_HOST_PORT}/crx/packmgr/service.jsp?cmd=ls"
+curl --fail -o installed-packages.txt -u "${USER}:${PASSWORD}" "http://${CRX_HOST}:${CRX_TEST_PORT}/crx/packmgr/service.jsp?cmd=ls"
 
 upload_failed() {
     echo "Upload failed."
