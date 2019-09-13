e4a44ee4d330723e41b2dce9956be465c68a0a6e
James Morcom <morcs@morcs.com>
2019-09-13 12:28:45 +0100

    Remove smoke test for deploy

--- deploy.sh
+++ deploy.sh
@@ -21,42 +21,42 @@ for filename in "$PKGS_DIR"/*; do
     curl --fail -u "${USER}:${PASSWORD}" -F file=@"${filename}" -F name="${filename}" -F force=true -F install=true "http://${CRX_HOST}:${CRX_PORT}/crx/packmgr/service.jsp"
 done
 
-echo "Checking packages installed correctly..."
+# echo "Checking packages installed correctly..."
 
-attempt_counter=0
-max_attempts=5
+# attempt_counter=0
+# max_attempts=5
 
-until $(curl --fail -o installed-packages.txt -u "${USER}:${PASSWORD}" "http://${CRX_HOST}:${CRX_TEST_PORT}/crx/packmgr/service.jsp?cmd=ls"); do
-    if [ ${attempt_counter} -eq ${max_attempts} ];then
-      echo "Max attempts reached when trying to list installed packages"
-      exit 1
-    fi
+# until $(curl --fail -o installed-packages.txt -u "${USER}:${PASSWORD}" "http://${CRX_HOST}:${CRX_TEST_PORT}/crx/packmgr/service.jsp?cmd=ls"); do
+#     if [ ${attempt_counter} -eq ${max_attempts} ];then
+#       echo "Max attempts reached when trying to list installed packages"
+#       exit 1
+#     fi
 
-    printf '.'
-    attempt_counter=$(($attempt_counter+1))
-    sleep 5
-done
+#     printf '.'
+#     attempt_counter=$(($attempt_counter+1))
+#     sleep 5
+# done
 
-for filename in "$PKGS_DIR"/*; do
-    echo "Checking ${filename} was correctly installed..."
-    pkg_file="$(basename "$filename")"
-    result=$(grep -c -A2 -B2 "<downloadName>${pkg_file}</downloadName>" installed-packages.txt) || :
-    if [ "${result}" -gt 0 ] 
-    then
-        echo "OK - ${pkg_file} was correctly installed..."
-    else
-        echo "${pkg_file} not found in package list after upload"
-        pkg_file_prefix=$(sed -E 's/[0-9].+//' <<< $pkg_file) 
-        echo "Searching instead for: $pkg_file_prefix"
-        result=$(grep "<downloadName>${pkg_file_prefix}" installed-packages.txt) || :
-        if [ -z ${result[0]} ]
-        then
-            echo "No match found for ${pkg_file_prefix} either."
-            echo "Outputting first 30 lines of response:"
-            echo $(head -30 installed-packages.txt)
-        else
-            echo "Found: ${result[0]}"
-        fi
-        exit 1
-    fi
-done
+# for filename in "$PKGS_DIR"/*; do
+#     echo "Checking ${filename} was correctly installed..."
+#     pkg_file="$(basename "$filename")"
+#     result=$(grep -c -A2 -B2 "<downloadName>${pkg_file}</downloadName>" installed-packages.txt) || :
+#     if [ "${result}" -gt 0 ] 
+#     then
+#         echo "OK - ${pkg_file} was correctly installed..."
+#     else
+#         echo "${pkg_file} not found in package list after upload"
+#         pkg_file_prefix=$(sed -E 's/[0-9].+//' <<< $pkg_file) 
+#         echo "Searching instead for: $pkg_file_prefix"
+#         result=$(grep "<downloadName>${pkg_file_prefix}" installed-packages.txt) || :
+#         if [ -z ${result[0]} ]
+#         then
+#             echo "No match found for ${pkg_file_prefix} either."
+#             echo "Outputting first 30 lines of response:"
+#             echo $(head -30 installed-packages.txt)
+#         else
+#             echo "Found: ${result[0]}"
+#         fi
+#         exit 1
+#     fi
+# done
