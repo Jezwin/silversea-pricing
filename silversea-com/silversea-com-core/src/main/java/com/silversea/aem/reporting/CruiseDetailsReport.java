45baaddb41cbdf51763b6f78a23c5cd38eab0ddf
Yogendra Valani <dr.valani@gmail.com>
2019-09-25 16:32:01 +0100

    Getting all itineraries

--- silversea-com/silversea-com-core/src/main/java/com/silversea/aem/reporting/CruiseDetailsReport.java
+++ silversea-com/silversea-com-core/src/main/java/com/silversea/aem/reporting/CruiseDetailsReport.java
@@ -1,30 +1,30 @@
-package com.silversea.aem.importers.servlets.reporting;
+package com.silversea.aem.reporting;
 
 import com.day.cq.wcm.api.PageManager;
+import com.opencsv.CSVWriter;
 import com.silversea.aem.importers.ImporterException;
 import com.silversea.aem.importers.ImportersConstants;
-import com.silversea.aem.importers.utils.ImportersUtils;
-import com.silversea.aem.models.ItineraryModel;
+import com.silversea.aem.logging.LogzLogger;
+import com.silversea.aem.models.CruiseModel;
 import com.silversea.aem.services.ApiConfigurationService;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 
 import javax.jcr.Session;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-@SlingServlet(paths = "/bin/report-itinerary")
-public class ItineraryReport extends SlingSafeMethodsServlet {
+@SlingServlet(paths = "/bin/report-cruisedetails")
+public class CruiseDetailsReport extends SlingSafeMethodsServlet {
 
     @Reference
     private ResourceResolverFactory resourceResolverFactory;
@@ -35,7 +35,7 @@ public class ItineraryReport extends SlingSafeMethodsServlet {
     @Override
     public void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
         response.setContentType("text/plain");
-        response.setHeader("Content-disposition", "attachment; filename=sample.txt");
+        response.setHeader("Content-disposition", "attachment; filename=sample.csv");
 
         Map<String, Object> authenticationParams = new HashMap<>();
         authenticationParams.put(ResourceResolverFactory.SUBSERVICE, ImportersConstants.SUB_SERVICE_IMPORT_DATA);
@@ -49,22 +49,42 @@ public class ItineraryReport extends SlingSafeMethodsServlet {
                 throw new ImporterException("Cannot initialize pageManager and session");
             }
 
-            final List<ItineraryModel> itineraries = ImportersUtils.getItineraries(resourceResolver);
-            for (ItineraryModel itinerary: itineraries) {
-                List<String> output = new ArrayList<>();
-                output.add(itinerary.getCruiseId().toString());
-                output.add(itinerary.getItineraryId().toString());
-                output.add(itinerary.getNumberDays().toString());
-                out.println(String.join(",", output));
-            }
+            final List<CruiseModel> cruiseModels = getCruiseDetails(resourceResolver);
 
+            CSVWriter csvWriter = new CSVWriter(out, ',', '"', '\\', System.lineSeparator());
+
+
+            for (CruiseModel cruiseModel: cruiseModels) {
+                List<String> output = new ArrayList<>();
+                output.add(cruiseModel.getCruiseCode());
+                output.add(cruiseModel.getBigItineraryMap());
+                output.add(cruiseModel.getDescription());
+                output.add(cruiseModel.getDescription());
+                csvWriter.writeNext(output.toArray(new String[0]));
+            }
             out.close();
+
+            csvWriter.close();
+
             // Iterate over exclusive offers from API
         } catch (LoginException | ImporterException e) {
         }
+    }
+    private List<CruiseModel> getCruiseDetails(ResourceResolver resourceResolver) {
+        final String query = "/jcr:root/content/silversea-com//element(*,cq:Page)" +
+                "[jcr:content/sling:resourceType=\"silversea/silversea-com/components/pages/destination\"]";
 
+        final Iterator<Resource> resources = resourceResolver.findResources(query,"xpath");
 
-        int numBytesRead = 10;
-        out.write(numBytesRead);
+        final List<CruiseModel> cruiseModels = new ArrayList<>();
+        while (resources.hasNext()) {
+            final Resource itinerary = resources.next();
+            final CruiseModel itineraryModel = itinerary.adaptTo(CruiseModel.class);
+
+            if (itineraryModel != null) {
+                cruiseModels.add(itineraryModel);
+            }
+        }
+        return cruiseModels;
     }
 }
--- silversea-com/silversea-com-core/src/main/java/com/silversea/aem/reporting/ItineraryReport.java
+++ silversea-com/silversea-com-core/src/main/java/com/silversea/aem/reporting/ItineraryReport.java
@@ -1,16 +1,21 @@
-package com.silversea.aem.importers.servlets.reporting;
+package com.silversea.aem.reporting;
 
 import com.day.cq.wcm.api.PageManager;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import com.silversea.aem.importers.ImporterException;
 import com.silversea.aem.importers.ImportersConstants;
-import com.silversea.aem.models.CruiseModel;
+import com.silversea.aem.importers.utils.ImportersUtils;
+import com.silversea.aem.models.ItineraryModel;
+import com.silversea.aem.reporting.models.ItineraryBean;
 import com.silversea.aem.services.ApiConfigurationService;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.LoginException;
-import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
@@ -18,10 +23,12 @@ import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import javax.jcr.Session;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-@SlingServlet(paths = "/bin/report-cruisedetails")
-public class CruiseDetailsReport extends SlingSafeMethodsServlet {
+@SlingServlet(paths = "/bin/report-itinerary")
+public class ItineraryReport extends SlingSafeMethodsServlet {
 
     @Reference
     private ResourceResolverFactory resourceResolverFactory;
@@ -32,7 +39,7 @@ public class CruiseDetailsReport extends SlingSafeMethodsServlet {
     @Override
     public void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
         response.setContentType("text/plain");
-        response.setHeader("Content-disposition", "attachment; filename=sample.txt");
+        response.setHeader("Content-disposition", "attachment; filename=itinerary.csv");
 
         Map<String, Object> authenticationParams = new HashMap<>();
         authenticationParams.put(ResourceResolverFactory.SUBSERVICE, ImportersConstants.SUB_SERVICE_IMPORT_DATA);
@@ -46,40 +53,21 @@ public class CruiseDetailsReport extends SlingSafeMethodsServlet {
                 throw new ImporterException("Cannot initialize pageManager and session");
             }
 
-            final List<CruiseModel> cruiseModels = getCruiseDetails(resourceResolver);
-            for (CruiseModel cruiseModel: cruiseModels) {
-                List<String> output = new ArrayList<>();
-                output.add(cruiseModel.getCruiseCode());
-                output.add(cruiseModel.getBigItineraryMap());
-                output.add(cruiseModel.getDescription());
-                out.println(String.join(",", output));
+            final List<ItineraryModel> itineraryModels = ImportersUtils.getItineraries(resourceResolver);
+            StatefulBeanToCsv<ItineraryBean> beanToCsv = new StatefulBeanToCsvBuilder<ItineraryBean>(out).build();
+
+            for (ItineraryModel itineraryModel : itineraryModels) {
+                ItineraryBean bean = new ItineraryBean(itineraryModel);
+                beanToCsv.write(bean);
             }
-
-
             out.close();
-            // Iterate over exclusive offers from API
+
         } catch (LoginException | ImporterException e) {
+
+        } catch (CsvRequiredFieldEmptyException e) {
+            e.printStackTrace();
+        } catch (CsvDataTypeMismatchException e) {
+            e.printStackTrace();
         }
-
-
-        int numBytesRead = 10;
-        out.write(numBytesRead);
-    }
-    private List<CruiseModel> getCruiseDetails(ResourceResolver resourceResolver) {
-        final String query = "/jcr:root/content/silversea-com//element(*,cq:Page)" +
-                "[jcr:content/sling:resourceType=\"silversea/silversea-com/components/pages/destination\"]";
-
-        final Iterator<Resource> resources = resourceResolver.findResources(query,"xpath");
-
-        final List<CruiseModel> cruiseModels = new ArrayList<>();
-        while (resources.hasNext()) {
-            final Resource itinerary = resources.next();
-            final CruiseModel itineraryModel = itinerary.adaptTo(CruiseModel.class);
-
-            if (itineraryModel != null) {
-                cruiseModels.add(itineraryModel);
-            }
-        }
-        return cruiseModels;
     }
 }
