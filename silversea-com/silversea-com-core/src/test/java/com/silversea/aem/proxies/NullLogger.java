88dd15edf6d8c94b61a19e0b8a6da1a12e2c1d69
lukewstock <lukewstock@gmail.com>
2019-10-18 12:21:36 +0100

    Add null logger

---
+++ silversea-com/silversea-com-core/src/test/java/com/silversea/aem/proxies/NullLogger.java
@@ -0,0 +1,21 @@
+package com.silversea.aem.proxies;
+
+import com.silversea.aem.logging.JsonLog;
+import com.silversea.aem.logging.SSCLogger;
+
+public class NullLogger implements SSCLogger {
+    @Override
+    public void logInfo(JsonLog message) {
+
+    }
+
+    @Override
+    public void logWarning(JsonLog message) {
+
+    }
+
+    @Override
+    public void logError(JsonLog message) {
+
+    }
+}
