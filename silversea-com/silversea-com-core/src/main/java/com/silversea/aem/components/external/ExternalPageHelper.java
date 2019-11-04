1cf64276da8ffe218f383707cac2300f2092df9a
James Morcom <morcs@morcs.com>
2019-11-04 16:51:57 +0000

    Use helper to find template, seems to work

--- silversea-com/silversea-com-core/src/main/java/com/silversea/aem/components/external/ExternalPageHelper.java
+++ silversea-com/silversea-com-core/src/main/java/com/silversea/aem/components/external/ExternalPageHelper.java
@@ -6,14 +6,41 @@ import com.silversea.aem.content.CrxContentLoader;
 import com.silversea.aem.models.AppSettingsModel;
 import org.apache.sling.api.resource.ResourceResolver;
 
+import java.util.Arrays;
+
+// "/content/silversea-com/[^\\/]+/terms-and-conditions"
+// "/apps/silversea/silversea-com/components/external/termsandconditions/termsandconditions.html"
+
 public class ExternalPageHelper extends WCMUsePojo {
+    final ExternalPageDef[] externalPageDefs = {
+            new ExternalPageDef("/terms-and-conditions",
+                    "/termsandconditions/termsandconditions.html",
+                    x -> x.isTermsAndConditionsExternalUiEnabled())
+    };
+
+    static final String EXTERNAL_TEMPLATE_PATH = "/apps/silversea/silversea-com/components/external";
     static final String TERMS_AND_CONDITIONS_PATH_REGEX = "/content/silversea-com/[^\\/]+/terms-and-conditions";
     static final String FIND_YOUR_CRUISE_RESULTS_PATH_REGEX = "/content/silversea-com/[^\\/]+/cruise/cruise-results";
 
     private AppSettingsModel appSettings;
 
     public Boolean isExternalPage() {
-        return isTermsAndConditionsPage() || isFindYourCruiseResultsPage();
+        return Arrays.stream(externalPageDefs)
+                .filter(x -> x.featureToggle.isEnabled(appSettings))
+                .anyMatch(x -> matchesCurrentPage(x));
+    }
+
+    public String getTemplatePath() {
+        return Arrays.stream(externalPageDefs)
+                .filter(x -> x.featureToggle.isEnabled(appSettings))
+                .filter(x -> matchesCurrentPage(x))
+                .map(x -> EXTERNAL_TEMPLATE_PATH + x.templatePath)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private boolean matchesCurrentPage(ExternalPageDef x) {
+        return this.getCurrentPage().getPath().matches("/content/silversea-com/[^\\/]+" + x.crxPathRegex);
     }
 
     public Boolean isTermsAndConditionsPage() {
@@ -32,5 +59,30 @@ public class ExternalPageHelper extends WCMUsePojo {
         CrxContentLoader contentLoader = new CrxContentLoader(resourceResolver);
         ConfigurationManager configurationManager = new ConfigurationManager(contentLoader);
         this.appSettings = configurationManager.getAppSettings();
+    }
+
+    public interface FeatureToggle {
+        public Boolean isEnabled(AppSettingsModel appSettings);
+    }
+
+    private class ExternalPageDef {
+        private final FeatureToggle featureToggle;
+        private String crxPathRegex;
+        private String templatePath;
+
+        public ExternalPageDef(String crxPathRegex, String templatePath, FeatureToggle featureToggle) {
+
+            this.crxPathRegex = crxPathRegex;
+            this.templatePath = templatePath;
+            this.featureToggle = featureToggle;
+        }
+
+        public String getCrxPathRegex() {
+            return crxPathRegex;
+        }
+
+        public String getTemplatePath() {
+            return templatePath;
+        }
     }
 }
