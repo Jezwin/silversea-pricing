8e4593d8e77b4904606d6f6aa03fc72f99a116a4
James Morcom <morcs@morcs.com>
2019-11-04 15:52:21 +0000

    Separate component for all external UI pages.

--- silversea-com/silversea-com-core/src/main/java/com/silversea/aem/components/external/ExternalPageHelper.java
+++ silversea-com/silversea-com-core/src/main/java/com/silversea/aem/components/external/ExternalPageHelper.java
@@ -8,12 +8,22 @@ import org.apache.sling.api.resource.ResourceResolver;
 
 public class ExternalPageHelper extends WCMUsePojo {
     static final String TERMS_AND_CONDITIONS_PATH_REGEX = "/content/silversea-com/[^\\/]+/terms-and-conditions";
+    static final String FIND_YOUR_CRUISE_RESULTS_PATH_REGEX = "/content/silversea-com/[^\\/]+/cruise/cruise-results";
 
     private AppSettingsModel appSettings;
+
+    public Boolean isExternalPage() {
+        return isTermsAndConditionsPage() || isFindYourCruiseResultsPage();
+    }
 
     public Boolean isTermsAndConditionsPage() {
         return appSettings.isTermsAndConditionsExternalUiEnabled()
                 && this.getCurrentPage().getPath().matches(TERMS_AND_CONDITIONS_PATH_REGEX);
+    }
+
+    public Boolean isFindYourCruiseResultsPage() {
+        return appSettings.isFindYourCruiseExternalUiEnabled()
+                && this.getCurrentPage().getPath().matches(FIND_YOUR_CRUISE_RESULTS_PATH_REGEX);
     }
 
     @Override
